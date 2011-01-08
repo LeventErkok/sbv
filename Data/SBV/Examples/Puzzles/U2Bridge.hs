da4a4cdb0cf422e856aba9f3814775d785a83fa6
Levent Erkok <erkokl@gmail.com>
2011-01-08 10:23:57 -0800

    add more tests

--- Data/SBV/Examples/Puzzles/U2Bridge.hs
+++ Data/SBV/Examples/Puzzles/U2Bridge.hs
@@ -174,6 +174,14 @@ main = go 1
 
 -- Test suite
 testSuite :: SBVTestSuite
-testSuite = mkTestSuite $ \_ -> test [
-  "TBD" ~: assert True
+testSuite = mkTestSuite $ \goldCheck -> test [
+   "U2Bridge-1" ~: assert $ (0 ==) `fmap` count 1
+ , "U2Bridge-2" ~: assert $ (0 ==) `fmap` count 2
+ , "U2Bridge-3" ~: assert $ (0 ==) `fmap` count 3
+ , "U2Bridge-4" ~: assert $ (0 ==) `fmap` count 4
+ , "U2Bridge-5" ~: solve 5 `goldCheck` "U2Bridge.gold"
+ , "U2Bridge-6" ~: assert $ (0 ==) `fmap` count 6
  ]
+ where act     = do b <- free_; p1 <- free_; p2 <- free_; return (b, p1, p2)
+       count n = numberOfModels $ mapM (const act) [1..(n::Int)] >>= output . isValid
+       solve n = sat $ mapM (const act) [1..(n::Int)] >>= output . isValid
