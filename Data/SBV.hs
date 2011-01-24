955ae3ac129d8343feec4f1f30f96c8eca991d37
Levent Erkok <erkokl@gmail.com>
2011-01-24 00:54:55 -0800

    doc tweaks

--- Data/SBV.hs
+++ Data/SBV.hs
@@ -45,7 +45,8 @@
 --
 --   * Symbolic polynomials over GF(2^n), and polynomial arithmetic
 --
---   * Uninterpreted constants and functions over symbolic values
+--   * Uninterpreted constants and functions over symbolic values, with user
+--     defined SMT-Lib axioms
 --
 -- The user can construct ordinary Haskell programs using these types, which behave
 -- very similar to their concrete counterparts. In particular these types belong to the
