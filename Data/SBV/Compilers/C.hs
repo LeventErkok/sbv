e770e0891f867e94672739bdedae3016d4a48157
Levent Erkok <erkokl@gmail.com>
2011-03-06 19:47:18 -0800

    more work on compilation

--- Data/SBV/Compilers/C.hs
+++ Data/SBV/Compilers/C.hs
@@ -12,31 +12,25 @@
 
 module Data.SBV.Compilers.C(compileToC) where
 
-import Data.SBV.BitVectors.Data (Outputtable(..), runSymbolic, Result, Symbolic)
+import Data.SBV.BitVectors.Data (Outputtable(..), Result)
+import Data.SBV.Compilers.CodeGen
+
+-- token for the target language
+data SBVToC = SBVToC
 
 -- | Given a symbolic computation, render it as an equivalent
 --   C program. The first argument is an optional directory name
 --   under which the files will be saved. If `Nothing`, the result
 --   will be written to stdout. Use @`Just` \".\"@ for creating files in
 --   the current directory. The second argument is name of the function,
---   which also forms the names of the C and header files.
-compileToC :: Outputtable a => Maybe FilePath -> String -> Symbolic a -> IO ()
-compileToC mbDirName nm c = do
-   putStrLn "Performing symbolic execution.."
-   res <- runSymbolic (c >>= output)
-   cp <- symbolicToC nm res
-   putStrLn "Rendering as a C program.."
-   renderC mbDirName cp
-   putStrLn "Done."
+--   which also forms the names of the C and header files. The third argument
+--   is the names of the arguments to be used, finally followed by the computation.
+compileToC :: (CgArgs a, Outputtable b) => Maybe FilePath -> String -> [String] -> (a -> b) -> IO ()
+compileToC = codeGen SBVToC
 
-data CPgm = CPgm { makeFile :: [String]
-                 , cHeader  :: [String]
-                 , cProg    :: [String]
-                 }
-                 deriving Show
+instance SBVTarget SBVToC where
+  targetName _ = "C"
+  translate _  = cgen
 
-symbolicToC :: String -> Result -> IO CPgm
-symbolicToC nm c = return $ CPgm [] [] (lines (show c))
-
-renderC :: Maybe FilePath -> CPgm -> IO ()
-renderC mbDirName cp = print cp
+cgen :: String -> Result -> [(FilePath, [String])]
+cgen nm sbvProg = [(nm ++ ".c", lines (show sbvProg))]
---
+++ Data/SBV/Compilers/CodeGen.hs
@@ -0,0 +1,65 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Data.SBV.Compilers.CodeGen
+-- Copyright   :  (c) Levent Erkok
+-- License     :  BSD3
+-- Maintainer  :  erkokl@gmail.com
+-- Stability   :  experimental
+-- Portability :  portable
+--
+-- Code generation utilities
+-----------------------------------------------------------------------------
+
+module Data.SBV.Compilers.CodeGen where
+
+import Data.Char (toLower)
+import Data.List (isPrefixOf)
+import Control.Monad (when, filterM)
+import System.Directory (createDirectory, doesDirectoryExist, doesFileExist)
+import System.FilePath ((</>))
+
+import Data.SBV.BitVectors.Data (Outputtable(..), runSymbolic, Symbolic, Result, SymWord(..), SBV(..))
+
+codeGen :: (SBVTarget l, CgArgs a, Outputtable b) => l -> Maybe FilePath -> String -> [String] -> (a -> b) -> IO ()
+codeGen l mbDirName nm args f = do
+   putStrLn $ "Translating " ++ nm ++ " to " ++ targetName l ++ ".."
+   res <- runSymbolic $ cgArgs args >>= output . f
+   let files = translate l nm res
+   goOn <- maybe (return True) (check files) mbDirName
+   if goOn then do mapM_ (renderFile mbDirName) files
+                   putStrLn "Done."
+           else putStrLn "Aborting."
+  where createOutDir :: FilePath -> IO ()
+        createOutDir dn = do b <- doesDirectoryExist dn
+                             when (not b) $ do putStrLn $ "Creating directory " ++ show dn
+                                               createDirectory dn
+        check files dn = do createOutDir dn
+                            dups <- filterM (\fn -> doesFileExist (dn </> fn)) (map fst files)
+                            case dups of
+                              [] -> return True
+                              _  -> do putStrLn $ "Code generation would override the following " ++ (if length dups == 1 then "file:" else "files:")
+                                       mapM_ (\fn -> putStrLn ("\t" ++ fn)) dups
+                                       putStr "Continue? [yn] "
+                                       resp <- getLine
+                                       return $ map toLower resp `isPrefixOf` "yes"
+
+renderFile :: Maybe FilePath -> (FilePath, [String]) -> IO ()
+renderFile (Just d) (f, cs) = do let fn = d </> f
+                                 putStrLn $ "Generating: " ++ show fn ++ ".."
+                                 writeFile fn (unlines cs)
+renderFile Nothing  (f, cs) =  do putStrLn $ "== BEGIN: " ++ show f ++ " ================"
+                                  mapM_ putStrLn cs
+                                  putStrLn $ "== END: " ++ show f ++ " =================="
+
+-- | Abstract over code generation for different languages
+class SBVTarget a where
+  targetName :: a -> String
+  translate  :: a -> String -> Result -> [(FilePath, [String])]
+
+-- | Abstract over input variables over generated functions
+class CgArgs a where
+   cgArgs :: [String] -> Symbolic a
+
+instance SymWord a => CgArgs (SBV a) where
+   cgArgs []    = free_
+   cgArgs (s:_) = free s
