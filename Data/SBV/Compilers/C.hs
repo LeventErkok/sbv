6ce3466670c5c56fa65ae9387c2dcb9bd5f5f663
Levent Erkok <erkokl@gmail.com>
2011-04-03 23:15:14 -0700

    minor tweak to C output

--- Data/SBV/Compilers/C.hs
+++ Data/SBV/Compilers/C.hs
@@ -235,7 +235,7 @@ genDriver randVals fn inps outs mbRet =
   $$ text ""
   $$ nest 2 (   vcat (map mkInp pairedInputs)
              $$ vcat (map mkOut outs)
-             $$ sepIf (not (null pairedInputs) || not (null outs))
+             $$ sepIf (not (null [() | (_, _, CgArray{}) <- pairedInputs]) || not (null outs))
              $$ call
              $$ text ""
              $$ (case mbRet of
