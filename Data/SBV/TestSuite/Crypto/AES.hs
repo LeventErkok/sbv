c66e04f8a8bc1b6e8f83bd7c5b39d2de96705041
Levent Erkok <erkokl@gmail.com>
2011-04-07 00:25:29 -0700

    fix AES-lib-test inputs at 0 for repeatability

--- Data/SBV/TestSuite/Crypto/AES.hs
+++ Data/SBV/TestSuite/Crypto/AES.hs
@@ -21,7 +21,7 @@ testSuite :: SBVTestSuite
 testSuite = mkTestSuite $ \goldCheck -> test [
    "aes128Enc" ~: compileToC'    "aes128Enc" (aes128EncDec True)  `goldCheck` "aes128Enc.gold"
  , "aes128Dec" ~: compileToC'    "aes128Dec" (aes128EncDec False) `goldCheck` "aes128Dec.gold"
- , "aes128Lib" ~: compileToCLib' "aes128Lib" aes128LibComponents  `goldCheck` "aes128Lib.gold"
+ , "aes128Lib" ~: compileToCLib' "aes128Lib" aes128Comps          `goldCheck` "aes128Lib.gold"
  ]
  where aes128EncDec d = do pt  <- cgInputArr 4 "pt"
                            key <- cgInputArr 4 "key"
@@ -30,3 +30,5 @@ testSuite = mkTestSuite $ \goldCheck -> test [
                                res | d    = aesEncrypt pt encKs
                                    | True = aesDecrypt pt decKs
                            cgOutputArr "ct" res
+       aes128Comps = [(f, setVals c) | (f, c) <- aes128LibComponents]
+       setVals c = cgSetDriverValues (repeat 0) >> c
