fd861d9a767e44c98b1a68eb2d5314617c2eb742
Levent Erkok <erkokl@gmail.com>
2011-05-27 09:57:20 -0700

    Finish up RC4 example

--- Data/SBV/Examples/Crypto/RC4.hs
+++ Data/SBV/Examples/Crypto/RC4.hs
@@ -11,9 +11,9 @@
 -- using SBV. For information on RC4, see: <http://en.wikipedia.org/wiki/RC4>.
 --
 -- We make no effort to optimize the code, and instead focus on a clear
--- implementation. We then prove that encryption followed by decryption
--- yields the original input. From this, we extract fast C code that
--- executes RC4 over 64 byte inputs with a key size of 40 bits.
+-- implementation. In fact, the RC4 algorithm relies on in-place update of
+-- its state heavily for efficiency, and is therefore unsuitable for a purely
+-- functional implementation.
 -----------------------------------------------------------------------------
 
 {-# LANGUAGE ScopedTypeVariables #-}
@@ -21,7 +21,6 @@
 
 module Data.SBV.Examples.Crypto.RC4 where
 
-import Control.Monad
 import Data.Char  (ord, chr)
 import Data.List  (genericIndex)
 import Data.Maybe (fromJust)
@@ -66,21 +65,20 @@ prga (st', i', j') = (readArray st kInd, (st, i, j))
 -----------------------------------------------------------------------------
 
 -- | Constructs the state to be used by the PRGA using the given key.
-initRC4 :: Key -> RC4
+initRC4 :: Key -> S
 initRC4 key
  | keyLength < 1 || keyLength > 256
  = error $ "RC4 requires a key of length between 1 and 256, received: " ++ show keyLength
  | True
- = (snd $ foldl mix (0, sInit) [0..255], 0, 0)
+ = snd $ foldl mix (0, mkSFunArray id) [0..255]
  where keyLength = length key
-       sInit     = foldr (\i a -> writeArray a i i) (mkSFunArray 0) [0..255]
        mix :: (SWord8, S) -> SWord8 -> (SWord8, S)
        mix (j', s) i = let j = j' + readArray s i + genericIndex key (fromJust (unliteral i) `mod` fromIntegral keyLength)
                        in (j, swap i j s)
 
 -- | The key-schedule. Note that this function returns an infinite list.
 keySchedule :: Key -> [SWord8]
-keySchedule key = genKeys (initRC4 key)
+keySchedule key = genKeys (initRC4 key, 0, 0)
   where genKeys :: RC4 -> [SWord8]
         genKeys st = let (k, st') = prga st in k : genKeys st'
 
@@ -93,7 +91,7 @@ keyScheduleString = keySchedule . map (literal . fromIntegral . ord)
 -----------------------------------------------------------------------------
 
 -- | RC4 encryption. We generate key-words and xor it with the input. The
--- following test-vectors are from Wikipedia: <http://en.wikipedia.org/wiki/RC4>:
+-- following test-vectors are from Wikipedia <http://en.wikipedia.org/wiki/RC4>:
 --
 -- >>> concatMap hex $ encrypt "Key" "Plaintext"
 -- "bbf316e8d940af0ad3"
@@ -107,7 +105,7 @@ encrypt :: String -> String -> [SWord8]
 encrypt key pt = zipWith xor (keyScheduleString key) (map cvt pt)
   where cvt = literal . fromIntegral . ord
 
--- | RC4 decryption. Essentially the same as decryption. We have:
+-- | RC4 decryption. Essentially the same as decryption. For the above test vectors we have:
 --
 -- >>> decrypt "Key" [0xbb, 0xf3, 0x16, 0xe8, 0xd9, 0x40, 0xaf, 0x0a, 0xd3]
 -- "Plaintext"
@@ -129,43 +127,16 @@ decrypt key ct = map cvt $ zipWith xor (keyScheduleString key) ct
 -- The theorem is stated parametrically over key and plain-text sizes. Here is the
 -- proof for a 40-bit key (5 bytes) and 40-bit plaintext (again 5 bytes).
 --
--- >>> rc4IsCorrect 5 5
--- Q.E.D.
---
--- Note that this theorem is trivial, since it is essentially establishing
+-- Note that this theorem is trivial to prove, since it is essentially establishing
 -- xor'in the same value twice leaves a word unchanged (i.e., @x `xor` y `xor` y = x@).
 -- However, the proof takes quite a while to complete, as it gives rise to a fairly
 -- large symbolic trace. The time spent in completing this proof is purely in the symbolic
 -- simulation, while the actual proof time taken by Yices is comparatively negligable.
 rc4IsCorrect :: Int -> Int -> IO ThmResult
-rc4IsCorrect keyLen ptLen = proveWith verboseSMTCfg $ do
+rc4IsCorrect keyLen ptLen = prove $ do
         key <- mkFreeVars keyLen
         pt  <- mkFreeVars ptLen
         let ks  = keySchedule key
             ct  = zipWith xor ks pt
             pt' = zipWith xor ks ct
         return $ pt .== pt'
-
------------------------------------------------------------------------------
--- * Code generation
------------------------------------------------------------------------------
--- | Generates C functions that perform the 'initRC4' and 'prga' functions.
--- In an imperative setting like in C, these two functions will suffice to
--- construct RC4 encryption/decryption quite easily. The 'initRC4' function
--- will expect a 40-byte input as the key stream.
-codegenRC4 :: IO ()
-codegenRC4 = compileToCLib Nothing "rc4Lib" [  ("initRC4", cInitRC4)
-                                             , ("prga",    cPRGA)
-                                            ]
-  where cInitRC4 = do key <- cgInputArr 5 "key"
-                      let (s, _, _) = initRC4 key
-                      cgOutputArr "S" [readArray s i | i <- [0 .. 255]]
-        cPRGA = do s <- cgInputArr 256 "S"
-                   i <- cgInput "i"
-                   j <- cgInput "j"
-                   let sa = foldr (\i a -> writeArray a (literal i) (genericIndex s i)) (mkSFunArray 0) [0..255]
-                       (k, (s', i', j')) = prga (sa, i, j)
-                   cgOutputArr "newS" [readArray s' i | i <- [0 .. 255]]
-                   cgOutput "newI" i'
-                   cgOutput "newJ" j'
-                   cgReturn k
