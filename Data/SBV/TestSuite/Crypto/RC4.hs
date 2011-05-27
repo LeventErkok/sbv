712329c874af28d0552f9c07addf62ab39a44297
Levent Erkok <erkokl@gmail.com>
2011-05-27 10:21:47 -0700

    add RC4 to test suite

---
+++ Data/SBV/TestSuite/Crypto/RC4.hs
@@ -0,0 +1,26 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Data.SBV.TestSuite.Crypto.RC4
+-- Copyright   :  (c) Levent Erkok
+-- License     :  BSD3
+-- Maintainer  :  erkokl@gmail.com
+-- Stability   :  experimental
+-- Portability :  portable
+--
+-- Test suite for Data.SBV.Examples.Crypto.RC4
+-----------------------------------------------------------------------------
+
+module Data.SBV.TestSuite.Crypto.RC4(testSuite) where
+
+import Data.SBV
+import Data.SBV.Internals
+import Data.SBV.Examples.Crypto.RC4
+
+-- Test suite
+testSuite :: SBVTestSuite
+testSuite = mkTestSuite $ \goldCheck -> test [
+   "rc4swap" ~: assert =<< isTheorem swapIsCorrect
+ ]
+ where swapIsCorrect i j = swap i j (swap j i s) `arrEq` s
+          where arrEq a b = bAnd [readArray a i .== readArray b i | i <- [0 .. 255]]
+                s = mkSFunArray id
