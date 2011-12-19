edcf2aaf6784aefce4d836c81e737299fd22a4c5
Levent Erkok <erkokl@gmail.com>
2011-12-18 20:36:38 -0800

    specialize cost to SBV values; simplifies signatures a bit

--- Data/SBV/BitVectors/Optimize.hs
+++ Data/SBV/BitVectors/Optimize.hs
@@ -15,6 +15,8 @@
 
 module Data.SBV.BitVectors.Optimize (OptimizeOpts(..), optimize, optimizeWith, minimize, minimizeWith, maximize, maximizeWith) where
 
+import Data.Maybe (fromJust)
+
 import Data.SBV.BitVectors.Data
 import Data.SBV.BitVectors.Model (OrdSymbolic(..), EqSymbolic(..))
 import Data.SBV.Provers.Prover   (satWith, z3)
@@ -29,11 +31,11 @@ data OptimizeOpts = Iterative  Bool   -- ^ Iteratively search. if True, it will 
 
 -- | Symbolic optimization. Generalization on 'minimize' and 'maximize' that allows arbitrary
 -- cost functions and comparisons.
-optimizeWith :: (Show cost, OrdSymbolic cost, Show a, SatModel a, SymWord a)
+optimizeWith :: (SatModel a, SymWord a, Show a, SymWord c, Show c)
              => SMTConfig                         -- ^ SMT configuration
              -> OptimizeOpts                      -- ^ Optimization options
-             -> (cost -> cost -> SBool)           -- ^ comparator
-             -> ([SBV a] -> cost)                 -- ^ cost function
+             -> (SBV c -> SBV c -> SBool)         -- ^ comparator
+             -> ([SBV a] -> SBV c)                -- ^ cost function
              -> Int                               -- ^ how many elements?
              -> ([SBV a] -> SBool)                -- ^ validity constraint
              -> IO (Maybe [a])
@@ -41,33 +43,33 @@ optimizeWith cfg (Iterative chatty) = iterOptimize chatty cfg
 optimizeWith cfg Quantified         = quantOptimize cfg
 
 -- | Variant of 'optimizeWith' using z3
-optimize :: (Show a, Show cost, OrdSymbolic cost, SatModel a, SymWord a) => OptimizeOpts -> (cost -> cost -> SBool) -> ([SBV a] -> cost) -> Int -> ([SBV a] -> SBool) -> IO (Maybe [a])
+optimize :: (SatModel a, SymWord a, Show a, SymWord c, Show c) => OptimizeOpts -> (SBV c -> SBV c -> SBool) -> ([SBV a] -> SBV c) -> Int -> ([SBV a] -> SBool) -> IO (Maybe [a])
 optimize = optimizeWith z3
 
--- | Variant of 'maximize' allowing the use of a user specified solver
-maximizeWith :: (Show a, Show cost, SatModel a, SymWord a, OrdSymbolic cost) => SMTConfig -> OptimizeOpts -> ([SBV a] -> cost) -> Int -> ([SBV a] -> SBool) -> IO (Maybe [a])
+-- | Variant of 'maximize' allowing the use of a user specified solver.
+maximizeWith :: (SatModel a, SymWord a, Show a, SymWord c, Show c) => SMTConfig -> OptimizeOpts -> ([SBV a] -> SBV c) -> Int -> ([SBV a] -> SBool) -> IO (Maybe [a])
 maximizeWith cfg opts = optimizeWith cfg opts (.>=)
 
 -- | Maximizes a cost function with respect to a constraint. Examples:
 --
 --   >>> maximize Quantified sum 3 (bAll (.< (10 :: SInteger)))
 --   Just [9,9,9]
-maximize :: (Show a, Show cost, SatModel a, SymWord a, OrdSymbolic cost) => OptimizeOpts -> ([SBV a] -> cost) -> Int -> ([SBV a] -> SBool) -> IO (Maybe [a])
+maximize :: (SatModel a, SymWord a, Show a, SymWord c, Show c) => OptimizeOpts -> ([SBV a] -> SBV c) -> Int -> ([SBV a] -> SBool) -> IO (Maybe [a])
 maximize = maximizeWith z3
 
--- | Variant of 'minimize' allowing the use of a user specified solver
-minimizeWith :: (Show a, Show cost, SatModel a, SymWord a, OrdSymbolic cost) => SMTConfig -> OptimizeOpts -> ([SBV a] -> cost) -> Int -> ([SBV a] -> SBool) -> IO (Maybe [a])
+-- | Variant of 'minimize' allowing the use of a user specified solver.
+minimizeWith :: (SatModel a, SymWord a, Show a, SymWord c, Show c) => SMTConfig -> OptimizeOpts -> ([SBV a] -> SBV c) -> Int -> ([SBV a] -> SBool) -> IO (Maybe [a])
 minimizeWith cfg opts = optimizeWith cfg opts (.<=)
 
 -- | Minimizes a cost function with respect to a constraint. Examples:
 --
 --   >>> minimize Quantified sum 3 (bAll (.> (10 :: SInteger)))
 --   Just [11,11,11]
-minimize :: (Show a, Show cost, SatModel a, SymWord a, OrdSymbolic cost) => OptimizeOpts -> ([SBV a] -> cost) -> Int -> ([SBV a] -> SBool) -> IO (Maybe [a])
+minimize :: (SatModel a, SymWord a, Show a, SymWord c, Show c) => OptimizeOpts -> ([SBV a] -> SBV c) -> Int -> ([SBV a] -> SBool) -> IO (Maybe [a])
 minimize = minimizeWith z3
 
 -- | Optimization using quantifiers
-quantOptimize :: (SatModel a, SymWord a) => SMTConfig -> (cost -> cost -> SBool) -> ([SBV a] -> cost) -> Int -> ([SBV a] -> SBool) -> IO (Maybe [a])
+quantOptimize :: (SatModel a, SymWord a) => SMTConfig -> (SBV c -> SBV c -> SBool) -> ([SBV a] -> SBV c) -> Int -> ([SBV a] -> SBool) -> IO (Maybe [a])
 quantOptimize cfg cmp cost n valid = do
            m <- satWith cfg $ do xs <- mkExistVars  n
                                  ys <- mkForallVars n
@@ -78,7 +80,7 @@ quantOptimize cfg cmp cost n valid = do
               Left _           -> return Nothing
 
 -- | Optimization using iteration
-iterOptimize :: (Show cost, OrdSymbolic cost, Show a, SatModel a, SymWord a) => Bool -> SMTConfig -> (cost -> cost -> SBool) -> ([SBV a] -> cost) -> Int -> ([SBV a] -> SBool) -> IO (Maybe [a])
+iterOptimize :: (SatModel a, Show a, SymWord a, Show c, SymWord c) =>  Bool -> SMTConfig -> (SBV c -> SBV c -> SBool) -> ([SBV a] -> SBV c) -> Int -> ([SBV a] -> SBool) -> IO (Maybe [a])
 iterOptimize chatty cfg cmp cost n valid = do
         msg "Trying to find a satisfying solution."
         m <- satWith cfg $ valid `fmap` mkExistVars n
@@ -87,7 +89,7 @@ iterOptimize chatty cfg cmp cost n valid = do
           Right (True, _)  -> error "SBV: Backend solver reported \"unknown\""
           Right (False, a) -> do msg $ "First solution found: " ++ show a
                                  let c = cost (map literal a)
-                                 msg $ "Initial value is    : " ++ show c
+                                 msg $ "Initial value is    : " ++ show (fromJust (unliteral c))
                                  msg "Starting iterative search."
                                  go (1::Int) a c
   where msg m | chatty = putStrLn $ "*** " ++ m
@@ -102,5 +104,5 @@ iterOptimize chatty cfg cmp cost n valid = do
                   Right (True, _)  -> error "SBV: Backend solver reported \"unknown\""
                   Right (False, a) -> do msg $ "Solution: " ++ show a
                                          let c = cost (map literal a)
-                                         msg $ "Value   : " ++ show c
+                                         msg $ "Value   : " ++ show (fromJust (unliteral c))
                                          go (i+1) a c
