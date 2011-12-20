03cf68e558a50c89b321939c887fc38c6a13901c
Levent Erkok <erkokl@gmail.com>
2011-12-19 22:42:08 -0800

    Just use a simpler Setup; the additional work is not really worth it.

--- Setup.hs
+++ Setup.hs
@@ -13,22 +13,7 @@
 {-# OPTIONS_GHC -Wall #-}
 module Main(main) where
 
-import Distribution.Simple (defaultMainWithHooks, simpleUserHooks, postInst)
-import System.Directory    (findExecutable)
-import System.Exit         (exitWith, ExitCode(..))
-
-import Data.SBV.Provers.Prover (SMTConfig(..), SMTSolver(..), defaultSMTCfg)
+import Distribution.Simple
 
 main :: IO ()
-main = defaultMainWithHooks simpleUserHooks{ postInst = checkDefSolver }
- where checkDefSolver _ _ _ _ = do
-                -- Issue a warning if the executable is not found; but otherwise harmless.
-                mbP <- findExecutable ex
-                case mbP of
-                   Nothing -> do putStrLn $ "*** The sbv library requires the default solver " ++ nm ++ " to be installed."
-                                 putStrLn $ "*** The executable " ++ show ex ++ " must be in your path."
-                                 putStrLn $ "*** Do not forget to install " ++ nm ++ "!"
-                   Just _  -> return ()
-                exitWith ExitSuccess
-       ex = executable (solver defaultSMTCfg)
-       nm = name (solver defaultSMTCfg)
+main = defaultMain
