ac3cb43dc39f3bcfbf6c555ba91d1945e995f91d
Levent Erkok <erkokl@gmail.com>
2011-12-27 00:57:35 -0800

    genTest: properly handle constraints

--- Data/SBV/BitVectors/GenTest.hs
+++ Data/SBV/BitVectors/GenTest.hs
@@ -19,10 +19,19 @@ import Data.SBV.BitVectors.Data
 -- | Generate a set of concrete test values from a symbolic program. The output
 -- can be rendered as test vectors in different languages as necessary. Use the
 -- function 'output' call to indicate what fields should be in the test result.
+-- (Also see 'constrain' and 'pConstrain' for filtering acceptable test values.)
 genTest :: Int -> Symbolic () -> IO [([CW], [CW])]
-genTest n m = sequence [tc | _ <- [1 .. n]]
-  where tc = do (_, Result _ tvals _ _ cs _ _ _ _ _ cstrs os) <- runSymbolic' Concrete m
-                case cstrs of
-                  [] -> let cval = fromMaybe (error "Cannot quick-check in the presence of uninterpeted constants!") . (`lookup` cs)
-                        in return (map snd tvals, map cval os)
-                  _  -> error "Not yet supported: Quickcheck in the presence of explicit constraints."
+genTest n m = gen 0 []
+  where gen i sofar
+         | i == n = return (reverse sofar)
+         | True   = do mbT <- tc
+                       case mbT of
+                        Nothing -> gen i     sofar
+                        Just t  -> gen (i+1) (t:sofar)
+        tc = do (_, Result _ tvals _ _ cs _ _ _ _ _ cstrs os) <- runSymbolic' Concrete m
+                let cval = fromMaybe (error "Cannot generate tests in the presence of uninterpeted constants!") . (`lookup` cs)
+                    cond = all (cwToBool . cval) cstrs
+                    io   = (map snd tvals, map cval os)
+                if cond
+                   then return $ Just io
+                   else return Nothing
