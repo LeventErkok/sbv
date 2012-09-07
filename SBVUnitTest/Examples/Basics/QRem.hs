d7c85e6c52e83462f914c75c9fb8bfe40246554a
Levent Erkok <erkokl@gmail.com>
2012-09-07 00:42:52 -0700

    add divMod tests and a couple of extra instances

--- SBVUnitTest/Examples/Basics/QRem.hs
+++ SBVUnitTest/Examples/Basics/QRem.hs
@@ -6,7 +6,7 @@
 -- Maintainer  :  erkokl@gmail.com
 -- Stability   :  experimental
 --
--- Testing the qrem (quote-rem) function
+-- Testing the sQuotRem and sDivMod
 -----------------------------------------------------------------------------
 
 module Examples.Basics.QRem where
@@ -14,12 +14,17 @@ module Examples.Basics.QRem where
 import Data.SBV
 
 -- check: if (a, b) = x `quotRem` y then x = y*a + b
+-- same is also true for divMod
 -- being careful about y = 0. When divisor is 0, then quotient is
 -- defined to be 0 and the remainder is the numerator
 qrem :: (Num a, EqSymbolic a, SDivisible a) => a -> a -> SBool
-qrem x y = ite (y .== 0) ((0, x) .== (a, b)) (x .== y * a + b)
-  where (a, b) = x `sQuotRem` y
+qrem x y = ite (y .== 0)
+               ((0, x) .== (q, r) &&& (0, x) .== (d, m))
+               (x .== y * q + r &&& x .== y * d + m)
+  where (q, r) = x `sQuotRem` y
+        (d, m) = x `sDivMod` y
 
 check :: IO ()
-check = print =<< prove (qrem :: SWord8 -> SWord8 -> SBool)
-         -- print =<< prove (qrem :: SWord16 -> SWord16 -> SBool)   -- takes too long!
+check = do print =<< prove (qrem :: SWord8   -> SWord8   -> SBool)
+           print =<< prove (qrem :: SInt8    -> SInt8    -> SBool)
+           print =<< prove (qrem :: SInteger -> SInteger -> SBool)
