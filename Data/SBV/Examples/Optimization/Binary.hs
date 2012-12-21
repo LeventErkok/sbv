77738b3f015a2d27d2457052ca9b89ca066de181
Takayuki Muranushi <muranushi@gmail.com>
2012-12-21 12:55:55 +0900

    begin create binary search examples

---
+++ Data/SBV/Examples/Optimization/Binary.hs
@@ -0,0 +1,14 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Data.SBV.Examples.Optimization.Binary
+-- Copyright   :  (c) Levent Erkok, Takayuki Muranushi
+-- License     :  BSD3
+-- Maintainer  :  erkokl@gmail.com, muranushi@gmail.com
+-- Stability   :  experimental
+--
+--
+module Data.SBV.Examples.Optimization.Binary where
+
+import Data.SBV
+
+-- argminSat :: (OrdSymbolic a, Num a) => a
