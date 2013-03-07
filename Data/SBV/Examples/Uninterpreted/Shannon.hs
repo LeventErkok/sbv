4a9fb315a9037dab1718fa114a8688129065cd19
Levent Erkok <erkokl@gmail.com>
2013-03-06 23:27:22 -0800

    Add further shannon examples

--- Data/SBV/Examples/Uninterpreted/Shannon.hs
+++ Data/SBV/Examples/Uninterpreted/Shannon.hs
@@ -52,14 +52,14 @@ shannon = prove $ \x y z -> f x y z .== (x &&& pos f y z ||| bnot x &&& neg f y 
        f = uninterpret "f"
 
 -----------------------------------------------------------------------------
--- * Computing derivatives of boolean functions
+-- * Derivatives
 -----------------------------------------------------------------------------
 
 -- | Computing the derivative of a boolean function (boolean difference).
 -- Defined as exclusive-or of Shannon cofactors with respect to that
 -- variable.
 derivative :: Ternary -> Binary
-derivative f x y = pos f x y <+> neg f x y
+derivative f y z = pos f y z <+> neg f y z
 
 -- | The no-wiggle theorem: If the derivative of a function with respect to
 -- a variable is constant False, then that variable does not "wiggle" the
@@ -72,5 +72,49 @@ derivative f x y = pos f x y <+> neg f x y
 noWiggle :: IO ThmResult
 noWiggle = prove $ \y z -> bnot (f' y z) <=> pos f y z .== neg f y z
   where f :: Ternary
-        f = uninterpret "f"
+        f  = uninterpret "f"
         f' = derivative f
+
+-----------------------------------------------------------------------------
+-- * Universal quantification
+-----------------------------------------------------------------------------
+
+-- | Universal quantification of a boolean function with respect to a variable.
+-- Simply defined as the conjunction of the Shannon cofactors.
+universal :: Ternary -> Binary
+universal f y z = pos f y z &&& neg f y z
+
+-- | Show that universal quantification is really meaningful: That is, if the universal
+-- quantification with respect to a variable is True, then both cofactors are true for
+-- those arguments. Of course, this is a trivial theorem if you think about it for a
+-- moment, or you can just let SBV prove it for you:
+--
+-- >>> univOK
+-- Q.E.D.
+univOK :: IO ThmResult
+univOK = prove $ \y z -> f' y z ==> pos f y z &&& neg f y z
+  where f :: Ternary
+        f  = uninterpret "f"
+        f' = universal f
+
+-----------------------------------------------------------------------------
+-- * Existential quantification
+-----------------------------------------------------------------------------
+
+-- | Existential quantification of a boolean function with respect to a variable.
+-- Simply defined as the conjunction of the Shannon cofactors.
+existential :: Ternary -> Binary
+existential f y z = pos f y z ||| neg f y z
+
+-- | Show that existential quantification is really meaningful: That is, if the existential
+-- quantification with respect to a variable is True, then one of the cofactors must be true for
+-- those arguments. Again, this is a trivial theorem if you think about it for a moment, but
+-- we will just let SBV prove it:
+--
+-- >>> existsOK
+-- Q.E.D.
+existsOK :: IO ThmResult
+existsOK = prove $ \y z -> f' y z ==> pos f y z ||| neg f y z
+  where f :: Ternary
+        f  = uninterpret "f"
+        f' = existential f
