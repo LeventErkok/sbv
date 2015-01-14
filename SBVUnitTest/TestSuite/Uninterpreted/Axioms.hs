83b43e450faabc545f73cbd29d5543dd6aa7c830
Levent Erkok <erkokl@gmail.com>
2015-01-13 20:05:37 -0800

    Uninterpreted sorts now need Show instance

--- SBVUnitTest/TestSuite/Uninterpreted/Axioms.hs
+++ SBVUnitTest/TestSuite/Uninterpreted/Axioms.hs
@@ -23,7 +23,7 @@ testSuite = mkTestSuite $ \_ -> test [
  ]
 
 -- Example provided by Thomas DuBuisson:
-data Bitstring = Bitstring () deriving (Eq, Ord, Data, Typeable, Read)
+data Bitstring = Bitstring () deriving (Eq, Ord, Data, Typeable, Read, Show)
 instance SymWord Bitstring
 instance HasKind Bitstring
 type SBitstring = SBV Bitstring
