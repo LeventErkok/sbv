34cd5e8e70ff2efc6f4963774567fedabaa585f3
Brian Huffman <huffman@galois.com>
2015-01-21 11:37:10 -0800

    Data.SBV.Internals exports more functions useful for class instances

--- Data/SBV/BitVectors/Model.hs
+++ Data/SBV/BitVectors/Model.hs
@@ -30,7 +30,8 @@ module Data.SBV.BitVectors.Model (
   , sWord32s, sWord64, sWord64s, sInt8, sInt8s, sInt16, sInt16s, sInt32, sInt32s, sInt64
   , sInt64s, sInteger, sIntegers, sReal, sReals, toSReal, sFloat, sFloats, sDouble, sDoubles, slet
   , fusedMA
-  , liftQRem, liftDMod, genLiteral, symbolicMergeWithKind
+  , liftQRem, liftDMod, symbolicMergeWithKind
+  , genLiteral, genFromCW, genMkSymVar
   )
   where
 
