c6b0ff6a52f3c9171885fee97b748b99c673ae0a
Brian Huffman <huffman@galois.com>
2015-03-26 08:52:45 -0700

    Add module Data.SBV.Dynamic, providing low-level dynamically typed API

---
+++ Data/SBV/Dynamic.hs
@@ -0,0 +1,40 @@
+---------------------------------------------------------------------------------
+-- |
+-- Module      :  Data.SBV.Dynamic
+-- Copyright   :  (c) Levent Erkok
+-- License     :  BSD3
+-- Maintainer  :  erkokl@gmail.com
+-- Stability   :  experimental
+--
+-- Dynamically typed low-level API to the SBV library, for users who
+-- want to generate symbolic values at run-time. Note that with this
+-- API it is possible to create terms that are not type correct; use
+-- at your own risk!
+---------------------------------------------------------------------------------
+
+module Data.SBV.Dynamic
+  ( Kind(..)
+  , Symbolic
+  , Quantifier(..)
+  , SVal
+  , svKind
+  , svMkSymVar
+  -- ** Basic constructors
+  , svTrue, svFalse, svBool
+  , svInteger
+  , svAsBool
+  -- ** Basic operations
+  , svPlus, svTimes, svMinus, svUNeg, svAbs
+  , svQuot, svRem
+  , svEqual, svNotEqual
+  , svLessThan, svGreaterThan, svLessEq, svGreaterEq
+  , svAnd, svOr, svXOr, svNot
+  , svShl, svShr, svRol, svRor
+  , svExtract, svJoin
+  , svUninterpreted
+  , svIte, svLazyIte, svSymbolicMerge
+  ) where
+
+import Data.SBV.BitVectors.Kind
+import Data.SBV.BitVectors.Symbolic
+import Data.SBV.BitVectors.Operations
