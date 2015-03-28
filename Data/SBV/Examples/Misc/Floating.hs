29adfaef519e905b4f8a8b7f1a60d2c1cee26d69
Levent Erkok <erkokl@gmail.com>
2015-03-28 15:30:46 -0700

    Floats: Update expected results from latest z3

--- Data/SBV/Examples/Misc/Floating.hs
+++ Data/SBV/Examples/Misc/Floating.hs
@@ -48,7 +48,7 @@ assocPlus :: SFloat -> SFloat -> SFloat -> SBool
 assocPlus x y z = x + (y + z) .== (x + y) + z
 
 -- | Prove that addition is not associative, even if we ignore @NaN@/@Infinity@ values.
--- To do this, we use the predicate 'isFPPoint', which is true of a floating point
+-- To do this, we use the predicate 'isPointFP', which is true of a floating point
 -- number ('SFloat' or 'SDouble') if it is neither @NaN@ nor @Infinity@. (That is, it's a
 -- representable point in the real-number line.)
 --
@@ -56,25 +56,25 @@ assocPlus x y z = x + (y + z) .== (x + y) + z
 --
 -- >>> assocPlusRegular
 -- Falsifiable. Counter-example:
---   x = -3.7777752e22 :: SFloat
---   y = -1.180801e18 :: SFloat
---   z = 9.4447324e21 :: SFloat
+--   x = -1.9414223e-37 :: SFloat
+--   y = 1.0652902e-38 :: SFloat
+--   z = 8.474605e-37 :: SFloat
 --
 -- Indeed, we have:
 --
--- >>> ((-3.7777752e22 + (-1.180801e18)) + 9.4447324e21) :: Float
--- -2.83342e22
--- >>> (-3.7777752e22 + ((-1.180801e18) + 9.4447324e21)) :: Float
--- -2.8334201e22
+-- >>> ((-1.9414223e-37 + 1.0652902e-38) + 8.474605e-37) :: Float
+-- 6.6397113e-37
+-- >>> ((-1.9414223e-37) + (1.0652902e-38 + 8.474605e-37)) :: Float
+-- 6.6397118e-37
 --
--- Note the loss of precision in the first expression.
+-- Note the difference in the last digit by one bit. (Recal: 3 is 0111, while 8 is 1000 in binary!)
 assocPlusRegular :: IO ThmResult
 assocPlusRegular = prove $ do [x, y, z] <- sFloats ["x", "y", "z"]
                               let lhs = x+(y+z)
                                   rhs = (x+y)+z
                               -- make sure we do not overflow at the intermediate points
-                              constrain $ isFPPoint lhs
-                              constrain $ isFPPoint rhs
+                              constrain $ isPointFP lhs
+                              constrain $ isPointFP rhs
                               return $ lhs .== rhs
 
 -----------------------------------------------------------------------------
@@ -87,23 +87,23 @@ assocPlusRegular = prove $ do [x, y, z] <- sFloats ["x", "y", "z"]
 --
 -- >>> nonZeroAddition
 -- Falsifiable. Counter-example:
---   a = 2.1474839e10 :: SFloat
---   b = -7.275957e-11 :: SFloat
+--   a = 1.5046344e-36 :: SFloat
+--   b = -6.6e-44 :: SFloat
 --
 -- Indeed, we have:
 --
--- >>> 2.1474839e10 + (-7.275957e-11) == (2.1474839e10 :: Float)
+-- >>> 1.5046344e-36 + (-6.6e-44) == (1.5046344e-36 :: Float)
 -- True
 --
 -- But:
 --
--- >>> -7.275957e-11 == (0 :: Float)
+-- >>> -6.6e-44 == (0::Float)
 -- False
 --
 nonZeroAddition :: IO ThmResult
 nonZeroAddition = prove $ do [a, b] <- sFloats ["a", "b"]
-                             constrain $ isFPPoint a
-                             constrain $ isFPPoint b
+                             constrain $ isPointFP a
+                             constrain $ isPointFP b
                              constrain $ a + b .== a
                              return $ b .== 0
 
@@ -118,17 +118,17 @@ nonZeroAddition = prove $ do [a, b] <- sFloats ["a", "b"]
 --
 -- >>> multInverse
 -- Falsifiable. Counter-example:
---   a = 1.2354518252390238e308 :: SDouble
+--   a = -8.988465741280883e307 :: SDouble
 --
 -- Indeed, we have:
 --
--- >>> let a = 1.2354518252390238e308 :: Double
+-- >>> let a = -8.988465741280883e307 :: Double
 -- >>> a * (1/a)
--- 0.9999999999999998
+-- 0.9999999999999999
 multInverse :: IO ThmResult
 multInverse = prove $ do a <- sDouble "a"
-                         constrain $ isFPPoint a
-                         constrain $ isFPPoint (1/a)
+                         constrain $ isPointFP a
+                         constrain $ isPointFP (1/a)
                          return $ a * (1/a) .== 1
 
 -----------------------------------------------------------------------------
