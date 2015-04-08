c04867ac760b568a6d32deb1846ea9efbbeaeb21
Brian Huffman <huffman@galois.com>
2015-04-07 17:24:10 -0700

    Export more from Data.SBV.Dynamic

--- Data/SBV/Dynamic.hs
+++ Data/SBV/Dynamic.hs
@@ -13,47 +13,97 @@
 ---------------------------------------------------------------------------------
 
 module Data.SBV.Dynamic
-  ( Kind(..)
+  (
+  -- * Programming with symbolic values
+  -- $progIntro
+  -- ** Symbolic types
+  -- *** Abstract symbolic value type
+    SVal
+  , CW(..)
+  , Kind(..)
+  , svKind, svBitSize, svSigned
+  -- *** Arrays of symbolic values
+  , SArr
+  , readSArr, resetSArr, writeSArr, mergeSArr, newSArr, eqSArr
+
+  -- ** Creating a symbolic variable
+  -- $createSym
   , Symbolic
   , Quantifier(..)
-  , SVal
-  , svKind
-  , svBitSize, svSigned
   , svMkSymVar
-  -- ** Basic constructors
-  , svTrue, svFalse, svBool
-  , svInteger
-  -- ** Basic destructors
-  , svAsBool, svAsInteger
-  -- ** Basic operations
+  -- ** Operations on symbolic values
+  -- *** Boolean literals
+  , svTrue, svFalse, svBool, svAsBool
+  -- *** Integer literals
+  , svInteger, svAsInteger
+  -- *** Symbolic equality
+  , svEqual, svNotEqual
+  -- *** Symbolic ordering
+  , svLessThan, svGreaterThan, svLessEq, svGreaterEq
+  -- *** Arithmetic operations
   , svPlus, svTimes, svMinus, svUNeg, svAbs
   , svDivide, svQuot, svRem
-  , svEqual, svNotEqual
-  , svLessThan, svGreaterThan, svLessEq, svGreaterEq
+  -- *** Logical operations
   , svAnd, svOr, svXOr, svNot
   , svShl, svShr, svRol, svRor
+  -- *** Splitting, joining, and extending
   , svExtract, svJoin
-  , svUninterpreted
-  , svIte, svLazyIte, svSymbolicMerge
-  , svSelect
+  -- *** Sign-casting
   , svSign, svUnsign
-  -- ** Derived operations
+  -- *** Indexed lookups
+  , svSelect
+  -- *** Word-level operations
   , svToWord1, svFromWord1, svTestBit
   , svShiftLeft, svShiftRight
   , svRotateLeft, svRotateRight
-  , svCompileToSMTLib
-  -- ** Code Generation
+  -- ** Conditionals: Mergeable values
+  , svIte, svLazyIte, svSymbolicMerge
+  -- * Uninterpreted sorts, constants, and functions
+  -- $uninterpreted
+  , svUninterpreted
+  -- * Model extraction
+  -- $modelExtraction
+
+  -- ** Inspecting proof results
+  -- $resultTypes
+  , ThmResult(..), SatResult(..), AllSatResult(..), SMTResult(..), SafeResult(..)
+  -- * SMT Interface: Configurations and solvers
+  , SMTConfig(..), SMTLibLogic(..), Logic(..), OptimizeOpts(..), Solver(..), SMTSolver(..), boolector, cvc4, yices, z3, mathSAT, abc, defaultSolverConfig, sbvCurrentSolver, defaultSMTCfg, sbvCheckSolverInstallation, sbvAvailableSolvers
+
+  -- * Symbolic computations
+  , Symbolic, outputSVal{-, SymWord(..)-}
+
+  -- * Getting SMT-Lib output (for offline analysis)
+  , svCompileToSMTLib{-, generateSMTBenchmarks-}
+  -- * Code generation from symbolic programs
+  -- $cCodeGeneration
   , SBVCodeGen
-  , svCgInput, svCgInputArr
-  , svCgOutput, svCgOutputArr
-  , svCgReturn, svCgReturnArr
+
+  -- ** Setting code-generation options
   , cgPerformRTCs, cgSetDriverValues, cgGenerateDriver, cgGenerateMakefile
+
+  -- ** Designating inputs
+  , svCgInput, svCgInputArr
+
+  -- ** Designating outputs
+  , svCgOutput, svCgOutputArr
+
+  -- ** Designating return values
+  , svCgReturn, svCgReturnArr
+
+  -- ** Code generation with uninterpreted functions
   , cgAddPrototype, cgAddDecl, cgAddLDFlags
+
+  -- ** Code generation with 'SInteger' and 'SReal' types
+  -- $unboundedCGen
   , cgIntegerSize, cgSRealType, CgSRealType(..)
+
+  -- ** Compilation to C
   , compileToC, compileToCLib
   ) where
 
 import Data.SBV.BitVectors.Kind
+import Data.SBV.BitVectors.Concrete
 import Data.SBV.BitVectors.Symbolic
 import Data.SBV.BitVectors.Operations
 
@@ -69,4 +119,10 @@ import Data.SBV.Compilers.CodeGen
 import Data.SBV.Compilers.C
   ( compileToC, compileToCLib )
 import Data.SBV.Provers.Prover
-  ( svCompileToSMTLib )
+  ( boolector, cvc4, yices, z3, mathSAT, abc, defaultSMTCfg, svCompileToSMTLib )
+import Data.SBV.SMT.SMT
+  ( ThmResult(..), SatResult(..), AllSatResult(..), SafeResult(..) )
+import Data.SBV.Tools.Optimize
+  ( OptimizeOpts(..) )
+import Data.SBV
+  ( sbvCurrentSolver, sbvCheckSolverInstallation, defaultSolverConfig, sbvAvailableSolvers )
