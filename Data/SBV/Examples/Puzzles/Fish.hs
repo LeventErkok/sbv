acccb91c0da9b6413074d3710f25538aef19efa7
Levent Erkok <erkokl@gmail.com>
2015-05-18 09:06:44 -0700

    Make it compile with GHC 7.8..
    Need to get rid of the extra boilerplate when Travis-CI gets GHC7.10

--- Data/SBV/Examples/Puzzles/Fish.hs
+++ Data/SBV/Examples/Puzzles/Fish.hs
@@ -36,19 +36,19 @@ import Data.Generics
 import Data.SBV
 
 -- | Colors of houses
-data Color       = Red      | Green    | White      | Yellow    | Blue   deriving (Show, Read, Data, Eq, Ord)
+data Color       = Red      | Green    | White      | Yellow    | Blue   deriving (Show, Read, Data, Typeable, Eq, Ord)
 
 -- | Nationalities of the occupants
-data Nationality = Briton   | Dane     | Swede      | Norwegian | German deriving (Show, Read, Data, Eq, Ord)
+data Nationality = Briton   | Dane     | Swede      | Norwegian | German deriving (Show, Read, Data, Typeable, Eq, Ord)
 
 -- | Beverage choices
-data Beverage    = Tea      | Coffee   | Milk       | Beer      | Water  deriving (Show, Read, Data, Eq, Ord)
+data Beverage    = Tea      | Coffee   | Milk       | Beer      | Water  deriving (Show, Read, Data, Typeable, Eq, Ord)
 
 -- | Pets they keep
-data Pet         = Dog      | Horse    | Cat        | Bird      | Fish   deriving (Show, Read, Data, Eq, Ord)
+data Pet         = Dog      | Horse    | Cat        | Bird      | Fish   deriving (Show, Read, Data, Typeable, Eq, Ord)
 
 -- | Sports they engage in
-data Sport       = Football | Baseball | Volleyball | Hockey    | Tennis deriving (Show, Read, Data, Eq, Ord)
+data Sport       = Football | Baseball | Volleyball | Hockey    | Tennis deriving (Show, Read, Data, Typeable, Eq, Ord)
 
 -- | 'SymWord' instance. With GHC 7.10; we'll be able to derive this automatically
 instance SymWord Color
