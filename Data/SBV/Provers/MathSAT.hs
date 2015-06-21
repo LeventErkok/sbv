0187558ec5545e9b2fd85ac9f6321fed5acb9cdd
Levent Erkok <erkokl@gmail.com>
2015-06-20 17:57:03 -0700

    Simplify MathSAT interface

--- Data/SBV/Provers/MathSAT.hs
+++ Data/SBV/Provers/MathSAT.hs
@@ -13,45 +13,29 @@
 
 module Data.SBV.Provers.MathSAT(mathSAT) where
 
-import Data.Function      (on)
-import Data.List          (sortBy)
-
 import Data.SBV.BitVectors.Data
 import Data.SBV.SMT.SMT
-import Data.SBV.SMT.SMTLib
 
 -- | The description of the MathSAT SMT solver
 -- The default executable is @\"mathsat\"@, which must be in your path. You can use the @SBV_MATHSAT@ environment variable to point to the executable on your system.
 -- The default options are @\"-input=smt2\"@. You can use the @SBV_MATHSAT_OPTIONS@ environment variable to override the options.
 mathSAT :: SMTSolver
 mathSAT = SMTSolver {
-           name           = MathSAT
-         , executable     = "mathsat"
-         , options        = ["-input=smt2"]
-         , engine         = standardEngine "SBV_MATHSAT" "SBV_MATHSAT_OPTIONS" addTimeOut extractMap
-         , capabilities   = SolverCapabilities {
-                                  capSolverName              = "MathSAT"
-                                , mbDefaultLogic             = Nothing
-                                , supportsMacros             = False
-                                , supportsProduceModels      = True
-                                , supportsQuantifiers        = True
-                                , supportsUninterpretedSorts = True
-                                , supportsUnboundedInts      = True
-                                , supportsReals              = True
-                                , supportsFloats             = True
-                                , supportsDoubles            = True
-                                }
+           name         = MathSAT
+         , executable   = "mathsat"
+         , options      = ["-input=smt2"]
+         , engine       = standardEngine "SBV_MATHSAT" "SBV_MATHSAT_OPTIONS" addTimeOut standardModel
+         , capabilities = SolverCapabilities {
+                                capSolverName              = "MathSAT"
+                              , mbDefaultLogic             = Nothing
+                              , supportsMacros             = False
+                              , supportsProduceModels      = True
+                              , supportsQuantifiers        = True
+                              , supportsUninterpretedSorts = True
+                              , supportsUnboundedInts      = True
+                              , supportsReals              = True
+                              , supportsFloats             = True
+                              , supportsDoubles            = True
+                              }
          }
- where addTimeOut _ _ = error "MathSAT: Timeout values are not supported by MathSat"
-
-extractMap :: Bool -> [(Quantifier, NamedSymVar)] -> [String] -> SMTModel
-extractMap _isSat qinps solverLines =
-   SMTModel { modelAssocs = map snd $ sortByNodeId $ concatMap (interpretSolverModelLine (map snd qinps) . cvt) solverLines }
-  where sortByNodeId :: [(Int, a)] -> [(Int, a)]
-        sortByNodeId = sortBy (compare `on` fst)
-        cvt :: String -> String
-        cvt s = case words s of
-                  [var, val] -> "((" ++ var ++ " #b" ++ map tr val ++ "))"
-                  _          -> s -- good-luck..
-          where tr 'x' = '0'
-                tr x   = x
+ where addTimeOut _ _ = error "MathSAT: Timeout values are not supported"
--- SBVUnitTest/SBVUnitTestBuildTime.hs
+++ SBVUnitTest/SBVUnitTestBuildTime.hs
@@ -2,4 +2,4 @@
 module SBVUnitTestBuildTime (buildTime) where
 
 buildTime :: String
-buildTime = "Sat Jun 20 17:32:38 PDT 2015"
+buildTime = "Sat Jun 20 17:55:55 PDT 2015"
