e715411098338d6afb240014de669b43d091da37
Levent Erkok <erkokl@gmail.com>
2015-06-20 17:24:45 -0700

    Further clean-up ABC interface

--- Data/SBV/Provers/ABC.hs
+++ Data/SBV/Provers/ABC.hs
@@ -9,8 +9,6 @@
 -- The connection to the ABC verification and synthesis tool
 -----------------------------------------------------------------------------
 
-{-# LANGUAGE ScopedTypeVariables #-}
-
 module Data.SBV.Provers.ABC(abc) where
 
 import Data.SBV.BitVectors.Data
@@ -23,21 +21,21 @@ import Data.SBV.SMT.SMT
 -- You can use the @SBV_ABC_OPTIONS@ environment variable to override the options.
 abc :: SMTSolver
 abc = SMTSolver {
-           name           = ABC
-         , executable     = "abc"
-         , options        = ["-S", "%blast; &sweep -C 5000; &syn4; &cec -s -m -C 2000"]
-         , engine         = standardEngine "SBV_ABC" "SBV_ABC_OPTIONS" addTimeOut standardModel
-         , capabilities   = SolverCapabilities {
-                                  capSolverName              = "ABC"
-                                , mbDefaultLogic             = Nothing
-                                , supportsMacros             = True
-                                , supportsProduceModels      = True
-                                , supportsQuantifiers        = False
-                                , supportsUninterpretedSorts = False
-                                , supportsUnboundedInts      = False
-                                , supportsReals              = False
-                                , supportsFloats             = False
-                                , supportsDoubles            = False
-                                }
+           name         = ABC
+         , executable   = "abc"
+         , options      = ["-S", "%blast; &sweep -C 5000; &syn4; &cec -s -m -C 2000"]
+         , engine       = standardEngine "SBV_ABC" "SBV_ABC_OPTIONS" addTimeOut standardModel
+         , capabilities = SolverCapabilities {
+                                capSolverName              = "ABC"
+                              , mbDefaultLogic             = Nothing
+                              , supportsMacros             = True
+                              , supportsProduceModels      = True
+                              , supportsQuantifiers        = False
+                              , supportsUninterpretedSorts = False
+                              , supportsUnboundedInts      = False
+                              , supportsReals              = False
+                              , supportsFloats             = False
+                              , supportsDoubles            = False
+                              }
          }
-  where addTimeOut _ _ = error "ABC: Timeout values are not supported by ABC"
+  where addTimeOut _ _ = error "ABC: Timeout values are not supported"
