8fec04172025e8f8c4d3a0aa3e87f4e95386fe4a
Levent Erkok <erkokl@gmail.com>
2015-09-22 22:31:47 -0700

    ignore herbie droppings

--- buildUtils/simplify.hs
+++ buildUtils/simplify.hs
@@ -48,6 +48,7 @@ junk s = any (`isPrefixOf` s) junkPre || any (`isInfixOf` s) junkIn
                  , "Warning: The documentation for the following packages are not installed."
                  , "links will be generated to these packages:"
                  , "haddock coverage for"
+                 , "Compiling with Herbie floating point stabilization"
                  ]
        junkIn  = [ "(reinstall) changes:"
                  , "could not find link destinations for"
