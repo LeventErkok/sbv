51fcfbad50c41ad70a77a8917217e3e36a7aa829
kanishka <nish2575@gmail.com>
2017-05-16 01:26:15 -0400

    Convert basics.index

--- SBVUnitTest/SBVIntTest.hs
+++ SBVUnitTest/SBVIntTest.hs
@@ -2,10 +2,13 @@ module Main(main) where
 
 import Test.Tasty
 import qualified TestSuite.Arrays.Memory
+import qualified TestSuite.Basics.Index
 
 main :: IO ()
 main = defaultMain (testGroup "Tests" tests)
 
 tests :: [TestTree]
 tests =
-  [ TestSuite.Arrays.Memory.tests ]
+  [ TestSuite.Arrays.Memory.tests
+  , TestSuite.Basics.Index.tests
+  ]
--- SBVUnitTest/TestSuite/Basics/Index.hs
+++ SBVUnitTest/TestSuite/Basics/Index.hs
@@ -9,12 +9,20 @@
 -- Test suite for Examples.Basics.Index
 -----------------------------------------------------------------------------
 
-module TestSuite.Basics.Index(testSuite) where
+module TestSuite.Basics.Index(tests) where
 
-import Examples.Basics.Index
+import Examples.Basics.Index (test1, test2, test3)
 import SBVTest
 
--- Test suite
-testSuite :: SBVTestSuite
-testSuite = mkTestSuite $ \_ -> test $ zipWith tst [f x | f <- [test1, test2, test3], x <- [0..13]] [(0::Int)..]
-  where tst t i = "index-" ++ show i ~: t `ioShowsAs` "True"
+tests :: TestTree
+tests =
+  testGroup "Basics.Index"
+    (zipWith
+      mkTest
+      [f x | f <- [test1, test2, test3], x <- [0..13]]
+      [(0::Int)..])
+
+mkTest :: IO Bool -> Int -> TestTree
+mkTest tst i =
+  testCase ("index-" ++ show i)
+    (assert tst)
