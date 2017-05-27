1ff8af219afd2a5136a5e92ca7d50346cb4ca2d8
Levent Erkok <erkokl@gmail.com>
2017-05-26 22:42:45 -0700

    Add RandomSeed option

--- Data/SBV/Control/Types.hs
+++ Data/SBV/Control/Types.hs
@@ -51,14 +51,14 @@ data SMTReasonUnknown = UnknownMemOut
 
 -- | Collectable information from the solver.
 data SMTInfoResponse = Resp_Unsupported
-                     | Resp_AllStatistics           [(String, String)]
-                     | Resp_AssertionStackLevels    Integer
-                     | Resp_Authors                 [String]
-                     | Resp_Error                   SMTErrorBehavior
-                     | Resp_Name                    String
-                     | Resp_ReasonUnknown           SMTReasonUnknown
-                     | Resp_Version                 String
-                     | Resp_InfoKeyword             String
+                     | Resp_AllStatistics        [(String, String)]
+                     | Resp_AssertionStackLevels Integer
+                     | Resp_Authors              [String]
+                     | Resp_Error                SMTErrorBehavior
+                     | Resp_Name                 String
+                     | Resp_ReasonUnknown        SMTReasonUnknown
+                     | Resp_Version              String
+                     | Resp_InfoKeyword          String
                      deriving Show
 
 -- | Show instance for SMTInfoFlag maintains smt-lib format per the SMTLib2 standard document.
@@ -75,9 +75,11 @@ instance Show SMTInfoFlag where
 -- | Option values that can be set in the solver. Note that not
 -- all solvers may support all of these!
 data SMTOption = DiagnosticOutputChannel FilePath
+               | RandomSeed Integer
 
 -- | Show instance for SMTOption maintains smt-lib format per the SMTLib2 standard document.
 instance Show SMTOption where
   show (DiagnosticOutputChannel f) = ":diagnostic-output-channel " ++ show f
+  show (RandomSeed              i) = ":random-seed " ++ show i
 
 {-# ANN type SMTInfoResponse ("HLint: ignore Use camelCase" :: String) #-}
