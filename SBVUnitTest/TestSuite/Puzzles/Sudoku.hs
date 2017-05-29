1c47529edf16f8c303d74715f75185b07c2425be
kanishka <nish2575@gmail.com>
2017-05-29 17:42:43 -0400

    Sudoku to Tasty style

--- SBVUnitTest/TestSuite/Puzzles/Sudoku.hs
+++ SBVUnitTest/TestSuite/Puzzles/Sudoku.hs
@@ -9,17 +9,21 @@
 -- Test suite for Data.SBV.Examples.Puzzles.Sudoku
 -----------------------------------------------------------------------------
 
-module TestSuite.Puzzles.Sudoku(testSuite) where
+module TestSuite.Puzzles.Sudoku(tests) where
 
 import Data.SBV
 import Data.SBV.Examples.Puzzles.Sudoku
 
 import SBVTest
 
--- Test suite
-testSuite :: SBVTestSuite
-testSuite = mkTestSuite $ \_ -> test [
-  "sudoku " ++ show n ~: assert (checkPuzzle s)
-     | (n, s) <- zip [(0::Int)..] [puzzle0, puzzle1, puzzle2, puzzle3, puzzle4, puzzle5, puzzle6]
- ]
- where checkPuzzle (i, f) = isSat $ (valid . f) `fmap` mkExistVars i
+tests :: TestTree
+tests =
+  testGroup "Puzzles.Sudoku"
+    [ testCase ("sudoku " ++ show n) (assert (checkPuzzle s))
+       | (n, s) <-
+           zip
+	     [(0::Int)..]
+	     [puzzle0, puzzle1, puzzle2, puzzle3, puzzle4, puzzle5, puzzle6] ]
+
+checkPuzzle :: Puzzle -> IO Bool
+checkPuzzle (i, f) = isSat $ (valid . f) `fmap` mkExistVars i
