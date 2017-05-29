cecd147b822b06e2f7bc58acb605edf93e9f41b4
kanishka <nish2575@gmail.com>
2017-05-29 17:32:59 -0400

    CRC.USB5, MagicSquare, NQueens to Tasty style

--- SBVUnitTest/SBVIntTest.hs
+++ SBVUnitTest/SBVIntTest.hs
@@ -8,10 +8,19 @@ import qualified TestSuite.Basics.QRem
 import qualified TestSuite.BitPrecise.BitTricks
 import qualified TestSuite.BitPrecise.PrefixSum
 import qualified TestSuite.CRC.CCITT_Unidir
+import qualified TestSuite.CRC.USB5
 import qualified TestSuite.CRC.GenPoly
 import qualified TestSuite.Crypto.RC4
 import qualified TestSuite.Polynomials.Polynomials
 import qualified TestSuite.CRC.Parity
+import qualified TestSuite.Puzzles.MagicSquare
+import qualified TestSuite.Puzzles.NQueens
+import qualified TestSuite.Puzzles.PowerSet
+import qualified TestSuite.Puzzles.Sudoku
+import qualified TestSuite.Uninterpreted.Axioms
+import qualified TestSuite.Uninterpreted.Function
+import qualified TestSuite.Uninterpreted.Sort
+import qualified TestSuite.Uninterpreted.Uninterpreted
 
 main :: IO ()
 main = defaultMain (testGroup "Tests" tests)
@@ -28,8 +37,22 @@ tests =
   , TestSuite.BitPrecise.BitTricks.tests
   , TestSuite.BitPrecise.PrefixSum.tests
   , TestSuite.CRC.CCITT_Unidir.tests
+  , TestSuite.CRC.USB5.tests
   , TestSuite.CRC.GenPoly.tests
   , TestSuite.Crypto.RC4.tests
   , TestSuite.Polynomials.Polynomials.tests
   , TestSuite.CRC.Parity.tests
+  -- dogcat
+  -- euler
+  , TestSuite.Puzzles.MagicSquare.tests
+  , TestSuite.Puzzles.NQueens.tests
+  -- , TestSuite.Puzzles.PowerSet.tests
+  -- , TestSuite.Puzzles.Sukoku.tests
+  -- temp
+  -- u2br
+  -- auf
+  -- , TestSuite.Uninterpreted.Axioms.tests
+  -- , TestSuite.Uninterpreted.Function.tests
+  -- , TestSuite.Uninterpreted.Sort.tests
+  -- , TestSuite.Uninterpreted.Uninterpreted.tests
   ]
--- SBVUnitTest/TestSuite/Puzzles/MagicSquare.hs
+++ SBVUnitTest/TestSuite/Puzzles/MagicSquare.hs
@@ -9,17 +9,20 @@
 -- Test suite for Data.SBV.Examples.Puzzles.MagicSquare
 -----------------------------------------------------------------------------
 
-module TestSuite.Puzzles.MagicSquare(testSuite) where
+module TestSuite.Puzzles.MagicSquare(tests) where
 
 import Data.SBV
 import Data.SBV.Examples.Puzzles.MagicSquare
 
 import SBVTest
 
--- Test suite
-testSuite :: SBVTestSuite
-testSuite = mkTestSuite $ \_ -> test [
-   "magic 2" ~: assert . not =<< isSat (mkMagic 2)
- , "magic 3" ~: assert       =<< isSat (mkMagic 3)
- ]
- where mkMagic n = (isMagic . chunk n) `fmap` mkExistVars (n*n)
+tests :: TestTree
+tests =
+  testGroup "Puzzles.MagicSquare"
+    [ testCase "magic 2" (assertIsntSat (mkMagic 2))
+    , testCase "magic 3" (assertIsSat (mkMagic 3))
+    ]
+
+
+mkMagic :: Int -> Symbolic SBool
+mkMagic n = (isMagic . chunk n) `fmap` mkExistVars (n*n)
