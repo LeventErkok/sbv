e2466d66b939d928b7f2251a91bbbe246226a1fc
kanishka <nish2575@gmail.com>
2017-05-29 16:10:40 -0400

    Convert CRC.GenPoly to Tasty style

--- SBVUnitTest/TestSuite/CRC/GenPoly.hs
+++ SBVUnitTest/TestSuite/CRC/GenPoly.hs
@@ -9,7 +9,7 @@
 -- Test suite for Examples.CRC.GenPoly
 -----------------------------------------------------------------------------
 
-module TestSuite.CRC.GenPoly(testSuite) where
+module TestSuite.CRC.GenPoly(tests) where
 
 import Data.SBV
 
@@ -17,13 +17,17 @@ import Examples.CRC.GenPoly
 import SBVTest
 
 -- Test suite
-testSuite :: SBVTestSuite
-testSuite = mkTestSuite $ \_ -> test [
-   "crcGood" ~: assert       =<< isSat crcGoodE
- , "crcGood" ~: assert . not =<< isThm (crcGood 3 12)
- ]
- where crcGoodE = do x1 <- exists_
-                     x2 <- exists_
-                     y1 <- exists_
-                     y2 <- exists_
-                     return (crcGood 3 0 (x1,x2) (y1,y2))
+tests :: TestTree
+tests =
+  testGroup "CRC.GenPoly"
+    [   testCase "crcGoodE" (assertIsSat crcGoodE)
+      , testCase "crcGood" (assertIsntThm (crcGood 3 12))
+    ]
+
+crcGoodE :: Symbolic SBool
+crcGoodE = do
+  x1 <- exists_
+  x2 <- exists_
+  y1 <- exists_
+  y2 <- exists_
+  return (crcGood 3 0 (x1,x2) (y1,y2))
