0ec0874308f655cd221d2e283d27641c252b3187
Levent Erkok <erkokl@gmail.com>
2017-06-05 16:45:47 -0700

    Add showTDiff back (Reimplement for NominalTimeDiff)
    Fixes #283

--- Data/SBV/Utils/TDiff.hs
+++ Data/SBV/Utils/TDiff.hs
@@ -14,6 +14,7 @@ module Data.SBV.Utils.TDiff
   , Timing(..)
   , TimedStep(..)
   , TimingInfo
+  , showTDiff
   )
   where
 
@@ -23,6 +24,13 @@ import Data.Time       (NominalDiffTime, getCurrentTime, diffUTCTime)
 import           Data.Map (Map)
 import qualified Data.Map as Map
 import           Data.IORef(IORef, modifyIORef')
+
+import Data.List (intercalate)
+
+import Data.Ratio
+import GHC.Real   (Ratio((:%)))
+
+import Numeric (showFFloat)
 
 -- | Specify how to save timing information, if at all.
 data Timing     = NoTiming | PrintTiming | SaveTiming (IORef TimingInfo)
@@ -42,6 +50,34 @@ timedStepLabel lbl =
     Translation         -> "translation"
     WorkByProver x      -> x
 
+-- Show NominalDiffTime in human readable form. NominalDiffTime is
+-- essentially picoseconds (10^-12 seconds). We show it so that
+-- it's represented at the day:hour:minute:second.XXX granularity
+showTDiff :: NominalDiffTime -> String
+showTDiff diff
+   | denom /= 1    -- Should never happen! But just in case.
+   = show diff
+   | True
+   = intercalate ":" fields
+   where total, denom :: Integer
+         total :% denom = (picoFactor % 1) * toRational diff
+
+         -- there are 10^12 pico-seconds in a second
+         picoFactor :: Integer
+         picoFactor = (10 :: Integer) ^ (12 :: Integer)
+
+         [s2p, m2s, h2m, d2h] = drop 1 $ scanl (*) 1 [picoFactor, 60, 60, 24]
+
+         (days,    days')    = total    `divMod` d2h
+         (hours,   hours')   = days'    `divMod` h2m
+         (minutes, seconds') = hours'   `divMod` m2s
+         (seconds, picos)    = seconds' `divMod` s2p
+         secondsPicos        =  show seconds
+                             ++ dropWhile (/= '.') (showFFloat (Just 3) (fromIntegral picos * (10**(-12) :: Double)) "s")
+
+         aboveSeconds = map (\(t, v) -> show v ++ [t]) $ dropWhile (\p -> snd p == 0) [('d', days), ('h', hours), ('m', minutes)]
+         fields       = aboveSeconds ++ [secondsPicos]
+
 -- | If selected, runs the computation @m@, and prints the time it took
 -- to run it. The return type should be an instance of 'NFData' to ensure
 -- the correct elapsed time is printed.
@@ -50,7 +86,7 @@ timeIf how what m =
   case how of
     NoTiming    -> m
     PrintTiming -> do (elapsed, a) <- doTime m
-                      putStrLn $ "** Elapsed " ++ timedStepLabel what ++ " time: " ++ show elapsed
+                      putStrLn $ "** Elapsed " ++ timedStepLabel what ++ " time: " ++ showTDiff elapsed
                       return a
     SaveTiming here -> do (elapsed, a) <- doTime m
                           modifyIORef' here (Map.insert what elapsed)
