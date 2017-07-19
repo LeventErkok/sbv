167a7dff176c35f32d9a74bda00972119506a138
Levent Erkok <erkokl@gmail.com>
2017-07-18 23:17:53 -0700

    Add a few dynamic tests too.

--- SBVTestSuite/TestSuite/Basics/Recursive.hs
+++ SBVTestSuite/TestSuite/Basics/Recursive.hs
@@ -13,16 +13,46 @@ module TestSuite.Basics.Recursive(tests) where
 
 import Utils.SBVTestFramework
 
+import Control.Monad.Reader (ask)
+import Control.Monad.Trans  (liftIO)
+import qualified Data.SBV.Dynamic as D
+
 -- This is recursive and suffers from the termination problem.
 -- But we can still prove a few things about it!
 mgcd :: SWord8 -> SWord8 -> SWord8
 mgcd a b = ite (b .== 0) a (mgcd b (a `sMod` b))
 
 -- Same construction, expressed in terms of the dynamic interface
+mgcdDyn :: Int -> IO ThmResult
+mgcdDyn i = D.proveWith z3 $ do
+
+              let var8 :: String -> Symbolic D.SVal
+                  var8 nm = ask >>= liftIO . D.svMkSymVar (Just D.ALL) word8 (Just nm)
+
+                  word8   = KBounded False 8
+                  zero8   = D.svInteger word8 0
+
+                  gcdDyn a b = D.svIte (b `D.svEqual` zero8) a (gcdDyn b (a `D.svRem` b))
+
+              x <- var8 "x"
+
+              let prop0 = gcdDyn zero8 x `D.svEqual` x
+                  prop1 = gcdDyn x zero8 `D.svEqual` x
+
+              return $  if i == 0 then prop0 else prop1
+
+checkThm :: ThmResult -> Assertion
+checkThm r = assert isThm
+  where isThm = case r of
+                  ThmResult Unsatisfiable{} -> (return True :: IO Bool)
+                  ThmResult Satisfiable{}   -> return False
+                  _                         -> error "checkThm: Unexpected result!"
 
 -- Test suite
 tests :: TestTree
 tests = testGroup "Basics.Recursive"
    [ testCase "recursive1"    $ assertIsThm $ \x -> mgcd    0 x .== x
    , testCase "recursive2"    $ assertIsThm $ \x -> mgcd    x 0 .== x
+   , testCase "recursiveDyn1" $ checkThm =<< mgcdDyn 0
+   , testCase "recursiveDyn2" $ checkThm =<< mgcdDyn 1
    ]
