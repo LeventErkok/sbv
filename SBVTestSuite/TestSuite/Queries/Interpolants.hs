8c7d22f6ec3a0422ae251cabafee30a08b160e21
Levent Erkok <erkokl@gmail.com>
2017-07-26 21:31:24 -0700

    Another interpolant test
    Fixes #306

--- SBVTestSuite/TestSuite/Queries/Interpolants.hs
+++ SBVTestSuite/TestSuite/Queries/Interpolants.hs
@@ -22,6 +22,7 @@ tests =
   testGroup "Basics.QueryIndividual"
     [ goldenCapturedIO "query_Interpolant1" $ \rf -> runSMTWith z3{verbose=True, redirectVerbose=Just rf} q1
     , goldenCapturedIO "query_Interpolant2" $ \rf -> runSMTWith z3{verbose=True, redirectVerbose=Just rf} q2
+    , goldenCapturedIO "query_Interpolant3" $ \rf -> runSMTWith z3{verbose=True, redirectVerbose=Just rf} q3
     ]
 
 q1 :: Symbolic ()
@@ -53,6 +54,23 @@ q2 = do a <- sInteger "a"
 
         namedConstraint "c1" $ f a .== c &&& f b .== d
         namedConstraint "c2" $   a .== b &&& g c ./= g d
+
+        query $ do _ <- checkSat
+                   _ <- getInterpolant "c1" "c2"
+                   return ()
+
+q3 :: Symbolic ()
+q3 = do x <- sInteger "x"
+        y <- sInteger "y"
+        z <- sInteger "z"
+
+        a :: SArray Integer Integer <- newArray "a"
+        b :: SArray Integer Integer <- newArray "b"
+
+        namedConstraint "c1" $ b .== writeArray (writeArray a x 0) y (0::SInteger)
+        namedConstraint "c2" $ z .== x &&& readArray b z .== 1
+
+        setOption $ ProduceInterpolants True
 
         query $ do _ <- checkSat
                    _ <- getInterpolant "c1" "c2"
