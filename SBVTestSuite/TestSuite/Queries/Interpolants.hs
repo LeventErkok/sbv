b6bb07a06f53bfe830b53e75b97464836dd8218e
Levent Erkok <erkokl@gmail.com>
2017-07-26 21:06:15 -0700

    Another interplant test

--- SBVTestSuite/TestSuite/Queries/Interpolants.hs
+++ SBVTestSuite/TestSuite/Queries/Interpolants.hs
@@ -21,6 +21,7 @@ tests :: TestTree
 tests =
   testGroup "Basics.QueryIndividual"
     [ goldenCapturedIO "query_Interpolant1" $ \rf -> runSMTWith z3{verbose=True, redirectVerbose=Just rf} q1
+    , goldenCapturedIO "query_Interpolant2" $ \rf -> runSMTWith z3{verbose=True, redirectVerbose=Just rf} q2
     ]
 
 q1 :: Symbolic ()
@@ -33,6 +34,25 @@ q1 = do a <- sInteger "a"
 
         namedConstraint "c1" $ a .== b &&& a .== c
         namedConstraint "c2" $ b .== d &&& bnot (c .== d)
+
+        query $ do _ <- checkSat
+                   _ <- getInterpolant "c1" "c2"
+                   return ()
+
+q2 :: Symbolic ()
+q2 = do a <- sInteger "a"
+        b <- sInteger "b"
+        c <- sInteger "c"
+        d <- sInteger "d"
+
+        let f, g :: SInteger -> SInteger
+            f = uninterpret "f"
+            g = uninterpret "g"
+
+        setOption $ ProduceInterpolants True
+
+        namedConstraint "c1" $ f a .== c &&& f b .== d
+        namedConstraint "c2" $   a .== b &&& g c ./= g d
 
         query $ do _ <- checkSat
                    _ <- getInterpolant "c1" "c2"
