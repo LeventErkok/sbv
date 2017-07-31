4c173e7cc1c1a42f2631874cb82fc232e0a5e8d6
Levent Erkok <erkokl@gmail.com>
2017-07-31 00:07:06 -0700

    Appveyor really can't handle any of the heavy stuff. Sigh.

--- SBVTestSuite/SBVTest.hs
+++ SBVTestSuite/SBVTest.hs
@@ -89,7 +89,7 @@ ciFilter te tt = do putStrLn $ "CI: Reducing tests by " ++ show (100-p) ++ "% fo
         p = case te of
               CILinux   -> 30
               CIOSX     -> 10
-              CIWindows -> 10
+              CIWindows ->  0 -- Appveyor runs out-of memory even with 10%
 
 main :: IO ()
 main = do testEnv <- getTestEnvironment
