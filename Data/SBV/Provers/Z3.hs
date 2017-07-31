a7ef4aee1818e13c2a5400029dba2ebd16bcf3ad
Levent Erkok <erkokl@gmail.com>
2017-07-30 20:54:03 -0700

    Z3: Use the '-' option prefix for Windows too.

--- Data/SBV/Provers/Z3.hs
+++ Data/SBV/Provers/Z3.hs
@@ -18,15 +18,6 @@ import Data.Char (toLower)
 import Data.SBV.Core.Data
 import Data.SBV.SMT.SMT
 
-import qualified System.Info as S(os)
-
--- Choose the correct prefix character for passing options
--- TBD: Is there a more foolproof way of determining this?
-optionPrefix :: Char
-optionPrefix
-  | map toLower S.os `elem` ["linux", "darwin"] = '-'
-  | True                                        = '/'   -- windows
-
 -- | The description of the Z3 SMT solver.
 -- The default executable is @\"z3\"@, which must be in your path. You can use the @SBV_Z3@ environment variable to point to the executable on your system.
 -- The default options are @\"-nw -in -smt2\"@, which is valid for Z3 4.1. You can use the @SBV_Z3_OPTIONS@ environment variable to override the options.
@@ -34,7 +25,7 @@ z3 :: SMTSolver
 z3 = SMTSolver {
            name         = Z3
          , executable   = "z3"
-         , options      = map (optionPrefix:) . modConfig ["nw", "in", "smt2"]
+         , options      = modConfig ["-nw", "-in", "-smt2"]
          , engine       = standardEngine "SBV_Z3" "SBV_Z3_OPTIONS"
          , capabilities = SolverCapabilities {
                                 supportsQuantifiers        = True
