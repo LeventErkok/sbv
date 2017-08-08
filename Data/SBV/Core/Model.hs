260cb95581027872f4fcce14d9af9904f1aa9780
Levent Erkok <levent.erkok@intel.com>
2017-08-08 15:27:19 -0700

    Work around a GHC 8.0.1 warning

--- Data/SBV/Core/Model.hs
+++ Data/SBV/Core/Model.hs
@@ -9,7 +9,6 @@
 -- Instance declarations for our symbolic world
 -----------------------------------------------------------------------------
 
-{-# OPTIONS_GHC -fno-warn-orphans   #-}
 {-# LANGUAGE TypeSynonymInstances   #-}
 {-# LANGUAGE BangPatterns           #-}
 {-# LANGUAGE PatternGuards          #-}
@@ -20,6 +19,12 @@
 {-# LANGUAGE Rank2Types             #-}
 {-# LANGUAGE TypeOperators          #-}
 {-# LANGUAGE DefaultSignatures      #-}
+
+-- Keep GHC quiet.. I wish we can be more specific on these as opposed to module
+-- level turning of redundant-constraints. Note that his is not needed >= GHC 8.0.2
+-- since they removed this constraint from -Wall, see: https://ghc.haskell.org/trac/ghc/ticket/10635
+{-# OPTIONS_GHC -fno-warn-orphans               #-}
+{-# OPTIONS_GHC -fno-warn-redundant-constraints #-}
 
 module Data.SBV.Core.Model (
     Mergeable(..), EqSymbolic(..), OrdSymbolic(..), SDivisible(..), Uninterpreted(..), Metric(..), assertSoft, SIntegral
