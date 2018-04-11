baf72492cbbb6646534788929fd409982795ae19
Levent Erkok <erkokl@gmail.com>
2018-04-10 22:24:14 -0700

    Rework the SQL-injection example

--- Data/SBV/Examples/Strings/SQLInjection.hs
+++ Data/SBV/Examples/Strings/SQLInjection.hs
@@ -6,113 +6,136 @@
 -- Maintainer  :  erkokl@gmail.com
 -- Stability   :  experimental
 --
--- This implements the symbolic evaluation of a language which operates on
+-- Implement the symbolic evaluation of a language which operates on
 -- strings in a way similar to bash. It's possible to do different analyses,
 -- but this example finds program inputs which result in a query containing a
 -- SQL injection.
---
--- The example program (in pseudo-code) @query ("SELECT msg FROM msgs where
--- topicid='" ++ my_topicid ++ "'")@ can be analyzed via:
---
--- >>> findInjection exampleProgram
--- Satisfiable. Model:
--- my_topicid = "h'; DROP TABLE 'users" :: String
--- undef      =                      "" :: String
--- s2         =                      "" :: String
 -----------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings   #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
-module Data.SBV.Examples.Strings.SQLInjection
-  ( findInjection
-  , exampleProgram
-  ) where
+module Data.SBV.Examples.Strings.SQLInjection where
 
 import Control.Monad.State
 import Control.Monad.Writer
-import Data.SBV hiding (name)
 import Data.String
 
-data Expr
-  = Query Expr
-  | Const String
-  | Concat Expr Expr
-  | ReadVar Expr
-  | WriteVar Expr Expr
+import Data.SBV
+import Data.SBV.Control
 
-instance IsString Expr where
+-- | Simple expression language
+data SQLExpr = Query    SQLExpr
+             | Const    String
+             | Concat   SQLExpr SQLExpr
+             | ReadVar  SQLExpr
+             | WriteVar SQLExpr SQLExpr
+
+-- | Literals strings can be lifted to be constant programs
+instance IsString SQLExpr where
   fromString = Const
 
+-- | Evaluation monad. The state argument is the environment to store
+-- variables as we evaluate.
 type M = StateT (SFunArray String String) (WriterT [SString] Symbolic)
 
--- TODO: this is only interesting with ite
-eval :: Expr -> M (SBV String)
-eval expr = case expr of
-  Query q -> do
-    q' <- eval q
-    tell [q']
-    lift $ lift exists_
-  Const str -> pure $ literal str
-  Concat exp1 exp2 -> (.++) <$> eval exp1 <*> eval exp2
-  ReadVar nameExp -> do
-    name <- eval nameExp
-    arr <- get
-    pure $ readArray arr name
-  WriteVar nameExp valExp -> do
-    name <- eval nameExp
-    val  <- eval valExp
-    modify $ \arr -> writeArray arr name val
-    pure val
+-- | Given an expression, symbolically evaluate it
+eval :: SQLExpr -> M SString
+eval (Query q)         = do q' <- eval q
+                            tell [q']
+                            lift $ lift exists_
+eval (Const str)       = return $ literal str
+eval (Concat e1 e2)    = (.++) <$> eval e1 <*> eval e2
+eval (ReadVar nm)      = do n   <- eval nm
+                            arr <- get
+                            return $ readArray arr n
+eval (WriteVar nm val) = do n <- eval nm
+                            v <- eval val
+                            modify $ \arr -> writeArray arr n v
+                            return v
 
--- | A simple program to query all messages with a given topic id.
-exampleProgram :: Expr
-exampleProgram = Query
-  (Concat
-    (Concat
-      "SELECT msg FROM msgs WHERE topicid='"
-      (ReadVar "my_topicid"))
-    "'")
+-- | A simple program to query all messages with a given topic id. In SQL like notation:
+--
+-- @
+--   query ("SELECT msg FROM msgs where topicid='" ++ my_topicid ++ "'")
+-- @
+exampleProgram :: SQLExpr
+exampleProgram = Query $ foldr1 Concat [ "SELECT msg FROM msgs WHERE topicid='"
+                                       , ReadVar "my_topicid"
+                                       , "'"
+                                       ]
 
-nameRe, strRe, selectRe, dropRe, statementRe, exploitRe :: SRegExp
-
--- we have to severely limit the length of names and strings to get an answer
+-- | Limit names to be at most 7 chars long, with simple letters.
+nameRe :: SRegExp
 nameRe = RE_Loop 1 7 (RE_Range 'a' 'z')
+
+-- | Strings: Again, at most of lenght 5, surrounded by quotes.
+strRe :: SRegExp
 strRe = "'" * RE_Loop 1 5 (RE_Range 'a' 'z' + " ") * "'"
 
-selectRe
-  = "SELECT "
-  * (nameRe + "*")
-  * " FROM "
-  * nameRe
-  * RE_Opt (
-    " WHERE "
-    * nameRe
-    * "="
-    * (nameRe + strRe)
-    )
+-- | A "select" command:
+selectRe :: SRegExp
+selectRe = "SELECT "
+         * (nameRe + "*")
+         * " FROM "
+         * nameRe
+         * RE_Opt (  " WHERE "
+                   * nameRe
+                   * "="
+                   * (nameRe + strRe)
+                   )
 
+-- | A "drop" instruction, which can be exploited!
+dropRe :: SRegExp
 dropRe = "DROP TABLE " * (nameRe + strRe)
 
--- we'll simplify and say a command is just a sequence of select / drop.
+-- | We'll greatly simplify here and say a statement is either a select or a drop:
+statementRe :: SRegExp
 statementRe = selectRe + dropRe
 
--- we're looking for a DROP TABLE after at least one legitimate command
-exploitRe
-  = RE_Plus (statementRe * "; ")
-  * "DROP TABLE 'users'"
+-- | The exploit: We're looking for a DROP TABLE after at least one legitimate command.
+exploitRe :: SRegExp
+exploitRe = RE_Plus (statementRe * "; ")
+          * "DROP TABLE 'users'"
 
 -- | Analyze the program for inputs which result in a SQL injection. There are
 -- other possible injections, but in this example we're only looking for a
 -- @DROP TABLE@ command.
-findInjection :: Expr -> IO ()
-findInjection expr = do
-  satResult <- sat $ do
-    my_topicid <- sString "my_topicid"
-    undef      <- sString "undef"
-    let env :: SFunArray String String
-        env = mkSFunArray $ \varName ->
-          ite (varName .== "my_topicid") my_topicid undef
-    (_evalResult, queries) <- runWriterT (evalStateT (eval expr) env)
-    pure $ bAny (`strMatch` exploitRe) queries
+--
+-- Remember that our example program (in pseudo-code) is:
+--
+-- @
+--   query ("SELECT msg FROM msgs where topicid='" ++ my_topicid ++ "'")
+-- @
+--
+-- We have:
+--
+-- >>> findInjection exampleProgram
+-- "h'; DROP TABLE 'users"
+--
+-- Indeed, if we substitute the suggested string, we get the program:
+--
+-- @
+--   query ("SELECT msg FROM msgs where topicid='h'; DROP TABLE 'users'")
+-- @
+--
+-- which would query for topic 'h' and then delete the users table!
+findInjection :: SQLExpr -> IO String
+findInjection expr = runSMT $ do
+    badTopic <- sString "badTopic"
 
-  print satResult
+    -- Create an initial environment that returns the symbolic
+    -- value my_topicid only, and undefined for all other variables
+    undef      <- sString "uninitialized"
+    let env :: SFunArray String String
+        env = mkSFunArray $ \varName -> ite (varName .== "my_topicid") badTopic undef
+
+    (_, queries) <- runWriterT (evalStateT (eval expr) env)
+
+    -- For all the queries thus generated, ask that one of them be "explotiable"
+    constrain $ bAny (`strMatch` exploitRe) queries
+
+    query $ do cs <- checkSat
+               case cs of
+                 Unk   -> error "Solver returned unknown!"
+                 Unsat -> error "No exploits are found"
+                 Sat   -> getValue badTopic
