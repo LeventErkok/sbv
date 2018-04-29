d547a11dd3c986adb8d2a9f7492d8c9ea6bbdb17
Levent Erkok <erkokl@gmail.com>
2018-04-29 16:24:52 -0700

    Rename functions here to avoid name clashes

--- SBVTestSuite/TestSuite/Queries/Strings.hs
+++ SBVTestSuite/TestSuite/Queries/Strings.hs
@@ -24,33 +24,33 @@ import Utils.SBVTestFramework
 tests :: TestTree
 tests =
   testGroup "Basics.QueryStrings"
-    [ goldenCapturedIO "query_Strings1" $ testQuery qs1
-    , goldenCapturedIO "query_Chars1"   $ testQuery qc1
+    [ goldenCapturedIO "query_Strings1" $ testQuery queryString1
+    , goldenCapturedIO "query_Chars1"   $ testQuery queryChar1
     ]
 
 testQuery :: Show a => Symbolic a -> FilePath -> IO ()
 testQuery t rf = do r <- runSMTWith defaultSMTCfg{verbose=True, redirectVerbose=Just rf} t
                     appendFile rf ("\nFINAL OUTPUT:\n" ++ show r ++ "\n")
 
-qs1 :: Symbolic [String]
-qs1 = do a <- sString "a"
+queryString1 :: Symbolic [String]
+queryString1 = do a <- sString "a"
 
-         constrain $ a `R.match` R.Loop 5 5 "xyz"
+                  constrain $ a `R.match` R.Loop 5 5 "xyz"
 
-         query $ do _ <- checkSat
-                    s <- getValue a
-                    if s == concat (replicate 5 "xyz")
-                       then return [s]
-                       else error $ "Didn't expect this: " ++ show s
+                  query $ do _ <- checkSat
+                             s <- getValue a
+                             if s == concat (replicate 5 "xyz")
+                                then return [s]
+                                else error $ "Didn't expect this: " ++ show s
 
-qc1 :: Symbolic Char
-qc1 = do a <- sChar "a"
+queryChar1 :: Symbolic Char
+queryChar1 = do a <- sChar "a"
 
-         constrain $ C.ord a .>= 65
-         constrain $ C.ord a .<  66
+                constrain $ C.ord a .>= 65
+                constrain $ C.ord a .<  66
 
-         query $ do _ <- checkSat
-                    s <- getValue a
-                    if s == 'A'
-                       then return s
-                       else error $ "Didn't expect this: " ++ show s
+                query $ do _ <- checkSat
+                           s <- getValue a
+                           if s == 'A'
+                              then return s
+                              else error $ "Didn't expect this: " ++ show s
