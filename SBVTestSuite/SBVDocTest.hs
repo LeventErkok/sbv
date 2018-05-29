49204902d35d42b192667693a70aa6b11b7b8d28
Levent Erkok <erkokl@gmail.com>
2018-05-28 18:00:06 -0700

    Provisions for running local doctests

--- SBVTestSuite/SBVDocTest.hs
+++ SBVTestSuite/SBVDocTest.hs
@@ -16,26 +16,31 @@ main = do (testEnv, testPercentage) <- getTestEnvironment
           putStrLn $ "SBVDocTest: Test platform: " ++ show testEnv
 
           case testEnv of
-            TestEnvLocal   -> runDocTest False
+            TestEnvLocal   -> runDocTest False False
             TestEnvCI env  -> if testPercentage < 50
                               then do putStrLn $ "Test percentage below tresheold, skipping doctest: " ++ show testPercentage
                                       exitSuccess
-                              else runDocTest (env == CIWindows)
+                              else runDocTest (env == CIWindows) True
             TestEnvUnknown  -> do putStrLn "Unknown test environment, skipping doctests"
                                   exitSuccess
 
- where runDocTest windowsSkip = do srcFiles <- glob "Data/SBV/**/*.hs"
-                                   docFiles <- glob "Documentation/SBV/**/*.hs"
+ where runDocTest onWindows onRemote = do srcFiles <- glob "Data/SBV/**/*.hs"
+                                          docFiles <- glob "Documentation/SBV/**/*.hs"
 
-                                   let allFiles = srcFiles ++ docFiles
-                                       testFiles
-                                         | windowsSkip = filter (not . bad) allFiles
-                                         | True        = allFiles
-                                       args = ["--fast", "--no-magic"]
+                                          let allFiles  = srcFiles ++ docFiles
+                                              testFiles = filter (\nm -> not (skipWindows nm || skipRemote nm)) allFiles
 
-                                   doctest $ args ++ testFiles
+                                              args = ["--fast", "--no-magic"]
 
-       -- The following test has a path encoded in its output, and hence fails on Windows
-       -- since it has the c:\blah\blah format. Skip it:
-       bad fn = "nodiv0.hs" `isSuffixOf` map toLower fn
+                                          doctest $ args ++ testFiles
 
+         where skipWindows nm
+                 | not onWindows = False
+                 | True          = -- The following test has a path encoded in its output, and hence fails on Windows
+                                   -- since it has the c:\blah\blah format. Skip it:
+                                   "nodiv0.hs" `isSuffixOf` map toLower nm
+
+               skipRemote nm
+                 | not onRemote = False
+                 | True         = -- The following test requires mathSAT, so can't run on remote
+                                  "interpolants.hs" `isSuffixOf` map toLower nm
