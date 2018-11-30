42d656e2104c6b00759b452f62753ed254c91dda
Joel Burget <joelburget@gmail.com>
2018-11-30 14:30:50 -0800

    Experimental monadic bounded list operations.

--- Data/SBV/List/Bounded.hs
+++ Data/SBV/List/Bounded.hs
@@ -14,13 +14,15 @@
 -- bounded prefix of this list, at which point these functions come in handy.
 -----------------------------------------------------------------------------
 --
-{-# LANGUAGE OverloadedLists #-}
+{-# LANGUAGE OverloadedLists     #-}
+{-# LANGUAGE Rank2Types          #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Data.SBV.List.Bounded (
      -- * General folds
-     bfoldr, bfoldl
+     bfoldr, bfoldrM, bfoldl, bfoldlM
      -- * Map, filter, zipWith, elem
-   , bmap, bfilter, bzipWith, belem
+   , bmap, bmapM, bfilter, bzipWith, belem
      -- * Aggregates
    , bsum, bprod, band, bor, bany, ball, bmaximum, bminimum
      -- * Miscellaneous: Reverse and sort
@@ -42,11 +44,29 @@ bfoldr cnt f b = go (cnt `max` 0)
   where go 0 _ = b
         go i s = lcase s b (\h t -> h `f` go (i-1) t)
 
+-- | Bounded monadic fold from the right.
+bfoldrM
+  :: forall a b m. (SymWord a, SymWord b, Monad m, Mergeable (m (SBV b)))
+  => Int -> (SBV a -> SBV b -> m (SBV b)) -> SBV b -> SList a -> m (SBV b)
+bfoldrM cnt f b = go (cnt `max` 0)
+  where go :: Int -> SList a -> m (SBV b)
+        go 0 _ = return b
+        go i s = lcase s (return b) (\h t -> f h =<< go (i-1) t)
+
 -- | Bounded fold from the left.
 bfoldl :: (SymWord a, SymWord b) => Int -> (SBV b -> SBV a -> SBV b) -> SBV b -> SList a -> SBV b
 bfoldl cnt f = go (cnt `max` 0)
   where go 0 b _ = b
         go i b s = lcase s b (\h t -> go (i-1) (b `f` h) t)
+
+-- | Bounded monadic fold from the left.
+bfoldlM
+  :: forall a b m. (SymWord a, SymWord b, Monad m, Mergeable (m (SBV b)))
+  => Int -> (SBV b -> SBV a -> m (SBV b)) -> SBV b -> SList a -> m (SBV b)
+bfoldlM cnt f = go (cnt `max` 0)
+  where go :: Int -> SBV b -> SList a -> m (SBV b)
+        go 0 b _ = return b
+        go i b s = lcase s (return b) (\h t -> do { fbh <- f b h; go (i-1) fbh t })
 
 -- | Bounded sum.
 bsum :: (SymWord a, Num a) => Int -> SList a -> SBV a
@@ -59,6 +79,12 @@ bprod i = bfoldl i (*) 1
 -- | Bounded map.
 bmap :: (SymWord a, SymWord b) => Int -> (SBV a -> SBV b) -> SList a -> SList b
 bmap i f = bfoldr i (\x -> (f x .:)) []
+
+-- | Bounded monadic map.
+bmapM
+  :: (SymWord a, SymWord b, Monad m, Mergeable (m (SBV [b])))
+  => Int -> (SBV a -> m (SBV b)) -> SList a -> m (SList b)
+bmapM i f = bfoldrM i (\a bs -> (.:) <$> f a <*> pure bs) []
 
 -- | Bounded filter.
 bfilter :: SymWord a => Int -> (SBV a -> SBool) -> SList a -> SList a
