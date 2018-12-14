882efa04e09972306d67ac4b51648c7ad14c0825
Levent Erkok <erkokl@gmail.com>
2018-12-14 08:54:41 -0800

    Match z3 output here

--- Data/SBV/String.hs
+++ Data/SBV/String.hs
@@ -305,8 +305,8 @@ replace s src dst
 -- Q.E.D.
 -- >>> prove $ \s i -> i .> 0 &&& i .< length s ==> indexOf s (subStr s i 1) .== i
 -- Falsifiable. Counter-example:
---   s0 = "\NUL\NUL\NUL\NUL\NUL" :: String
---   s1 =                      3 :: Integer
+--   s0 = " \NUL\NUL\NUL\NUL\NUL" :: String
+--   s1 =                       3 :: Integer
 -- >>> prove $ \s1 s2 -> length s2 .> length s1 ==> indexOf s1 s2 .== -1
 -- Q.E.D.
 indexOf :: SString -> SString -> SInteger
