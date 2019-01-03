211a52cccb9f4ca9ea13f94ed63983ee3a99e1c1
Levent Erkok <erkokl@gmail.com>
2019-01-03 14:02:38 -0800

    Remove the export of symbolicFieldAccess

--- Data/SBV/Tuple.hs
+++ Data/SBV/Tuple.hs
@@ -13,7 +13,7 @@ module Data.SBV.Tuple (
   -- * Natural numbers
     Nat(..), SNat(..)
   -- * Field access
-  , field, field1, field2, field3, field4, field5, field6, field7, field8, symbolicFieldAccess
+  , field, field1, field2, field3, field4, field5, field6, field7, field8
   ) where
 
 import Data.SBV.Core.Data hiding (StrOp(..))
