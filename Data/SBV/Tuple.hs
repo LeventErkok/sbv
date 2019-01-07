b2d15fff8b36f12b63daf6a43cd533f7c1fbf193
Levent Erkok <erkokl@gmail.com>
2019-01-06 21:17:39 -0800

    Tuples: Add tuple function, reverse of untuple

--- Data/SBV/Tuple.hs
+++ Data/SBV/Tuple.hs
@@ -19,8 +19,8 @@
 module Data.SBV.Tuple (
   -- * Symbolic field access
     (^.), _1, _2, _3, _4, _5, _6, _7, _8
-  -- * Untupling
-  , untuple
+  -- * Tupling and untupling
+  , tuple, untuple
   ) where
 
 import GHC.TypeLits
@@ -39,7 +39,7 @@ infixl 8 ^.
 
 -- | Dynamic interface to exporting tuples, this function is not
 -- exported on purpose; use it only via the field functions '_1', '_2', etc.
-symbolicFieldAccess :: SymVal a => Int -> SBV tup -> SBV a
+symbolicFieldAccess :: (SymVal a, HasKind tup) => Int -> SBV tup -> SBV a
 symbolicFieldAccess i tup
   | 1 > i || i > lks
   = bad $ "Index is out of bounds, " ++ show i ++ " is outside [1," ++ show lks ++ "]"
@@ -76,51 +76,51 @@ symbolicFieldAccess i tup
 data Label (l :: Symbol) = Get
 
 -- | The class 'HasField' captures the notion that a type has a certain field
-class SymVal elt => HasField l elt tup | l tup -> elt where
+class (SymVal elt, HasKind tup) => HasField l elt tup | l tup -> elt where
   field :: Label l -> SBV tup -> SBV elt
 
-instance SymVal a => HasField "_1" a (a, b)                   where field _ = symbolicFieldAccess 1
-instance SymVal a => HasField "_1" a (a, b, c)                where field _ = symbolicFieldAccess 1
-instance SymVal a => HasField "_1" a (a, b, c, d)             where field _ = symbolicFieldAccess 1
-instance SymVal a => HasField "_1" a (a, b, c, d, e)          where field _ = symbolicFieldAccess 1
-instance SymVal a => HasField "_1" a (a, b, c, d, e, f)       where field _ = symbolicFieldAccess 1
-instance SymVal a => HasField "_1" a (a, b, c, d, e, f, g)    where field _ = symbolicFieldAccess 1
-instance SymVal a => HasField "_1" a (a, b, c, d, e, f, g, h) where field _ = symbolicFieldAccess 1
+instance (HasKind a, HasKind b                                                                  , SymVal a) => HasField "_1" a (a, b)                   where field _ = symbolicFieldAccess 1
+instance (HasKind a, HasKind b, HasKind c                                                       , SymVal a) => HasField "_1" a (a, b, c)                where field _ = symbolicFieldAccess 1
+instance (HasKind a, HasKind b, HasKind c, HasKind d                                            , SymVal a) => HasField "_1" a (a, b, c, d)             where field _ = symbolicFieldAccess 1
+instance (HasKind a, HasKind b, HasKind c, HasKind d, HasKind e                                 , SymVal a) => HasField "_1" a (a, b, c, d, e)          where field _ = symbolicFieldAccess 1
+instance (HasKind a, HasKind b, HasKind c, HasKind d, HasKind e, HasKind f                      , SymVal a) => HasField "_1" a (a, b, c, d, e, f)       where field _ = symbolicFieldAccess 1
+instance (HasKind a, HasKind b, HasKind c, HasKind d, HasKind e, HasKind f, HasKind g           , SymVal a) => HasField "_1" a (a, b, c, d, e, f, g)    where field _ = symbolicFieldAccess 1
+instance (HasKind a, HasKind b, HasKind c, HasKind d, HasKind e, HasKind f, HasKind g, HasKind h, SymVal a) => HasField "_1" a (a, b, c, d, e, f, g, h) where field _ = symbolicFieldAccess 1
 
-instance SymVal b => HasField "_2" b (a, b)                   where field _ = symbolicFieldAccess 2
-instance SymVal b => HasField "_2" b (a, b, c)                where field _ = symbolicFieldAccess 2
-instance SymVal b => HasField "_2" b (a, b, c, d)             where field _ = symbolicFieldAccess 2
-instance SymVal b => HasField "_2" b (a, b, c, d, e)          where field _ = symbolicFieldAccess 2
-instance SymVal b => HasField "_2" b (a, b, c, d, e, f)       where field _ = symbolicFieldAccess 2
-instance SymVal b => HasField "_2" b (a, b, c, d, e, f, g)    where field _ = symbolicFieldAccess 2
-instance SymVal b => HasField "_2" b (a, b, c, d, e, f, g, h) where field _ = symbolicFieldAccess 2
+instance (HasKind a, HasKind b                                                                  , SymVal b) => HasField "_2" b (a, b)                   where field _ = symbolicFieldAccess 2
+instance (HasKind a, HasKind b, HasKind c                                                       , SymVal b) => HasField "_2" b (a, b, c)                where field _ = symbolicFieldAccess 2
+instance (HasKind a, HasKind b, HasKind c, HasKind d                                            , SymVal b) => HasField "_2" b (a, b, c, d)             where field _ = symbolicFieldAccess 2
+instance (HasKind a, HasKind b, HasKind c, HasKind d, HasKind e                                 , SymVal b) => HasField "_2" b (a, b, c, d, e)          where field _ = symbolicFieldAccess 2
+instance (HasKind a, HasKind b, HasKind c, HasKind d, HasKind e, HasKind f                      , SymVal b) => HasField "_2" b (a, b, c, d, e, f)       where field _ = symbolicFieldAccess 2
+instance (HasKind a, HasKind b, HasKind c, HasKind d, HasKind e, HasKind f, HasKind g           , SymVal b) => HasField "_2" b (a, b, c, d, e, f, g)    where field _ = symbolicFieldAccess 2
+instance (HasKind a, HasKind b, HasKind c, HasKind d, HasKind e, HasKind f, HasKind g, HasKind h, SymVal b) => HasField "_2" b (a, b, c, d, e, f, g, h) where field _ = symbolicFieldAccess 2
 
-instance SymVal c => HasField "_3" c (a, b, c)                where field _ = symbolicFieldAccess 3
-instance SymVal c => HasField "_3" c (a, b, c, d)             where field _ = symbolicFieldAccess 3
-instance SymVal c => HasField "_3" c (a, b, c, d, e)          where field _ = symbolicFieldAccess 3
-instance SymVal c => HasField "_3" c (a, b, c, d, e, f)       where field _ = symbolicFieldAccess 3
-instance SymVal c => HasField "_3" c (a, b, c, d, e, f, g)    where field _ = symbolicFieldAccess 3
-instance SymVal c => HasField "_3" c (a, b, c, d, e, f, g, h) where field _ = symbolicFieldAccess 3
+instance (HasKind a, HasKind b, HasKind c                                                       , SymVal c) => HasField "_3" c (a, b, c)                where field _ = symbolicFieldAccess 3
+instance (HasKind a, HasKind b, HasKind c, HasKind d                                            , SymVal c) => HasField "_3" c (a, b, c, d)             where field _ = symbolicFieldAccess 3
+instance (HasKind a, HasKind b, HasKind c, HasKind d, HasKind e                                 , SymVal c) => HasField "_3" c (a, b, c, d, e)          where field _ = symbolicFieldAccess 3
+instance (HasKind a, HasKind b, HasKind c, HasKind d, HasKind e, HasKind f                      , SymVal c) => HasField "_3" c (a, b, c, d, e, f)       where field _ = symbolicFieldAccess 3
+instance (HasKind a, HasKind b, HasKind c, HasKind d, HasKind e, HasKind f, HasKind g           , SymVal c) => HasField "_3" c (a, b, c, d, e, f, g)    where field _ = symbolicFieldAccess 3
+instance (HasKind a, HasKind b, HasKind c, HasKind d, HasKind e, HasKind f, HasKind g, HasKind h, SymVal c) => HasField "_3" c (a, b, c, d, e, f, g, h) where field _ = symbolicFieldAccess 3
 
-instance SymVal d => HasField "_4" d (a, b, c, d)             where field _ = symbolicFieldAccess 4
-instance SymVal d => HasField "_4" d (a, b, c, d, e)          where field _ = symbolicFieldAccess 4
-instance SymVal d => HasField "_4" d (a, b, c, d, e, f)       where field _ = symbolicFieldAccess 4
-instance SymVal d => HasField "_4" d (a, b, c, d, e, f, g)    where field _ = symbolicFieldAccess 4
-instance SymVal d => HasField "_4" d (a, b, c, d, e, f, g, h) where field _ = symbolicFieldAccess 4
+instance (HasKind a, HasKind b, HasKind c, HasKind d                                            , SymVal d) => HasField "_4" d (a, b, c, d)             where field _ = symbolicFieldAccess 4
+instance (HasKind a, HasKind b, HasKind c, HasKind d, HasKind e                                 , SymVal d) => HasField "_4" d (a, b, c, d, e)          where field _ = symbolicFieldAccess 4
+instance (HasKind a, HasKind b, HasKind c, HasKind d, HasKind e, HasKind f                      , SymVal d) => HasField "_4" d (a, b, c, d, e, f)       where field _ = symbolicFieldAccess 4
+instance (HasKind a, HasKind b, HasKind c, HasKind d, HasKind e, HasKind f, HasKind g           , SymVal d) => HasField "_4" d (a, b, c, d, e, f, g)    where field _ = symbolicFieldAccess 4
+instance (HasKind a, HasKind b, HasKind c, HasKind d, HasKind e, HasKind f, HasKind g, HasKind h, SymVal d) => HasField "_4" d (a, b, c, d, e, f, g, h) where field _ = symbolicFieldAccess 4
 
-instance SymVal e => HasField "_5" e (a, b, c, d, e)          where field _ = symbolicFieldAccess 5
-instance SymVal e => HasField "_5" e (a, b, c, d, e, f)       where field _ = symbolicFieldAccess 5
-instance SymVal e => HasField "_5" e (a, b, c, d, e, f, g)    where field _ = symbolicFieldAccess 5
-instance SymVal e => HasField "_5" e (a, b, c, d, e, f, g, h) where field _ = symbolicFieldAccess 5
+instance (HasKind a, HasKind b, HasKind c, HasKind d, HasKind e                                 , SymVal e) => HasField "_5" e (a, b, c, d, e)          where field _ = symbolicFieldAccess 5
+instance (HasKind a, HasKind b, HasKind c, HasKind d, HasKind e, HasKind f                      , SymVal e) => HasField "_5" e (a, b, c, d, e, f)       where field _ = symbolicFieldAccess 5
+instance (HasKind a, HasKind b, HasKind c, HasKind d, HasKind e, HasKind f, HasKind g           , SymVal e) => HasField "_5" e (a, b, c, d, e, f, g)    where field _ = symbolicFieldAccess 5
+instance (HasKind a, HasKind b, HasKind c, HasKind d, HasKind e, HasKind f, HasKind g, HasKind h, SymVal e) => HasField "_5" e (a, b, c, d, e, f, g, h) where field _ = symbolicFieldAccess 5
 
-instance SymVal f => HasField "_6" f (a, b, c, d, e, f)       where field _ = symbolicFieldAccess 6
-instance SymVal f => HasField "_6" f (a, b, c, d, e, f, g)    where field _ = symbolicFieldAccess 6
-instance SymVal f => HasField "_6" f (a, b, c, d, e, f, g, h) where field _ = symbolicFieldAccess 6
+instance (HasKind a, HasKind b, HasKind c, HasKind d, HasKind e, HasKind f                      , SymVal f) => HasField "_6" f (a, b, c, d, e, f)       where field _ = symbolicFieldAccess 6
+instance (HasKind a, HasKind b, HasKind c, HasKind d, HasKind e, HasKind f, HasKind g           , SymVal f) => HasField "_6" f (a, b, c, d, e, f, g)    where field _ = symbolicFieldAccess 6
+instance (HasKind a, HasKind b, HasKind c, HasKind d, HasKind e, HasKind f, HasKind g, HasKind h, SymVal f) => HasField "_6" f (a, b, c, d, e, f, g, h) where field _ = symbolicFieldAccess 6
 
-instance SymVal g => HasField "_7" g (a, b, c, d, e, f, g)    where field _ = symbolicFieldAccess 7
-instance SymVal g => HasField "_7" g (a, b, c, d, e, f, g, h) where field _ = symbolicFieldAccess 7
+instance (HasKind a, HasKind b, HasKind c, HasKind d, HasKind e, HasKind f, HasKind g           , SymVal g) => HasField "_7" g (a, b, c, d, e, f, g)    where field _ = symbolicFieldAccess 7
+instance (HasKind a, HasKind b, HasKind c, HasKind d, HasKind e, HasKind f, HasKind g, HasKind h, SymVal g) => HasField "_7" g (a, b, c, d, e, f, g, h) where field _ = symbolicFieldAccess 7
 
-instance SymVal h => HasField "_8" h (a, b, c, d, e, f, g, h) where field _ = symbolicFieldAccess 8
+instance (HasKind a, HasKind b, HasKind c, HasKind d, HasKind e, HasKind f, HasKind g, HasKind h, SymVal h) => HasField "_8" h (a, b, c, d, e, f, g, h) where field _ = symbolicFieldAccess 8
 
 -- | Access the 1st element of an @STupleN@, @2 <= N <= 8@. Also see '^.'.
 _1 :: HasField "_1" b a => SBV a -> SBV b
@@ -154,28 +154,121 @@ _7 = field (Get :: Label "_7")
 _8 :: HasField "_8" b a => SBV a -> SBV b
 _8 = field (Get :: Label "_8")
 
--- | Unconstructing a tuple to its parts
-class Untuple tup a | tup -> a where
+-- | Constructing a tuple from its parts and deconstructing back
+class Tuple tup a | tup -> a where
   -- | Deconstruct a tuple, getting its constituent parts apart.
   untuple :: SBV tup -> a
+  -- | Constructing a tuple from its parts
+  tuple   :: a -> SBV tup
 
-instance (SymVal a, SymVal b) => Untuple (a, b) (SBV a, SBV b) where
+instance (SymVal a, SymVal b, SymVal (a, b)) => Tuple (a, b) (SBV a, SBV b) where
   untuple p = (p^._1, p^._2)
 
-instance (SymVal a, SymVal b, SymVal c) => Untuple (a, b, c) (SBV a, SBV b, SBV c) where
+  tuple p@(sa, sb)
+    | Just a <- unliteral sa, Just b <- unliteral sb
+    = literal (a, b)
+    | True
+    = SBV $ SVal k $ Right $ cache res
+    where k      = kindOf p
+          res st = do asv <- sbvToSV st sa
+                      bsv <- sbvToSV st sb
+                      newExpr st k (SBVApp (TupleConstructor 2) [asv, bsv])
+
+instance (SymVal a, SymVal b, SymVal c, SymVal (a, b, c)) => Tuple (a, b, c) (SBV a, SBV b, SBV c) where
   untuple p = (p^._1, p^._2, p^._3)
 
-instance (SymVal a, SymVal b, SymVal c, SymVal d) => Untuple (a, b, c, d) (SBV a, SBV b, SBV c, SBV d) where
+  tuple p@(sa, sb, sc)
+    | Just a <- unliteral sa, Just b <- unliteral sb, Just c <- unliteral sc
+    = literal (a, b, c)
+    | True
+    = SBV $ SVal k $ Right $ cache res
+    where k      = kindOf p
+          res st = do asv <- sbvToSV st sa
+                      bsv <- sbvToSV st sb
+                      csv <- sbvToSV st sc
+                      newExpr st k (SBVApp (TupleConstructor 3) [asv, bsv, csv])
+
+instance (SymVal a, SymVal b, SymVal c, SymVal d, SymVal (a, b, c, d)) => Tuple (a, b, c, d) (SBV a, SBV b, SBV c, SBV d) where
   untuple p = (p^._1, p^._2, p^._3, p^._4)
 
-instance (SymVal a, SymVal b, SymVal c, SymVal d, SymVal e) => Untuple (a, b, c, d, e) (SBV a, SBV b, SBV c, SBV d, SBV e) where
+  tuple p@(sa, sb, sc, sd)
+    | Just a <- unliteral sa, Just b <- unliteral sb, Just c <- unliteral sc, Just d <- unliteral sd
+    = literal (a, b, c, d)
+    | True
+    = SBV $ SVal k $ Right $ cache res
+    where k      = kindOf p
+          res st = do asv <- sbvToSV st sa
+                      bsv <- sbvToSV st sb
+                      csv <- sbvToSV st sc
+                      dsv <- sbvToSV st sd
+                      newExpr st k (SBVApp (TupleConstructor 4) [asv, bsv, csv, dsv])
+
+instance (SymVal a, SymVal b, SymVal c, SymVal d, SymVal e, SymVal (a, b, c, d, e)) => Tuple (a, b, c, d, e) (SBV a, SBV b, SBV c, SBV d, SBV e) where
   untuple p = (p^._1, p^._2, p^._3, p^._4, p^._5)
 
-instance (SymVal a, SymVal b, SymVal c, SymVal d, SymVal e, SymVal f) => Untuple (a, b, c, d, e, f) (SBV a, SBV b, SBV c, SBV d, SBV e, SBV f) where
+  tuple p@(sa, sb, sc, sd, se)
+    | Just a <- unliteral sa, Just b <- unliteral sb, Just c <- unliteral sc, Just d <- unliteral sd, Just e <- unliteral se
+    = literal (a, b, c, d, e)
+    | True
+    = SBV $ SVal k $ Right $ cache res
+    where k      = kindOf p
+          res st = do asv <- sbvToSV st sa
+                      bsv <- sbvToSV st sb
+                      csv <- sbvToSV st sc
+                      dsv <- sbvToSV st sd
+                      esv <- sbvToSV st se
+                      newExpr st k (SBVApp (TupleConstructor 5) [asv, bsv, csv, dsv, esv])
+
+instance (SymVal a, SymVal b, SymVal c, SymVal d, SymVal e, SymVal f, SymVal (a, b, c, d, e, f)) => Tuple (a, b, c, d, e, f) (SBV a, SBV b, SBV c, SBV d, SBV e, SBV f) where
   untuple p = (p^._1, p^._2, p^._3, p^._4, p^._5, p^._6)
 
-instance (SymVal a, SymVal b, SymVal c, SymVal d, SymVal e, SymVal f, SymVal g) => Untuple (a, b, c, d, e, f, g) (SBV a, SBV b, SBV c, SBV d, SBV e, SBV f, SBV g) where
+  tuple p@(sa, sb, sc, sd, se, sf)
+    | Just a <- unliteral sa, Just b <- unliteral sb, Just c <- unliteral sc, Just d <- unliteral sd, Just e <- unliteral se, Just f <- unliteral sf
+    = literal (a, b, c, d, e, f)
+    | True
+    = SBV $ SVal k $ Right $ cache res
+    where k      = kindOf p
+          res st = do asv <- sbvToSV st sa
+                      bsv <- sbvToSV st sb
+                      csv <- sbvToSV st sc
+                      dsv <- sbvToSV st sd
+                      esv <- sbvToSV st se
+                      fsv <- sbvToSV st sf
+                      newExpr st k (SBVApp (TupleConstructor 6) [asv, bsv, csv, dsv, esv, fsv])
+
+instance (SymVal a, SymVal b, SymVal c, SymVal d, SymVal e, SymVal f, SymVal g, SymVal (a, b, c, d, e, f, g)) => Tuple (a, b, c, d, e, f, g) (SBV a, SBV b, SBV c, SBV d, SBV e, SBV f, SBV g) where
   untuple p = (p^._1, p^._2, p^._3, p^._4, p^._5, p^._6, p^._7)
 
-instance (SymVal a, SymVal b, SymVal c, SymVal d, SymVal e, SymVal f, SymVal g, SymVal h) => Untuple (a, b, c, d, e, f, g, h) (SBV a, SBV b, SBV c, SBV d, SBV e, SBV f, SBV g, SBV h) where
+  tuple p@(sa, sb, sc, sd, se, sf, sg)
+    | Just a <- unliteral sa, Just b <- unliteral sb, Just c <- unliteral sc, Just d <- unliteral sd, Just e <- unliteral se, Just f <- unliteral sf, Just g <- unliteral sg
+    = literal (a, b, c, d, e, f, g)
+    | True
+    = SBV $ SVal k $ Right $ cache res
+    where k      = kindOf p
+          res st = do asv <- sbvToSV st sa
+                      bsv <- sbvToSV st sb
+                      csv <- sbvToSV st sc
+                      dsv <- sbvToSV st sd
+                      esv <- sbvToSV st se
+                      fsv <- sbvToSV st sf
+                      gsv <- sbvToSV st sg
+                      newExpr st k (SBVApp (TupleConstructor 7) [asv, bsv, csv, dsv, esv, fsv, gsv])
+
+instance (SymVal a, SymVal b, SymVal c, SymVal d, SymVal e, SymVal f, SymVal g, SymVal h, SymVal (a, b, c, d, e, f, g, h)) => Tuple (a, b, c, d, e, f, g, h) (SBV a, SBV b, SBV c, SBV d, SBV e, SBV f, SBV g, SBV h) where
   untuple p = (p^._1, p^._2, p^._3, p^._4, p^._5, p^._6, p^._7, p^._8)
+
+  tuple p@(sa, sb, sc, sd, se, sf, sg, sh)
+    | Just a <- unliteral sa, Just b <- unliteral sb, Just c <- unliteral sc, Just d <- unliteral sd, Just e <- unliteral se, Just f <- unliteral sf, Just g <- unliteral sg, Just h <- unliteral sh
+    = literal (a, b, c, d, e, f, g, h)
+    | True
+    = SBV $ SVal k $ Right $ cache res
+    where k      = kindOf p
+          res st = do asv <- sbvToSV st sa
+                      bsv <- sbvToSV st sb
+                      csv <- sbvToSV st sc
+                      dsv <- sbvToSV st sd
+                      esv <- sbvToSV st se
+                      fsv <- sbvToSV st sf
+                      gsv <- sbvToSV st sg
+                      hsv <- sbvToSV st sh
+                      newExpr st k (SBVApp (TupleConstructor 8) [asv, bsv, csv, dsv, esv, fsv, gsv, hsv])
