eaf3ce3fd2b6758d0e1ed6ab536bf3ff14678c26
Levent Erkok <erkokl@gmail.com>
2019-01-28 22:13:56 -0800

    Add some use cases to WP/Basics example

--- Documentation/SBV/Examples/WeakestPreconditions/Basics.hs
+++ Documentation/SBV/Examples/WeakestPreconditions/Basics.hs
@@ -68,9 +68,10 @@ algorithm before after = Seq [ before
                              , after
                              ]
 
--- | Precondition for our program. Nothing.
+-- | Precondition for our program. Strictly speaking, we don't really need any preconditions,
+-- but for example purposes, we'll require @x@ to be non-negative.
 pre :: I -> SBool
-pre _ = sTrue
+pre IncS{x} = x .>= 0
 
 -- | Postcondition for our program: @y@ must @x+1@.
 post :: I -> SBool
@@ -93,3 +94,61 @@ imperativeInc before after = Program { precondition  = pre
 -- Q.E.D.
 correctness :: Stmt I -> Stmt I -> IO (ProofResult (IncS Integer))
 correctness before after = wpProveWith defaultWPCfg{wpVerbose=True} (imperativeInc before after)
+
+-- * Example proof attempts
+--
+-- $examples
+
+{- $examples
+It is instructive to look at how the proof changes as we put in different @pre@ and @post@ values.
+
+== Violating the post condition
+
+If we stick in an extra increment for @y@ after, we can easily break the postcondition:
+
+>>> import Control.Monad (void)
+>>> void $ correctness Skip $ Assign $ \st@IncS{y} -> st{y = y+1}
+Following proof obligation failed:
+==================================
+  Post condition fails:
+    Start: IncS {x = 0, y = 0}
+    End  : IncS {x = 0, y = 2}
+
+We're told that the program ends up in a state where @x=0@ and @y=2@, violating the requirement @y=x+1@, as expected.
+
+== Using 'assert'
+
+There are two main use cases for 'assert', which merely ends up being a call to 'Abort'.
+One is making sure the inputs are well formed. And the other is the user putting in their
+own invariants into the code.
+
+Let's assume that we only want to accept strictly positive values of @x@. We can try:
+
+>>> void $ correctness (assert "x > 0" (\st@IncS{x} -> x .> 0)) Skip
+Following proof obligation failed:
+==================================
+  Abort "x > 0" condition is satisfiable:
+    Before: IncS {x = 0, y = 0}
+    After : IncS {x = 0, y = 0}
+
+Recall that our precondition ('pre') required @x@ to be non-negative. So, we can put in something weaker and it would be fine:
+
+>>> void $ correctness (assert "x > -5" (\st@IncS{x} -> x .> -5)) Skip
+Total correctness is established.
+
+In this case the precondition to our program ensures that the 'assert' will always be satisfied.
+
+As another example, let us put a post assertion that @y@ is even:
+
+>>> void $ correctness Skip (assert "y is even" (\st@IncS{y} -> y `sMod` 2 .== 0))
+Following proof obligation failed:
+==================================
+  Abort "y is even" condition is satisfiable:
+    Before: IncS {x = 0, y = 0}
+    After : IncS {x = 0, y = 1}
+
+It is important to emphasize that you can put whatever invariant you might want:
+
+>>> void $ correctness Skip (assert "y > x" (\st@IncS{x, y} -> y .> x))
+Total correctness is established.
+-}
