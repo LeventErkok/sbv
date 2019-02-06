df72ba5d18d4f761cfa3d021fcc708d4814176f0
Levent Erkok <levent.erkok@intel.com>
2019-02-06 10:11:48 -0800

    Fix tester failure

--- SBVTestSuite/Utils/SBVTestFramework.hs
+++ SBVTestSuite/Utils/SBVTestFramework.hs
@@ -228,8 +228,8 @@ qc1 nm opC opS = [cf, sm]
                                    ]
 
                             model = case result of
-                                      Right v -> showModel defaultSMTCfg (SMTModel [] (vals ++ [getCV "Result" (literal v)]))
-                                      Left  e -> showModel defaultSMTCfg (SMTModel [] vals) ++ "\n" ++ e
+                                      Right v -> showModel defaultSMTCfg (SMTModel [] (vals ++ [getCV "Result" (literal v)]) [])
+                                      Left  e -> showModel defaultSMTCfg (SMTModel [] vals []) ++ "\n" ++ e
 
                         QC.monitor (QC.counterexample model)
 
@@ -286,8 +286,8 @@ qc2 nm opC opS = [cf, sm]
                                    ]
 
                             model = case result of
-                                      Right v -> showModel defaultSMTCfg (SMTModel [] (vals ++ [getCV "Result" (literal v)]))
-                                      Left  e -> showModel defaultSMTCfg (SMTModel [] vals) ++ "\n" ++ e
+                                      Right v -> showModel defaultSMTCfg (SMTModel [] (vals ++ [getCV "Result" (literal v)]) [])
+                                      Left  e -> showModel defaultSMTCfg (SMTModel [] vals []) ++ "\n" ++ e
 
                         QC.monitor (QC.counterexample model)
 
