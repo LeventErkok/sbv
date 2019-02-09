bbe93b85e2d2cba2a55be81a3099b94d3ca17cac
Levent Erkok <erkokl@gmail.com>
2019-02-08 21:31:09 -0800

    fix haddock

--- Documentation/SBV/Examples/Puzzles/Fish.hs
+++ Documentation/SBV/Examples/Puzzles/Fish.hs
@@ -74,7 +74,7 @@ mkSymbolicEnumeration ''Sport
 --
 -- It's not hard to modify this program to grab the values of all the assignments, i.e., the full
 -- solution to the puzzle. We leave that as an exercise to the interested reader!
--- NB. We use the 'allSatTrackUIFuns' configuration to indicate that the uninterpreted function
+-- NB. We use the 'allSatTrackUFs' configuration to indicate that the uninterpreted function
 -- changes do not matter for generating different values. All we care is that the fishOwner changes!
 fishOwner :: IO ()
 fishOwner = do vs <- getModelValues "fishOwner" `fmap` allSatWith z3{allSatTrackUFs = False} puzzle
