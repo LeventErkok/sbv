f4753dbcda5e5759a2d0760257de237098d9cc23
Levent Erkok <erkokl@gmail.com>
2019-02-09 21:02:33 -0800

    Reformat + change type of either

--- Data/SBV/Either.hs
+++ Data/SBV/Either.hs
@@ -1,31 +1,39 @@
-{-# language KindSignatures      #-}
-{-# language Rank2Types          #-}
-{-# language ScopedTypeVariables #-}
-{-# language TypeApplications    #-}
-{-# language TypeOperators       #-}
 -----------------------------------------------------------------------------
 -- |
 -- Module    : Data.SBV.Either
--- Author    : Joel Burget, Levent Erkok
+-- Copyright : (c) Joel Burget
+--                 Levent Erkok
 -- License   : BSD3
 -- Maintainer: erkokl@gmail.com
 -- Stability : experimental
 --
--- Symbolic Either.
+-- Symbolic sums, similar to Haskell's 'Either' type.
 -----------------------------------------------------------------------------
 
+{-# LANGUAGE KindSignatures      #-}
+{-# LANGUAGE Rank2Types          #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications    #-}
+{-# LANGUAGE TypeOperators       #-}
+
 module Data.SBV.Either (
-  left, right, liftEither, either,
-  bimap, first, second,
-  isLeft, isRight
+    -- * Constructing sums
+    left, right, liftEither
+    -- * Destructing sums
+    , either
+    -- * Mapping functions
+    , bimap, first, second
+    -- * Scrutinizing branches of a sum
+    , isLeft, isRight
   ) where
 
-import           Prelude             hiding      (either)
+import           Prelude hiding (either)
 import qualified Prelude
-import           Data.Proxy          (Proxy(Proxy))
-import           Data.SBV.Core.Data
-import           Data.SBV.Core.Model () -- instances only
 
+import Data.Proxy (Proxy(Proxy))
+
+import Data.SBV.Core.Data
+import Data.SBV.Core.Model () -- instances only
 
 -- | Construct an @SBV (Either a b)@ from an @SBV a@
 left :: forall a b. (SymVal a, SymVal b) => SBV a -> SBV (Either a b)
@@ -50,69 +58,62 @@ right sb
                     newExpr st k $ SBVApp (SumConstructor InR) [bsv]
 
 -- | Construct an @SBV (Either a b)@ from an @Either a b@
-liftEither
-  :: (SymVal a, SymVal b)
-  => Either (SBV a) (SBV b)
-  -> SBV (Either a b)
+liftEither :: (SymVal a, SymVal b) => Either (SBV a) (SBV b) -> SBV (Either a b)
 liftEither = Prelude.either left right
-
--- | Map over both sides of a symbolic 'Either' at the same time
-bimap
-  :: forall a b c d.
-     (SymVal a, SymVal b, SymVal c, SymVal d)
-  => (SBV a -> SBV b)
-  -> (SBV c -> SBV d)
-  -> SBV (Either a c)
-  -> SBV (Either b d)
-bimap brA brC sac = either sac (left . brA) (right . brC)
-
--- | Map over the left side of an 'Either'
-first
-  :: (SymVal a, SymVal b, SymVal c)
-  => (SBV a -> SBV b) -> SBV (Either a c) -> SBV (Either b c)
-first f = bimap f id
-
--- | Map over the right side of an 'Either'
-second
-  :: (SymVal a, SymVal b, SymVal c)
-  => (SBV b -> SBV c) -> SBV (Either a b) -> SBV (Either a c)
-second = bimap id
-
--- | Return 'sTrue' if the given symbolic value is 'Left', 'sFalse' otherwise
-isLeft :: (SymVal a, SymVal b) => SBV (Either a b) -> SBV Bool
-isLeft sab = either sab (const sTrue) (const sFalse)
-
--- | Return 'sTrue' if the given symbolic value is 'Right', 'sFalse' otherwise
-isRight :: (SymVal a, SymVal b) => SBV (Either a b) -> SBV Bool
-isRight sab = either sab (const sFalse) (const sTrue)
 
 -- | Case analysis for symbolic 'Either's. If the value 'isLeft', apply the
 -- first function; if it 'isRight', apply the second function.
-either
-  :: forall a b c.
-     (SymVal a, SymVal b, SymVal c)
-  => SBV (Either a b)
-  -> (SBV a -> SBV c)
-  -> (SBV b -> SBV c)
-  -> SBV c
-either sab brA brB
+either :: forall a b c. (SymVal a, SymVal b, SymVal c)
+       => (SBV a -> SBV c)
+       -> (SBV b -> SBV c)
+       -> SBV (Either a b)
+       -> SBV c
+either brA brB sab
   | Just (Left  a) <- unliteral sab
   = brA $ literal a
   | Just (Right b) <- unliteral sab
   = brB $ literal b
   | True
   = SBV $ SVal kc $ Right $ cache res
-
   where ka = kindOf (Proxy @a)
         kb = kindOf (Proxy @b)
         kc = kindOf (Proxy @c)
 
         res st = do abv <- sbvToSV st sab
-                    isLeft' <- newExpr st KBool $ SBVApp (SumIs InL) [abv]
-                    br1 <- sbvToSV st $ brA $ SBV $ SVal ka $ Right $
-                      cache $ \_ -> newExpr st ka $
-                        SBVApp (SumAccess InL) [abv]
-                    br2 <- sbvToSV st $ brB $ SBV $ SVal kb $ Right $
-                      cache $ \_ -> newExpr st kb $
-                        SBVApp (SumAccess InR) [abv]
-                    newExpr st kc $ SBVApp Ite [isLeft', br1, br2]
+
+                    let leftVal  = SBV $ SVal ka $ Right $ cache $ \_ -> newExpr st ka $ SBVApp (SumAccess InL) [abv]
+                        rightVal = SBV $ SVal kb $ Right $ cache $ \_ -> newExpr st kb $ SBVApp (SumAccess InR) [abv]
+
+                        leftRes  = brA leftVal
+                        rightRes = brB rightVal
+
+                    br1 <- sbvToSV st leftRes
+                    br2 <- sbvToSV st rightRes
+
+                    --  Which branch are we in? Return the appropriate value:
+                    onLeft <- newExpr st KBool $ SBVApp (SumIs InL) [abv]
+                    newExpr st kc $ SBVApp Ite [onLeft, br1, br2]
+
+-- | Map over both sides of a symbolic 'Either' at the same time
+bimap :: forall a b c d.  (SymVal a, SymVal b, SymVal c, SymVal d)
+      => (SBV a -> SBV b)
+      -> (SBV c -> SBV d)
+      -> SBV (Either a c)
+      -> SBV (Either b d)
+bimap brA brC = either (left . brA) (right . brC)
+
+-- | Map over the left side of an 'Either'
+first :: (SymVal a, SymVal b, SymVal c) => (SBV a -> SBV b) -> SBV (Either a c) -> SBV (Either b c)
+first f = bimap f id
+
+-- | Map over the right side of an 'Either'
+second :: (SymVal a, SymVal b, SymVal c) => (SBV b -> SBV c) -> SBV (Either a b) -> SBV (Either a c)
+second = bimap id
+
+-- | Return 'sTrue' if the given symbolic value is 'Left', 'sFalse' otherwise
+isLeft :: (SymVal a, SymVal b) => SBV (Either a b) -> SBV Bool
+isLeft = either (const sTrue) (const sFalse)
+
+-- | Return 'sTrue' if the given symbolic value is 'Right', 'sFalse' otherwise
+isRight :: (SymVal a, SymVal b) => SBV (Either a b) -> SBV Bool
+isRight = either (const sFalse) (const sTrue)
