18988ae7ec533b2a8b80492737ed1f96aaeadd00
Levent Erkok <erkokl@gmail.com>
2019-02-17 01:01:13 -0800

    More tests

--- SBVTestSuite/TestSuite/Queries/UISatEx.hs
+++ SBVTestSuite/TestSuite/Queries/UISatEx.hs
@@ -14,18 +14,37 @@
 
 module TestSuite.Queries.UISatEx where
 
+import Data.SBV.Control
+
 import Utils.SBVTestFramework
 
 -- Test suite
 tests :: TestTree
 tests =
   testGroup "Queries.UISatEx"
-    [ goldenCapturedIO "query_uisatex" testQuery
+    [ goldenCapturedIO "query_uisatex"  testQuery1
+    , goldenCapturedIO "query_uisatex2" testQuery2
     ]
 
-testQuery :: FilePath -> IO ()
-testQuery rf = do r <- satWith defaultSMTCfg{verbose=True, redirectVerbose=Just rf} core
-                  appendFile rf ("\n FINAL:\n" ++ show r ++ "\nDONE!\n")
+testQuery1 :: FilePath -> IO ()
+testQuery1 rf = do r <- satWith defaultSMTCfg{verbose=True, redirectVerbose=Just rf} core
+                   appendFile rf ("\n FINAL:\n" ++ show r ++ "\nDONE!\n")
+
+testQuery2 :: FilePath -> IO ()
+testQuery2 rf = do r <- runSMTWith defaultSMTCfg{verbose=True, redirectVerbose=Just rf, allSatMaxModelCount = Just 5} qCore
+                   appendFile rf ("\n FINAL:\n" ++ show r ++ "\nDONE!\n")
+  where qCore = do core
+                   let q6 :: SInteger -> SBool
+                       q6 = uninterpret "q6"
+                   constrain $ q6 0 .=> q6 0
+                   query $ do ensureSat
+                              qv1 <- getFunction q1
+                              qv2 <- getFunction q2
+                              qv3 <- getFunction q3
+                              qv4 <- getFunction q4
+                              qv5 <- getFunction q5
+                              qv6 <- getFunction q6
+                              return (qv1, qv2, qv3, qv4, qv5, qv6)
 
 q1 :: SInteger -> SInteger
 q1 = uninterpret "q1"
@@ -53,9 +72,9 @@ core = do x <- sInteger_
           constrain $ q2 sFalse 7  .== 6
           constrain $ q2 sFalse 12 .== 3
 
-          constrain $ q3 8.6 sTrue  12  .== 8.6
+          constrain $                    q3 8.6 sTrue  12  .== 8.6
           constrain $ fpIsNegativeZero $ q3 9.6 sTrue  121
-          constrain $ q3 9.6 sFalse 8   .== 1/0
+          constrain $                    q3 9.6 sFalse 8   .== 1/0
 
           constrain $ q4 (literal 'c') "hey" .== 78
           constrain $ q4 (literal 'c') "tey" .== 92
@@ -64,20 +83,3 @@ core = do x <- sInteger_
           constrain $ q5 [1,2,3] [8.2, 3] .== 7
           constrain $ q5 [9,5]   [8.2, 9] .== 21
           constrain $ q5 [5]     [8.2, 0] .== 210
-
-{-
-qCore :: IO ()
-qCore = runSMTWith z3{verbose=True} $ do
-          core
-          let q6 :: SInteger -> SBool
-              q6 = uninterpret "q6"
-          constrain $ q6 0 .=> q6 0
-          query $ do ensureSat
-                     qv1 <- getFunction q1
-                     qv2 <- getFunction q2
-                     qv3 <- getFunction q3
-                     qv4 <- getFunction q4
-                     qv5 <- getFunction q5
-                     qv6 <- getFunction q6
-                     return (qv1, qv2, qv3, qv4, qv5, qv6)
-                     -}
