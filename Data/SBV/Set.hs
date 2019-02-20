748541e7358cc7118158654e5297ac3bec73b309
Levent Erkok <erkokl@gmail.com>
2019-02-19 21:39:25 -0800

    comments

--- Data/SBV/Set.hs
+++ Data/SBV/Set.hs
@@ -8,13 +8,13 @@
 --
 -- A collection of set utilities, useful when working with symbolic sets.
 -- To the extent possible, the functions in this module follow those of "Data.Set"
--- so importing qualified is the recommended workflow. Note that unlike "Data.Set"
--- SBV sets can be infinite, represented as a complement of some finite set.
--- This means that a symbolic set is either finite, or its complement is finite.
--- Therefore, there are some differences in the API from Haskell sets. For
--- instance, while you can represent the universal set, you cannot talk about
--- the size of a set. However, all the usual set operations (union, intersection,
--- etc.) are supported.
+-- so importing qualified is the recommended workflow.
+--
+-- Note that unlike "Data.Set", SBV sets can be infinite, represented as a
+-- complement of some finite set. This means that a symbolic set is either finite,
+-- or its complement is finite. Therefore, there are some differences in the API
+-- from Haskell sets. For instance, you can take the complement of any set, which
+-- is something you cannot do in Haskell!
 -----------------------------------------------------------------------------
 
 {-# LANGUAGE Rank2Types          #-}
@@ -45,7 +45,7 @@ empty = SBV $ SVal k $ Left $ CV k $ CSet $ RegularSet Set.empty
 -- >>> full :: SSet Integer
 -- U :: {SInteger}
 --
--- Note that the universal set over a type is represented by the letter 'U'.
+-- Note that the universal set over a type is represented by the letter @U@.
 full :: forall a. HasKind a => SSet a
 full = SBV $ SVal k $ Left $ CV k $ CSet $ ComplementSet Set.empty
   where k = KSet $ kindOf (Proxy @a)
