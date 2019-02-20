ac5d0071909674a27c9079f6929f6859b8627726
Levent Erkok <erkokl@gmail.com>
2019-02-19 20:42:23 -0800

    Start working on symbolic set methods. WIP.

---
+++ Data/SBV/Set.hs
@@ -0,0 +1,51 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module    : Data.SBV.Set
+-- Copyright : (c) Levent Erkok
+-- License   : BSD3
+-- Maintainer: erkokl@gmail.com
+-- Stability : experimental
+--
+-- A collection of set utilities, useful when working with symbolic sets.
+-- To the extent possible, the functions in this module follow those of "Data.Set"
+-- so importing qualified is the recommended workflow. Note that unlike "Data.Set"
+-- SBV sets can be infinite, represented as a complement of some finite set.
+-- This means that a symbolic set is either finite, or its complement is finite.
+-- Therefore, there are some differences in the API from Haskell sets. For
+-- instance, while you can represent the universal set, you cannot talk about
+-- the size of a set. However, all the usual set operations (union, intersection,
+-- etc.) are supported.
+-----------------------------------------------------------------------------
+
+{-# LANGUAGE Rank2Types          #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications    #-}
+
+module Data.SBV.Set (
+        -- * Constructing sets
+          empty, full
+        ) where
+
+import Data.SBV.Core.Data
+
+import qualified Data.Set as Set
+
+import Data.Proxy
+
+-- | Empty set.
+--
+-- >>> empty :: SSet Integer
+-- {} :: {SInteger}
+empty :: forall a. HasKind a => SSet a
+empty = SBV $ SVal k $ Left $ CV k $ CSet $ RegularSet Set.empty
+  where k = KSet $ kindOf (Proxy @a)
+
+-- | Full set.
+--
+-- >>> full :: SSet Integer
+-- U :: {SInteger}
+--
+-- Note that the universal set over a type is represented by the letter 'U'.
+full :: forall a. HasKind a => SSet a
+full = SBV $ SVal k $ Left $ CV k $ CSet $ ComplementSet Set.empty
+  where k = KSet $ kindOf (Proxy @a)
