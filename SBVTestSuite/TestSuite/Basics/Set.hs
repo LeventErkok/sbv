32fb7a628fae1513816db86562606a5d5266d56d
Levent Erkok <erkokl@gmail.com>
2019-02-26 07:36:48 -0800

    Simplify set tests

--- SBVTestSuite/TestSuite/Basics/Set.hs
+++ SBVTestSuite/TestSuite/Basics/Set.hs
@@ -29,13 +29,6 @@ import Utils.SBVTestFramework hiding (complement)
 data E = A | B | C
 mkSymbolicEnumeration ''E
 
-c :: (SymVal a, Ord a) => SSet a -> SSet a
-c = complement
-
-u, i :: (SymVal a, Ord a) => SSet a -> SSet a -> SSet a
-u = union
-i = intersection
-
 isIdentifier :: RegExpMatchable a => a -> SBool
 isIdentifier = (`match` identifier)
 
@@ -44,14 +37,18 @@ sIdentifier nm = do n <- sChar nm
                     constrain $ isIdentifier n
                     return n
 
+type SC = SSet  Char
+type RC = RCSet Char
+
 -- Test suite
 tests :: TestTree
 tests = testGroup "Basics.Set" [
           goldenCapturedIO "set_uninterp1"  $ ta setE1
         , goldenCapturedIO "set_uninterp2"  $ tq setE2
-        , goldenCapturedIO "set_union1"     $ tq setU1
-        , goldenCapturedIO "set_intersect1" $ tq setI1
-        , goldenCapturedIO "set_compl1"     $ tq setC1
+        , goldenCapturedIO "set_compl1"     $ tq $ templateU complement
+        , goldenCapturedIO "set_union1"     $ tq $ templateB union
+        , goldenCapturedIO "set_intersect1" $ tq $ templateB intersection
+        , goldenCapturedIO "set_diff1"      $ tq $ templateB difference
         ]
     where ta tc goldFile    = record goldFile =<< tc defaultSMTCfg{verbose=True, redirectVerbose=Just goldFile}
           tq tc goldFile    = record goldFile =<< runSMTWith defaultSMTCfg{verbose=True, redirectVerbose=Just goldFile} tc
@@ -69,55 +66,30 @@ setE2 = do a :: SSet E <- sSet "a"
            query $ do ensureSat
                       (,) <$> getValue a <*> getValue b
 
-setU1 :: Symbolic (Char, Char, RCSet Char, RCSet Char, RCSet Char, RCSet Char)
-setU1 = do a <- sIdentifier "a"
-           b <- sIdentifier "b"
+templateU :: (SC -> SC) -> Symbolic (Char, RC, RC, RC, RC)
+templateU f = do a <- sIdentifier "a"
 
-           -- I'd love to have these distinct, but
-           -- currently z3 is spitting out too complicated a
-           -- model for us to parse back if I have this
-           -- constraint. See: https://github.com/Z3Prover/z3/issues/2153
-           -- If they ever fix it, add this constraint back.
-           -- constrain $ distinct [a, b]
+                 let sa = singleton a
 
-           let sa = singleton a
-               sb = singleton b
+                     o1 = f sa
+                     o2 = f o1
+                     o3 = o1 `intersection` sa
+                     o4 = o1 `union`        sa
 
-               o1 =   sa `u`   sb
-               o2 =   sa `u` c sb
-               o3 = c sa `u`   sb
-               o4 = c sa `u` c sb
+                 query $ do ensureSat
+                            (,,,,) <$> getValue a <*> getValue o1 <*> getValue o2 <*> getValue o3 <*> getValue o4
 
-           query $ do ensureSat
-                      (,,,,,) <$> getValue a <*> getValue b <*> getValue o1 <*> getValue o2 <*> getValue o3 <*> getValue o4
+templateB :: (SC -> SC -> SC) -> Symbolic (Char, Char, RC, RC, RC, RC)
+templateB f = do a <- sIdentifier "a"
+                 b <- sIdentifier "b"
 
-setI1 :: Symbolic (Char, Char, RCSet Char, RCSet Char, RCSet Char, RCSet Char)
-setI1 = do a <- sIdentifier "a"
-           b <- sIdentifier "b"
+                 let sa = singleton a
+                     sb = singleton b
 
-           -- See relevant note in setU1 for the following constraint:
-           -- constrain $ distinct [a, b]
+                     o1 =            sa `f`            sb
+                     o2 =            sa `f` complement sb
+                     o3 = complement sa `f`            sb
+                     o4 = complement sa `f` complement sb
 
-           let sa = singleton a
-               sb = singleton b
-
-               o1 =   sa `i`   sb
-               o2 =   sa `i` c sb
-               o3 = c sa `i`   sb
-               o4 = c sa `i` c sb
-
-           query $ do ensureSat
-                      (,,,,,) <$> getValue a <*> getValue b <*> getValue o1 <*> getValue o2 <*> getValue o3 <*> getValue o4
-
-setC1 :: Symbolic (Char, RCSet Char, RCSet Char, RCSet Char, RCSet Char)
-setC1 = do a <- sIdentifier "a"
-
-           let sa = singleton a
-
-               o1 = c sa
-               o2 = c o1
-               o3 = o1 `i` sa
-               o4 = o1 `u` sa
-
-           query $ do ensureSat
-                      (,,,,) <$> getValue a <*> getValue o1 <*> getValue o2 <*> getValue o3 <*> getValue o4
+                 query $ do ensureSat
+                            (,,,,,) <$> getValue a <*> getValue b <*> getValue o1 <*> getValue o2 <*> getValue o3 <*> getValue o4
