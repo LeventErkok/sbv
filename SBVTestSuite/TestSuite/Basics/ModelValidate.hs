91d9f6ed63939b2961bb04bb9e1e735fd4e40962
Levent Erkok <erkokl@gmail.com>
2019-03-08 00:40:08 -0800

    Another validation test

--- SBVTestSuite/TestSuite/Basics/ModelValidate.hs
+++ SBVTestSuite/TestSuite/Basics/ModelValidate.hs
@@ -9,7 +9,12 @@
 -- Make sure the ABC bug is there, and a few other validate tests.
 -----------------------------------------------------------------------------
 
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module TestSuite.Basics.ModelValidate (testsABC, tests)  where
+
+import qualified Control.Exception as C
+import Data.List (isPrefixOf)
 
 import Utils.SBVTestFramework
 
@@ -23,15 +28,21 @@ testsABC = testGroup "Basics.ModelValidate.ABC" [
 
 tests :: TestTree
 tests = testGroup "Basics.ModelValidate" [
-             goldenCapturedIO "validate_1" $ t satWith   t1
-           , goldenCapturedIO "validate_2" $ t satWith   t2
-           , goldenCapturedIO "validate_3" $ t satWith   t3
-           , goldenCapturedIO "validate_4" $ t proveWith t3
-           , goldenCapturedIO "validate_5" $ t satWith   t4
-           , goldenCapturedIO "validate_6" $ t proveWith t4
+             goldenCapturedIO "validate_1" $ t  satWith    t1
+           , goldenCapturedIO "validate_2" $ t  satWith    t2
+           , goldenCapturedIO "validate_3" $ t  satWith    t3
+           , goldenCapturedIO "validate_4" $ t  proveWith  t3
+           , goldenCapturedIO "validate_5" $ t  satWith    t4
+           , goldenCapturedIO "validate_6" $ t  proveWith  t4
+           , goldenCapturedIO "validate_7" $ tE satWith    t5
            ]
     where t f test goldFile = do r <- f defaultSMTCfg{verbose=True, redirectVerbose = Just goldFile, validateModel = True} test
                                  appendFile goldFile ("\nFINAL OUTPUT:\n" ++ show r ++ "\n")
+
+          tE f test goldFile = do r <- f defaultSMTCfg{verbose=True, redirectVerbose = Just goldFile, validateModel = True} test
+                                  appendFile goldFile ("\nFINAL OUTPUT:\n" ++ show r ++ "\n")
+                               `C.catch` (\(e::C.SomeException) -> appendFile goldFile ("\nEXCEPTION RAISED:\n" ++ pick (show e) ++ "\n"))
+              where pick s = unlines [l | l <- lines s, "***" `isPrefixOf` l]
 
           t1 = forSome ["x"] $ \x -> fpAdd sRTZ x x   .== (x::SFloat)
           t2 = forSome ["x"] $ \x -> fpFMA sRNE x x x .== (x::SFloat)
@@ -43,3 +54,7 @@ tests = testGroup "Basics.ModelValidate" [
                   y <- sInteger "y"
                   constrain $ x .> y
                   return $ x .== y+3
+
+          t5 = do x <- exists "x"
+                  y <- forall "y"
+                  return $ fpIsPoint y .=> x .<= (y::SFloat)
