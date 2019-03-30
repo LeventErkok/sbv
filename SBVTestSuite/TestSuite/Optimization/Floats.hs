d589168c9e1bd5cf9d6f20085a02f24c17565ae3
Levent Erkok <erkokl@gmail.com>
2019-03-30 11:52:32 -0700

    Make float-opt tests resilient to z3 version changes

--- SBVTestSuite/TestSuite/Optimization/Floats.hs
+++ SBVTestSuite/TestSuite/Optimization/Floats.hs
@@ -19,23 +19,31 @@ import Utils.SBVTestFramework
 tests :: TestTree
 tests =
   testGroup "Optimization.Floats"
-    [ goldenVsStringShow "optFloat1" $ optimizeWith z3{printBase=16} Independent (p True)
-    , goldenVsStringShow "optFloat2" $ optimizeWith z3{printBase=16} Independent (p False)
-    , goldenVsStringShow "optFloat3" $ optimizeWith z3{printBase=16} Lexicographic q
-    , goldenVsStringShow "optFloat4" $ optimizeWith z3{printBase=16} Lexicographic r
+    [ goldenVsStringShow "optFloat1a" $ optimizeWith z3{printBase=16} Lexicographic (floatMinMax  (minimize "min-x") True)
+    , goldenVsStringShow "optFloat1b" $ optimizeWith z3{printBase=16} Lexicographic (floatMinMax  (minimize "min-x") False)
+    , goldenVsStringShow "optFloat1c" $ optimizeWith z3{printBase=16} Lexicographic (floatMinMax  (maximize "max-x") True)
+    , goldenVsStringShow "optFloat1d" $ optimizeWith z3{printBase=16} Lexicographic (floatMinMax  (maximize "max-y") False)
+    , goldenVsStringShow "optFloat2a" $ optimizeWith z3{printBase=16} Lexicographic (doubleMinMax (minimize "min-x") True)
+    , goldenVsStringShow "optFloat2b" $ optimizeWith z3{printBase=16} Lexicographic (doubleMinMax (minimize "min-x") False)
+    , goldenVsStringShow "optFloat2c" $ optimizeWith z3{printBase=16} Lexicographic (doubleMinMax (maximize "max-x") True)
+    , goldenVsStringShow "optFloat2d" $ optimizeWith z3{printBase=16} Lexicographic (doubleMinMax (maximize "max-y") False)
+    , goldenVsStringShow "optFloat3"  $ optimizeWith z3{printBase=16} Lexicographic q
+    , goldenVsStringShow "optFloat4"  $ optimizeWith z3{printBase=16} Lexicographic r
     ]
 
-p :: Bool -> Goal
-p reqPoint = do x <- sFloat  "x"
-                y <- sDouble "y"
+floatMinMax :: (SFloat -> Symbolic ()) -> Bool -> Goal
+floatMinMax opt reqPoint = do x <- sFloat "x"
 
-                when reqPoint $ do constrain $ fpIsPoint x
-                                   constrain $ fpIsPoint y
+                              when reqPoint $ constrain $ fpIsPoint x
 
-                minimize "min-x" x
-                maximize "max-x" x
-                minimize "min-y" y
-                maximize "max-y" y
+                              opt x
+
+doubleMinMax :: (SDouble -> Symbolic ()) -> Bool -> Goal
+doubleMinMax opt reqPoint = do x <- sDouble "x"
+
+                               when reqPoint $ constrain $ fpIsPoint x
+
+                               opt x
 
 q :: Goal
 q = do x <- sFloat "x"
