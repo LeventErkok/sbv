c57673366e27561184f6a3d841ea35a68bb083dc
Levent Erkok <erkokl@gmail.com>
2019-06-12 15:17:54 -0700

    Towards automating build scripts

---
+++ buildUtils/ci.hs
@@ -0,0 +1,87 @@
+module Main(main) where
+
+import System.Environment
+
+travisFile, appveyorFile :: FilePath
+travisFile   = "../.travis.yml"
+appveyorFile = "../.appveyor.yml"
+
+main :: IO ()
+main = do as <- getArgs
+          case as of
+            ["travis"]   -> do writeFile travisFile $ unlines travis
+                               putStrLn $ "Generated: " ++ travisFile
+            ["appveyor"] -> do writeFile appveyorFile $ unlines appveyor
+                               putStrLn $ "Generated: " ++ appveyorFile
+            _            -> error $ "Call with either travis or appveyor"
+
+header :: [String]
+header = [ "#####################################################"
+         , "# Automatically generated CI build file. Do not edit!"
+         , "#####################################################"
+         ]
+
+footer :: [String]
+footer = [ "#####################################################"
+         , "# End of automatically generated CI build file."
+         , "#####################################################"
+         ]
+
+travis :: [String]
+travis = header ++ body ++ footer
+ where body = [ ""
+              , "Wish I knew how to generate travis"
+              , ""
+              ]
+
+appveyor :: [String]
+appveyor = header ++ body ++ footer
+ where -- put tweaks here
+       heavyTestPercentage :: Int
+       heavyTestPercentage = 0
+
+       ghcVersion :: String
+       ghcVersion = "8.6.5"
+
+       z3Version :: String
+       z3Version = "4.8.6"
+
+       z3Name :: String
+       z3Name = "z3-" ++ z3Version ++ "-x64-win.zip"
+
+       z3Path :: String
+       z3Path = "https://github.com/Z3Prover/z3/releases/download/Nightly/z3-" ++ z3Version ++ "-x64-win.zip"
+
+       -- No changes should be necessary below
+       body = [ ""
+              , "build: off"
+              , ""
+              , "environment:"
+              , "    SBV_TEST_ENVIRONMENT: win"
+              , "    SBV_HEAVYTEST_PERCENTAGE: " ++ show heavyTestPercentage
+              , "    TASTY_HIDE_SUCCESSES: True"
+              , ""
+              , "before_build:"
+              , "- curl -fsSL " ++ z3Path ++ " -o " ++ z3Name
+              , "- 7z e " ++ z3Name ++ " -oc:\\projects\\sbv\\z3_downloaded -r -y"
+              , "- choco install -y cabal"
+              , "- choco install -y ghc --version " ++ ghcVersion
+              , "- refreshenv"
+              , "- set PATH=C:\\projects\\sbv\\z3_downloaded;%PATH%"
+              , "- ghc --version"
+              , "- z3 --version"
+              , ""
+              , "skip_tags: true"
+              , ""
+              , "build_script:"
+              , "- cabal update"
+              , "- cabal install alex"
+              , "- cabal install happy"
+              , "- cabal install --only-dependencies --enable-tests --enable-benchmarks"
+              , "- cabal build"
+              , "- cabal test SBVTest"
+              , "- cabal test SBVDocTest"
+              , "- cabal test SBVHLint"
+              , "- cabal check"
+              , "- cabal sdist"
+              ]
