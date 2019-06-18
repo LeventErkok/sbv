2502cc2633149f8060e05dfb6cb3c0cc02549e73
Levent Erkok <erkokl@gmail.com>
2019-06-17 22:44:02 -0700

    Use Word 48 here

--- Documentation/SBV/Examples/Existentials/CRCPolynomial.hs
+++ Documentation/SBV/Examples/Existentials/CRCPolynomial.hs
@@ -13,30 +13,22 @@
 -- apart, then they must differ in at least 4 bits.
 -----------------------------------------------------------------------------
 
+{-# LANGUAGE DataKinds #-}
+
 module Documentation.SBV.Examples.Existentials.CRCPolynomial where
 
 import Data.SBV
 import Data.SBV.Tools.Polynomial
 
------------------------------------------------------------------------------
--- * Modeling 48 bit words
------------------------------------------------------------------------------
--- | SBV doesn't support 48 bit words natively. So, we represent them
--- as a tuple, 32 high-bits and 16 low-bits.
-type SWord48 = (SWord32, SWord16)
-
 -- | Compute the 16 bit CRC of a 48 bit message, using the given polynomial
-crc_48_16 :: SWord48 -> SWord16 -> [SBool]
-crc_48_16 msg poly = crcBV 16 msgBits polyBits
-  where (hi, lo) = msg
-        msgBits  = blastBE hi ++ blastBE lo
-        polyBits = blastBE poly
+crc_48_16 :: SWord 48 -> SWord16 -> [SBool]
+crc_48_16 msg poly = crcBV 16 (blastBE msg) (blastBE poly)
 
 -- | Count the differing bits in the message and the corresponding CRC
-diffCount :: (SWord48, [SBool]) -> (SWord48, [SBool]) -> SWord8
-diffCount ((h1, l1), crc1) ((h2, l2), crc2) = count xorBits
-  where bits1   = blastBE h1 ++ blastBE l1 ++ crc1
-        bits2   = blastBE h2 ++ blastBE l2 ++ crc2
+diffCount :: (SWord 48, [SBool]) -> (SWord 48, [SBool]) -> SWord8
+diffCount (d1, crc1) (d2, crc2) = count xorBits
+  where bits1   = blastBE d1 ++ crc1
+        bits2   = blastBE d2 ++ crc2
         -- xor will give us a false if bits match, true if they differ
         xorBits = zipWith (.<+>) bits1 bits2
         count []     = 0
@@ -48,33 +40,26 @@ diffCount ((h1, l1), crc1) ((h2, l2), crc2) = count xorBits
 -- @sent@ and @received@ messages are different, then it must be the
 -- case that that must differ from each other (including CRCs), in
 -- more than @hd@ bits.
-crcGood :: SWord8 -> SWord16 -> SWord48 -> SWord48 -> SBool
+crcGood :: SWord8 -> SWord16 -> SWord 48 -> SWord 48 -> SBool
 crcGood hd poly sent received =
      sent ./= received .=> diffCount (sent, crcSent) (received, crcReceived) .>= hd
    where crcSent     = crc_48_16 sent     poly
          crcReceived = crc_48_16 received poly
 
 -- | Generate good CRC polynomials for 48-bit words, given the hamming distance @hd@.
-genPoly :: SWord8 -> IO ()
-genPoly hd = do res <- allSat $ do
-                        -- the polynomial is existentially specified
-                        p <- exists "polynomial"
-                        -- sent word, universal
-                        s <- do sh <- forall "sh"
-                                sl <- forall "sl"
-                                return (sh, sl)
-                        -- received word, universal
-                        r <- do rh <- forall "rh"
-                                rl <- forall "rl"
-                                return (rh, rl)
-                        -- assert that the polynomial @p@ is good. Note
-                        -- that we also supply the extra information that
-                        -- the least significant bit must be set in the
-                        -- polynomial, as all CRC polynomials have the "+1"
-                        -- term in them set. This simplifies the query.
-                        return $ sTestBit p 0 .&& crcGood hd p s r
-                cnt <- displayModels disp res
-                putStrLn $ "Found: " ++ show cnt ++ " polynomail(s)."
+genPoly :: SWord8 -> Int -> IO ()
+genPoly hd maxCnt = do res <- allSatWith defaultSMTCfg{allSatMaxModelCount = Just maxCnt} $ do
+                                p <- exists "polynomial" -- the polynomial is existentially specified
+                                s <- forall "sent"       -- sent word, universal
+                                r <- forall "received"   -- received word, universal
+                                -- assert that the polynomial @p@ is good. Note
+                                -- that we also supply the extra information that
+                                -- the least significant bit must be set in the
+                                -- polynomial, as all CRC polynomials have the "+1"
+                                -- term in them set. This simplifies the query.
+                                return $ sTestBit p 0 .&& crcGood hd p s r
+                       cnt <- displayModels disp res
+                       putStrLn $ "Found: " ++ show cnt ++ " polynomail(s)."
         where disp :: Int -> (Bool, Word16) -> IO ()
               disp n (_, s) = putStrLn $ "Polynomial #" ++ show n ++ ". x^16 + " ++ showPolynomial False s
 
@@ -83,12 +68,12 @@ genPoly hd = do res <- allSat $ do
 -- When run, this function prints:
 --
 --  @
---    Polynomial #1. x^16 + x^2 + x + 1
---    Polynomial #2. x^16 + x^15 + x^2 + 1
---    Polynomial #3. x^16 + x^15 + x^2 + x + 1
---    Polynomial #4. x^16 + x^14 + x^10 + 1
---    Polynomial #5. x^16 + x^14 + x^9 + 1
---    ...
+--    Polynomial #1. x^16 + x^3 + x^2 + 1
+--    Polynomial #2. x^16 + x^3 + x^2 + x + 1
+--    Polynomial #3. x^16 + x^3 + x + 1
+--    Polynomial #4. x^16 + x^15 + x^2 + 1
+--    Polynomial #5. x^16 + x^15 + x^2 + x + 1
+--    Found: 5 polynomail(s).
 --  @
 --
 -- Note that different runs can produce different results, depending on the random
@@ -96,6 +81,7 @@ genPoly hd = do res <- allSat $ do
 -- time to generate these results. On my machine, the first five polynomials were
 -- generated in about 5 minutes.)
 findHD4Polynomials :: IO ()
-findHD4Polynomials = genPoly 4
+findHD4Polynomials = genPoly 4 cnt
+  where cnt = 5 -- Generate at most this many polynomials
 
 {-# ANN crc_48_16 ("HLint: ignore Use camelCase" :: String) #-}
