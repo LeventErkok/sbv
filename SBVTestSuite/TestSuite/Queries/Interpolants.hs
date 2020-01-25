2d072509e85246cd0b32096468f4b02f73558d32
Levent Erkok <erkokl@gmail.com>
2020-01-25 11:25:50 -0800

    add an interpolant test

--- SBVTestSuite/TestSuite/Queries/Interpolants.hs
+++ SBVTestSuite/TestSuite/Queries/Interpolants.hs
@@ -28,8 +28,6 @@ tests =
     , goldenCapturedIO "query_Interpolant2" $ testQuery mathSAT q2
     , goldenCapturedIO "query_Interpolant3" $ testQuery z3      q3
     , goldenCapturedIO "query_Interpolant4" $ testQuery z3      q4
-    , goldenCapturedIO "query_Interpolant5" $ testQuery z3      q5
-    , goldenCapturedIO "query_Interpolant6" $ testQuery z3      q6
     ]
 
 testQuery :: Show a => SMTConfig -> Symbolic a -> FilePath -> IO ()
@@ -78,8 +76,6 @@ q3 = do a <- sInteger "a"
         c <- sInteger "c"
         d <- sInteger "d"
 
-        setOption $ ProduceInterpolants True
-
         namedConstraint "c1" $ a .== b .&& a .== c
         namedConstraint "c2" $ b .== d .&& sNot (c .== d)
 
@@ -96,44 +92,10 @@ q4 = do a <- sInteger "a"
             f = uninterpret "f"
             g = uninterpret "g"
 
-        setOption $ ProduceInterpolants True
-
         namedConstraint "c1" $ f a .== c .&& f b .== d
         namedConstraint "c2" $   a .== b .&& g c ./= g d
 
         query $ do _ <- checkSat
                    getInterpolantZ3 ["c1", "c2"]
-
-q5 :: Symbolic String
-q5 = do x <- sInteger "x"
-        y <- sInteger "y"
-        z <- sInteger "z"
-
-        a :: SArray Integer Integer <- newArray "a" Nothing
-        b :: SArray Integer Integer <- newArray "b" Nothing
-
-        namedConstraint "c1" $ b .== writeArray (writeArray a x 0) y (0::SInteger)
-        namedConstraint "c2" $ z .== x .&& readArray b z .== 1
-
-        setOption $ ProduceInterpolants True
-
-        query $ do _ <- checkSat
-                   getInterpolantZ3 ["c1", "c2"]
-
-q6 :: Symbolic String
-q6 = do a <- sInteger "a"
-        b <- sInteger "b"
-        c <- sInteger "c"
-        d <- sInteger "d"
-        e <- sInteger "e"
-
-        namedConstraint "c1" $ a .== b .&& a .== c
-        namedConstraint "c2" $ c .== d
-        namedConstraint "c3" $ b .== e .&& d ./= e
-
-        setOption $ ProduceInterpolants True
-
-        query $ do _ <- checkSat
-                   getInterpolantZ3 ["c1", "c2", "c3"]
 
 {-# ANN module ("HLint: ignore Reduce duplication" :: String) #-}
