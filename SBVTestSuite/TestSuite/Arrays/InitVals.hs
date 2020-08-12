ad1f132db85cb0d5a11874d895ff8cc60ace42df
Levent Erkok <erkokl@gmail.com>
2020-08-11 18:22:21 -0700

    sListArray tests
    Addresses #553

--- SBVTestSuite/TestSuite/Arrays/InitVals.hs
+++ SBVTestSuite/TestSuite/Arrays/InitVals.hs
@@ -9,7 +9,8 @@
 -- Testing arrays with initializers
 -----------------------------------------------------------------------------
 
-{-# LANGUAGE Rank2Types #-}
+{-# LANGUAGE Rank2Types       #-}
+{-# LANGUAGE TypeApplications #-}
 
 {-# OPTIONS_GHC -Wall -Werror #-}
 
@@ -37,12 +38,26 @@ readNoDef proxy = do i <- free "i"
                      return $ readArray a i .== j
 
 
+constArr :: forall m. SymArray m => m Integer Integer -> Predicate
+constArr proxy = do i <- sInteger "i"
+                    j <- sInteger "j"
+
+                    constrain $ i ./= j
+                    constrain $ i `sElem` [1, 2, 3, 75]
+                    pure $ readArray myArray i .== readArray (myArray `asTypeOf` proxy) j
+  where myArray = sListArray (Just 7) [(1, 12), (2, 5) , (3, 6), (75, 5)]
+
 tests :: TestTree
 tests =
   testGroup "Arrays.InitVals"
-    [ testCase "readDef_SArray"    $ assertIsThm (readDef (undefined :: SArray    Integer Integer))
-    , testCase "readDef_SFunArray" $ assertIsThm (readDef (undefined :: SFunArray Integer Integer))
+    [ testCase "readDef_SArray"             $ assertIsThm (readDef (undefined :: SArray    Integer Integer))
+    , testCase "readDef_SFunArray"          $ assertIsThm (readDef (undefined :: SFunArray Integer Integer))
 
-    , testCase "readDef_SArray"    $ assertIsSat (readNoDef (undefined :: SArray    Integer Integer))
-    , testCase "readDef_SFunArray" $ assertIsSat (readNoDef (undefined :: SFunArray Integer Integer))
+    , testCase "readDef_SArray"             $ assertIsSat (readNoDef (undefined :: SArray    Integer Integer))
+    , testCase "readDef_SFunArray"          $ assertIsSat (readNoDef (undefined :: SFunArray Integer Integer))
+
+    , goldenCapturedIO "constArr_SArray"    $ t (undefined :: SArray    Integer Integer)
+    , goldenCapturedIO "constArr_SFunArray" $ t (undefined :: SFunArray Integer Integer)
     ]
+    where t p goldFile = do r <- satWith defaultSMTCfg{verbose=True, redirectVerbose = Just goldFile} (constArr p)
+                            appendFile goldFile ("\nFINAL OUTPUT:\n" ++ show r ++ "\n")
