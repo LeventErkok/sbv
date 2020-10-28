54e1446a083811c01083fc6115230be5c9e14ec8
Levent Erkok <erkokl@gmail.com>
2020-10-27 21:52:39 -0700

    Some extra uninterpreted tests

--- SBVTestSuite/TestSuite/Uninterpreted/Uninterpreted.hs
+++ SBVTestSuite/TestSuite/Uninterpreted/Uninterpreted.hs
@@ -8,20 +8,33 @@
 --
 -----------------------------------------------------------------------------
 
-{-# OPTIONS_GHC -Wall -Werror #-}
+{-# LANGUAGE DeriveAnyClass     #-}
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TemplateHaskell    #-}
+{-# OPTIONS_GHC -Wall -Werror   #-}
 
 module TestSuite.Uninterpreted.Uninterpreted(tests) where
 
 import Utils.SBVTestFramework
 
+data Q
+mkUninterpretedSort ''Q
+
 -- Test suite
 tests :: TestTree
 tests =
   testGroup "Uninterpreted.Uninterpreted"
-    [ testCase "uninterpreted-0" (assertIsThm p0)
-    , testCase "uninterpreted-1" (assertIsThm p1)
-    , testCase "uninterpreted-2" (assertIsntThm p2)
+    [ testCase         "uninterpreted-0"  $ assertIsThm   p0
+    , testCase         "uninterpreted-1"  $ assertIsThm   p1
+    , testCase         "uninterpreted-2"  $ assertIsntThm p2
+    , goldenCapturedIO "uninterpreted-3"  $ t p3 satWith
+    , goldenCapturedIO "uninterpreted-3a" $ t p3 allSatWith
+    , goldenCapturedIO "uninterpreted-4"  $ t p4 satWith
+    , goldenCapturedIO "uninterpreted-4a" $ t p4 allSatWith
     ]
+  where t tc chk goldFile = do r <- chk defaultSMTCfg{verbose = True, redirectVerbose = Just goldFile} tc
+                               appendFile goldFile ("\n FINAL:" ++ show r ++ "\nDONE!\n")
 
 f :: SInt8 -> SWord32
 f = uninterpret "f"
@@ -37,3 +50,18 @@ p1 x y z = y .== z .=> g x y .== g x z  -- OK
 
 p2 :: SInt8 -> SWord16 -> SWord16 -> SBool
 p2 x y z = y .== z .=> g x y .== f x    -- Not true
+
+
+a, b :: SBool
+a = sym "p"
+b = sym "q"
+
+p3 :: SBool
+p3 = a .|| b
+
+c, d :: SQ
+c = sym "c"
+d = sym "d"
+
+p4 :: SBool
+p4 = c ./= d
