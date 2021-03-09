d7a42ea00e53161df880947c52b5c9669b8c6ee9
Levent Erkok <erkokl@gmail.com>
2021-03-09 15:19:38 -0800

    Introduce Data.SBV.Float

---
+++ Data/SBV/Float.hs
@@ -0,0 +1,25 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module    : Data.SBV.Float
+-- Copyright : (c) Levent Erkok
+-- License   : BSD3
+-- Maintainer: erkokl@gmail.com
+-- Stability : experimental
+--
+-- A collection of arbitrary float operations.
+-----------------------------------------------------------------------------
+
+{-# OPTIONS_GHC -Wall -Werror #-}
+
+module Data.SBV.Float (
+        -- * Type-sized floats
+        FP(..)
+
+        -- * Constructing values
+        , fpFromRawRep, fpNaN, fpInf, fpZero
+
+        -- * Operations
+        , fpFromInteger, fpFromRational, fpFromFloat, fpFromDouble, fpEncodeFloat
+        ) where
+
+import Data.SBV.Core.SizedFloats
