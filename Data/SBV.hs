dcba3b5315e7bc59dc8df0a72d0a97364db768af
Ken Friis Larsen <ken@friislarsen.net>
2022-04-07 11:07:57 +0200

    Add some link to the note about nested quantifiers for `universal` and `existential`.

--- Data/SBV.hs
+++ Data/SBV.hs
@@ -990,7 +990,7 @@ See "Documentation.SBV.Examples.Misc.Enumerate" for an extended example on how t
 -}
 
 {- $noteOnNestedQuantifiers
-=== A note on reasoning in the presence of quantifiers
+=== A note on reasoning in the presence of quantifiers #noteOnNested#
 
 Note that SBV allows reasoning with quantifiers: Inputs can be existentially or universally quantified. Predicates can be built
 with arbitrary nesting of such quantifiers as well. However, SBV always /assumes/ that the input is in
