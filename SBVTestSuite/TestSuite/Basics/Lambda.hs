b2268a5d9163b0c8f205dfe193f50381388002eb
Levent Erkok <erkokl@gmail.com>
2023-03-06 15:29:52 -0800

    use simpler tests

--- SBVTestSuite/TestSuite/Basics/Lambda.hs
+++ SBVTestSuite/TestSuite/Basics/Lambda.hs
@@ -48,9 +48,8 @@ tests =
                                             ,                          P.sum . P.map P.sum
                                             )
 
-      , goldenCapturedIO "lambda08" t5
-      , goldenCapturedIO "lambda09" t6
-
+      , goldenCapturedIO "lambda08" $ eval1 [1 .. 5 :: Float]   (map (+1), P.map (+1))
+      , goldenCapturedIO "lambda09" $ eval1 [1 .. 5 :: Int8]    (map (+1), P.map (+1))
       , goldenCapturedIO "lambda10" $ eval1 [1 .. 5 :: Integer] (map (+1), P.map (+1))
       , goldenCapturedIO "lambda11" $ eval1 [1 .. 5 :: Word8]   (map (+1), P.map (+1))
 
@@ -105,65 +104,6 @@ tests =
    P.++ qc1 "lambda33" P.sum (foldr (+) (0::SInteger))
   where record :: IO String -> FilePath -> IO ()
         record gen rf = appendFile rf . (P.++ "\n") =<< gen
-
-        t5 rf = runSMTWith z3{verbose=True, redirectVerbose=Just rf} $ do
-
-                   let expecting = 5 :: Integer
-
-                   a :: SList Integer <- sList_
-                   b :: SList Integer <- sList_
-
-                   query $ do
-
-                     constrain $ length (zip a b) .== literal expecting
-                     constrain $ length a .== literal expecting
-                     constrain $ length b .== literal expecting
-                     constrain $ all (.== 1) a
-                     constrain $ all (.== 2) b
-
-                     cs <- checkSat
-                     case cs of
-                       Sat -> do av <- getValue a
-                                 bv <- getValue b
-                                 let len = P.fromIntegral $ P.length (P.zip av bv)
-
-                                 unless (len == expecting) $
-                                    error $ unlines [ "Bad output:"
-                                                    , "  a       = " P.++ show av
-                                                    , "  b       = " P.++ show bv
-                                                    , "  zip a b = " P.++ show (P.zip av bv)
-                                                    , "  Length  = " P.++ show len P.++ " was expecting: " P.++ show expecting
-                                                    ]
-
-                       _ -> error $ "Unexpected output: " P.++ show cs
-
-        t6 rf = runSMTWith z3{verbose=True, redirectVerbose=Just rf} $ do
-
-                   a :: SList [Integer] <- sList_
-
-                   sumVal <- sInteger_
-
-                   query $ do
-
-                     let expecting = 5
-
-                     constrain $ a .== literal (replicate expecting (replicate expecting 1))
-                     let sum = foldl (+) 0
-
-                     constrain $ sumVal .== sum (map sum a)  -- Must be expecting * expecting
-
-                     cs <- checkSat
-                     case cs of
-                       Sat -> do final <- getValue sumVal
-                                 av    <- getValue a
-
-                                 unless (final == fromIntegral (expecting * expecting)) $
-                                    error $ unlines [ "Bad output:"
-                                                    , "  a     = " P.++ show av
-                                                    , "  Final = " P.++ show final P.++ " was expecting: " P.++ show (expecting*expecting)
-                                                    ]
-
-                       _ -> error $ "Unexpected output: " P.++ show cs
 
 eval1 :: (SymVal a, SymVal b, Show a, Show b, Eq b) => a -> (SBV a -> SBV b, a -> b) -> FilePath -> IO ()
 eval1 cArg (sFun, cFun) rf = do m <- runSMTWith z3{verbose=True, redirectVerbose=Just rf} run
