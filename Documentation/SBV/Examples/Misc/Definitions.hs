56e9a424ac5c575c88276f7dc023393774ccdda3
Levent Erkok <erkokl@gmail.com>
2023-03-09 11:18:30 -0800

    Mutual recursion example

--- Documentation/SBV/Examples/Misc/Definitions.hs
+++ Documentation/SBV/Examples/Misc/Definitions.hs
@@ -11,13 +11,14 @@
 -- for recursive definitions.
 -----------------------------------------------------------------------------
 
-{-# LANGUAGE OverloadedLists #-}
+{-# LANGUAGE OverloadedLists  #-}
 
 {-# OPTIONS_GHC -Wall -Werror #-}
 
 module Documentation.SBV.Examples.Misc.Definitions where
 
 import Data.SBV
+import Data.SBV.Tuple
 import qualified Data.SBV.List as L
 
 -- | Add one to an argument
@@ -42,7 +43,7 @@ add1Example = sat $ do
 -- of itself, and all recursive calls are made with this name.
 sumToN :: SInteger -> SInteger
 sumToN = smtRecFunction "sumToN" f
-  where f rF x = ite (x .<= 0) 0 (x + rF (x - 1))
+  where f sumToN_ x = ite (x .<= 0) 0 (x + sumToN_ (x - 1))
 
 -- | Prove that sumToN works as expected.
 --
@@ -57,7 +58,7 @@ recExample = sat $ (.== sumToN 5)
 -- | Coding list-length recursively. Again, we map directly to an SMTLib function.
 len :: SList Integer -> SInteger
 len = smtRecFunction "list_length" f
-  where f rF xs = ite (L.null xs) 0 (1 + rF (L.tail xs))
+  where f len_ xs = ite (L.null xs) 0 (1 + len_ (L.tail xs))
 
 -- | Calculate the length of a list, using recursive functions.
 --
@@ -68,3 +69,36 @@ len = smtRecFunction "list_length" f
 --   s0 = 3 :: Integer
 lenExample :: IO SatResult
 lenExample = sat $ (.== len [1,2,3::Integer])
+
+-- | Mutually recursive definitions. The trick is to define the functions together, and pull the results out individually.
+isEvenOdd :: SInteger -> STuple Bool Bool
+isEvenOdd = smtRecFunction "isEvenOdd" f
+  where f isEvenOdd_ x = ite (x .<  0) (isEvenOdd_ (-x))
+                       $ ite (x .== 0) (tuple (sTrue, sFalse))
+                                       (swap (isEvenOdd_ (x - 1)))
+
+-- | Extract the isEven function for easier use.
+isEven :: SInteger -> SBool
+isEven x = isEvenOdd x ^._1
+
+-- | Extract the isEven function for easier use.
+isOdd :: SInteger -> SBool
+isOdd x = isEvenOdd x ^._2
+
+-- | Prove that 20 is even.
+--
+-- We have:
+-- >>> mutRecExample
+-- Satisfiable. Model:
+--  s0 = True :: Bool
+--
+-- Note that we would love to prove things of the form:
+--
+-- @
+--   proveWith z3{verbose=True} $ \x -> isEven x .|| isOdd x
+-- @
+--
+-- alas, if you try this you'll see that z3 goes on forever. Such proofs require induction
+-- and SMT-solvers do not do induction out-of-the box, at least not yet!
+mutRecExample :: IO SatResult
+mutRecExample = sat $ (.== isEven 20)
