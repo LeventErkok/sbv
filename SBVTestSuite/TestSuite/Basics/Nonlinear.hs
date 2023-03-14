3d9b0d6417c57f94c6f3e3e9bf605b20ed91e9b0
Levent Erkok <erkokl@gmail.com>
2023-03-14 07:13:00 -0700

    fix tests

--- SBVTestSuite/TestSuite/Basics/Nonlinear.hs
+++ SBVTestSuite/TestSuite/Basics/Nonlinear.hs
@@ -19,7 +19,8 @@ import Utils.SBVTestFramework
 tests :: TestTree
 tests = testGroup "Basics.Nonlinear" [
           goldenCapturedIO "nonlinear_z3"   $ check z3
-        , goldenCapturedIO "nonlinear_cvc4" $ check cvc5
+        , goldenCapturedIO "nonlinear_cvc4" $ check cvc4
+        , goldenCapturedIO "nonlinear_cvc5" $ check cvc5
         ]
     where check s gf = do r <- satWith s{verbose = True, redirectVerbose = Just gf} f
                           appendFile gf ("\nFINAL:\n" ++ show r ++ "\nDONE!\n")
