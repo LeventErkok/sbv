42d05b6e4d971415f50edcc39ebce33871586f4d
Levent Erkok <erkokl@gmail.com>
2023-08-09 11:20:43 -0700

    Add puzzle example
    Fixes #668

--- CHANGES.md
+++ CHANGES.md
@@ -13,6 +13,8 @@
 
   * Added `Documentation.SBV.Examples.BitPrecise.PEXT_PDEP`, modeling x86 instructions PDEP and PEXT.
 
+  * Added `Documentation/SBV/Examples/Puzzles/Newspaper.hs`, another puzzle example.
+
   * Add 'sbv2smt', which renders the given sbv definition as an SMTLib definition. Mainly useful for debugging purposes.
     It can render both ground definitions and functions, and the latter can be handy in producing SMTLib functions to
     be used in other settings.
---
+++ Documentation/SBV/Examples/Puzzles/Newspaper.hs
@@ -0,0 +1,103 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module    : Documentation.SBV.Examples.Puzzles.Newspaper
+-- Copyright : (c) Levent Erkok
+-- License   : BSD3
+-- Maintainer: erkokl@gmail.com
+-- Stability : experimental
+--
+-- Solution to the following puzzle, which contains 10 questions:
+--
+-- @
+-- a. What is sum of all integer answers?
+-- b. How many boolean answers are true?
+-- c. Is a the largest number?
+-- d. How many integers are equal to me?
+-- e. Are all integers positive?
+-- f. What is the average of all integers?
+-- g. Is d strictly larger than b?
+-- h. What is a / h?
+-- i. Is f equal to d - b - h * d?
+-- j. What is the answer to this question?
+-- @
+--
+-- Note that @j@ is ambiguous: It can be a boolean or an integer. We use
+-- the solver to decide what its type should be, so that all the other
+-- answers are consistent with that decision.
+-----------------------------------------------------------------------------
+
+{-# OPTIONS_GHC -Wall -Werror #-}
+
+module Documentation.SBV.Examples.Puzzles.Newspaper where
+
+import Data.SBV
+import Data.SBV.Either
+
+-- | Encoding of the constraints.
+puzzle :: Symbolic ()
+puzzle = do
+    a <- sInteger "a"
+    b <- sInteger "b"
+    c <- sBool    "c"
+    d <- sInteger "d"
+    e <- sBool    "e"
+    f <- sInteger "f"
+    g <- sBool    "g"
+    h <- sInteger "h"
+    i <- sBool    "i"
+    j <- sEither  "j"
+
+    jIsInt <- sBool "jIsInt"
+
+    let ints  intj = [a, b, d, f, h] ++ [fromRight j |     intj]
+        bools intj = [c, e, g, i]    ++ [fromLeft  j | not intj]
+
+        choice fn = ite jIsInt (fn True) (fn False)
+
+    -- a. What is sum of all integer answers?
+    constrain $ a .== choice (sum . ints)
+
+    -- b. How many boolean answers are true?
+    constrain $ b .== choice (sum . map oneIf . bools)
+
+    -- c. Is a the largest number?
+    constrain $ c .== (a .== choice (foldr1 smax . ints))
+
+    -- d. How many integers are equal to me?
+    constrain $ d .== choice (sum . map (oneIf . (d .==)) . ints)
+
+    -- e. Are all integers positive?
+    constrain $ e .== choice (sAll (.> 0) . ints)
+
+    -- f. What is the average of all integers?
+    constrain $ f * choice (literal . toInteger . length . ints) .== choice (sum . ints)
+
+    -- g. is d strictly larger than b?
+    constrain $ g .== (d .> b)
+
+    -- h. what is a / h?
+    constrain $ h * h .== a
+
+    -- i. is f equal to d - b - h * d?
+    constrain $ i .== (f .== d - b - h * d)
+
+    -- j. what is the answer to this question?
+    constrain $ ite jIsInt (isRight j) (isLeft j)
+
+-- | Print all solutions to the problem. We have:
+--
+-- >>> solvePuzzle
+-- Solution #1:
+--   a =         144 :: Integer
+--   b =           2 :: Integer
+--   c =        True :: Bool
+--   d =           2 :: Integer
+--   e =       False :: Bool
+--   f =          24 :: Integer
+--   g =       False :: Bool
+--   h =         -12 :: Integer
+--   i =        True :: Bool
+--   j = Right (-16) :: Either Bool Integer
+-- This is the only solution.
+solvePuzzle :: IO ()
+solvePuzzle = print =<< allSatWith z3{isNonModelVar = (== "jIsInt")} puzzle
