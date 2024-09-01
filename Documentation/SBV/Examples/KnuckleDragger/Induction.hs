2e15acc9bff114d3307f369e691624a3607419cf
Levent Erkok <erkokl@gmail.com>
2024-08-31 19:13:02 -0700

    Add an example of what happens if proof fails

--- Documentation/SBV/Examples/KnuckleDragger/Induction.hs
+++ Documentation/SBV/Examples/KnuckleDragger/Induction.hs
@@ -13,7 +13,7 @@
 {-# LANGUAGE DataKinds        #-}
 {-# LANGUAGE TypeAbstractions #-}
 
-{-# OPTIONS_GHC -Wall -Werror #-}
+{-# OPTIONS_GHC -Wall -Werror -Wno-unused-do-bind #-}
 
 module Documentation.SBV.Examples.KnuckleDragger.Induction where
 
@@ -24,6 +24,11 @@ import Data.SBV.Tools.KnuckleDragger
 import Data.SBV.Tools.KnuckleDragger.Induction
 
 import qualified Data.SBV.List as SL
+
+-- $setup
+-- >>> -- For doctest purposes only:
+-- >>> :set -XScopedTypeVariables
+-- >>> import Control.Exception
 
 -- | Prove that sum of numbers from @0@ to @n@ is @n*(n-1)/2@.
 --
@@ -90,3 +95,34 @@ listLengthProof = do
    induct <- inductionPrinciple p
 
    lemma "length_correct" (\(Forall @"xs" xs) -> p xs) [induct]
+
+-- | It is instructive to see what kind of counter-example we get if a lemma fails to prove.
+-- Below, we do a variant of the 'listLengthProof', but with a bad implementation, and see the
+-- counter-example. Our implementation returns an incorrect answer if the given list is longer
+-- than 5 elements and have 42 in it. We have:
+--
+-- >>> badProof `catch` (\(_ :: SomeException) -> pure ())
+-- Axiom: List(a).induction                       Admitted.
+-- Lemma: bad
+-- *** Failed to prove bad.
+-- Falsifiable. Counter-example:
+--   xs   = [42,99,100,101,59,102] :: [Integer]
+--   imp  =                     42 :: Integer
+--   spec =                      6 :: Integer
+badProof :: IO ()
+badProof = do
+   let length :: SList Integer -> SInteger
+       length = smtFunction "length" $ \xs -> ite (SL.length xs .> 5 .&& 42 `SL.elem` xs) 42
+                                            $ ite (SL.null xs) 0 (1 + length (SL.tail xs))
+
+       spec :: SList Integer -> SInteger
+       spec = SL.length
+
+       p :: SList Integer -> SBool
+       p xs = observe "imp" (length xs) .== observe "spec" (spec xs)
+
+   induct <- inductionPrinciple p
+
+   lemma "bad" (\(Forall @"xs" xs) -> p xs) [induct]
+
+   pure ()
