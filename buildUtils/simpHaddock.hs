6660f647b2e0a9ec56b0368cd1ad36f0bfb594f3
Levent Erkok <erkokl@gmail.com>
2024-09-01 09:24:12 -0700

    build docs: Fail if a line is not 100%

--- buildUtils/simpHaddock.hs
+++ buildUtils/simpHaddock.hs
@@ -13,10 +13,15 @@ msg s | verbose = putStrLn $ "HADDOCK SIMPLIFY: " ++ s
       | True    = pure ()
 
 main :: IO ()
-main = do
+main = go False
+
+go :: Bool -> IO ()
+go failed = do
   mbLn <- (Just <$> getLine) `C.catch` (\(_ :: C.SomeException) -> return Nothing)
   case mbLn of
-    Nothing -> pure ()
+    Nothing -> if failed
+                  then error "*** There were files that had missing haddock-documentation. Stopping."
+                  else pure ()
     Just ln -> if "Warning: '" `isPrefixOf` ln && "' is ambiguous. It is defined" `isSuffixOf` ln
                   then do l1 <- getLine
                           l2 <- getLine
@@ -26,13 +31,22 @@ main = do
                           if    l1 == "    You may be able to disambiguate the identifier by qualifying it or"
                              && l2 == "    by specifying the type/value namespace explicitly."
                              then do msg "Skipped lines"
-                                     main
+                                     go failed
                              else do msg ln
                                      msg l1
                                      msg l2
                                      msg l3
                                      msg l4
                                      msg l5
-                                     main
+                                     go failed
                   else do putStrLn ln
-                          main
+                          go (failed || bad ln)
+
+bad :: String -> Bool
+bad ln
+  | '%' `notElem` ln
+  = False
+  | True
+  = case words ln of
+     "100%" : _ -> False
+     _          -> True
