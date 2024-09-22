2e70ba589263057496b0ca8829da4156dd385358
Levent Erkok <erkokl@gmail.com>
2024-09-21 18:39:35 -0700

    KD: Basic examples, including first-order logic

---
+++ Documentation/SBV/Examples/KnuckleDragger/Basics.hs
@@ -0,0 +1,179 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module    : Documentation.SBV.Examples.KnuckleDragger.Basics
+-- Copyright : (c) Levent Erkok
+-- License   : BSD3
+-- Maintainer: erkokl@gmail.com
+-- Stability : experimental
+--
+-- Some basic KD usage.
+-----------------------------------------------------------------------------
+
+{-# LANGUAGE CPP                #-}
+{-# LANGUAGE DataKinds          #-}
+{-# LANGUAGE DeriveAnyClass     #-}
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE TemplateHaskell    #-}
+{-# LANGUAGE TypeAbstractions   #-}
+{-# LANGUAGE StandaloneDeriving #-}
+
+{-# OPTIONS_GHC -Wall -Werror #-}
+
+module Documentation.SBV.Examples.KnuckleDragger.Basics where
+
+import Data.SBV
+import Data.SBV.Tools.KnuckleDragger
+
+#ifndef HADDOCK
+-- $setup
+-- >>> -- For doctest purposes only:
+-- >>> :set -XScopedTypeVariables
+-- >>> import Control.Exception
+#endif
+
+-- | @sTrue@ is provable.
+--
+-- We have:
+--
+-- >>> trueIsProvable
+-- Lemma: true                             Q.E.D.
+-- [Proven] true
+trueIsProvable :: IO Proof
+trueIsProvable = runKD $ lemma "true" sTrue []
+
+-- | @sFalse@ isn't provable.
+--
+-- We have:
+--
+-- >>> falseIsn'tProvable `catch` (\(_ :: SomeException) -> pure ())
+-- Lemma: sFalse
+-- *** Failed to prove sFalse.
+-- Falsifiable
+falseIsn'tProvable :: IO ()
+falseIsn'tProvable = runKD $ do
+        _won'tGoThrough <- lemma "sFalse" sFalse []
+        pure ()
+
+-- | Basic quantification example: For every integer, there's a larger integer.
+--
+-- We have:
+-- >>> largerIntegerExists
+-- Lemma: largerIntegerExists              Q.E.D.
+-- [Proven] largerIntegerExists
+largerIntegerExists :: IO Proof
+largerIntegerExists = runKD $ lemma "largerIntegerExists"
+                                    (\(Forall @"x" x) (Exists @"y" y) -> x .< (y :: SInteger))
+                                    []
+
+-- | Use an uninterpreted type for the domain
+data T
+mkUninterpretedSort ''T
+
+-- | Pushing a universal through conjunction. We have:
+--
+-- >>> forallConjunction
+-- Lemma: forallConjunction                Q.E.D.
+-- [Proven] forallConjunction
+forallConjunction :: IO Proof
+forallConjunction = runKD $ do
+    let p, q :: ST -> SBool
+        p = uninterpret "p"
+        q = uninterpret "q"
+
+        qb = quantifiedBool
+
+    lemma "forallConjunction"
+           (      (qb (\(Forall @"x" x) -> p x) .&& qb (\(Forall @"x" x) -> q x))
+            .<=> -----------------------------------------------------------------
+                          qb (\(Forall @"x" x) -> p x .&& q x)
+           )
+           []
+
+-- | Pushing an existential through disjunction. We have:
+--
+-- >>> existsDisjunction
+-- Lemma: existsDisjunction                Q.E.D.
+-- [Proven] existsDisjunction
+existsDisjunction :: IO Proof
+existsDisjunction = runKD $ do
+    let p, q :: ST -> SBool
+        p = uninterpret "p"
+        q = uninterpret "q"
+
+        qb = quantifiedBool
+
+    lemma "existsDisjunction"
+           (      (qb (\(Exists @"x" x) -> p x) .|| qb (\(Exists @"x" x) -> q x))
+            .<=> -----------------------------------------------------------------
+                          qb (\(Exists @"x" x) -> p x .|| q x)
+           )
+           []
+
+-- | We cannot push a universal through a disjunction. We have:
+--
+-- >>> forallDisjunctionNot `catch` (\(_ :: SomeException) -> pure ())
+-- Lemma: forallConjunctionNot
+-- *** Failed to prove forallConjunctionNot.
+-- Falsifiable. Counter-example:
+--   p :: T -> Bool
+--   p T!val!2 = True
+--   p T!val!0 = True
+--   p _       = False
+-- <BLANKLINE>
+--   q :: T -> Bool
+--   q T!val!2 = False
+--   q T!val!0 = False
+--   q _       = True
+--
+-- Note how @p@ assigns two selected values to @True@ and everything else to @False@, while @q@ does the exact opposite.
+-- So, there is no common value that satisfies both, providing a counter-example. (It's not clear why the solver finds
+-- a model with two distinct values, as one would have sufficed. But it is still a valud model.)
+forallDisjunctionNot :: IO ()
+forallDisjunctionNot = runKD $ do
+    let p, q :: ST -> SBool
+        p = uninterpret "p"
+        q = uninterpret "q"
+
+        qb = quantifiedBool
+
+    -- This won't prove!
+    _won'tGoThrough <- lemma "forallConjunctionNot"
+                             (      (qb (\(Forall @"x" x) -> p x) .|| qb (\(Forall @"x" x) -> q x))
+                              .<=> -----------------------------------------------------------------
+                                              qb (\(Forall @"x" x) -> p x .|| q x)
+                             )
+                             []
+
+    pure ()
+
+-- | We cannot push an existential through conjunction. We have:
+--
+-- >>> existsConjunctionNot `catch` (\(_ :: SomeException) -> pure ())
+-- Lemma: existsConjunctionNot
+-- *** Failed to prove existsConjunctionNot.
+-- Falsifiable. Counter-example:
+--   p :: T -> Bool
+--   p T!val!1 = False
+--   p _       = True
+-- <BLANKLINE>
+--   q :: T -> Bool
+--   q T!val!1 = True
+--   q _       = False
+--
+-- In this case, we again have a predicate That disagree at every point, providing a counter-example.
+existsConjunctionNot :: IO ()
+existsConjunctionNot = runKD $ do
+    let p, q :: ST -> SBool
+        p = uninterpret "p"
+        q = uninterpret "q"
+
+        qb = quantifiedBool
+
+    _wont'GoThrough <- lemma "existsConjunctionNot"
+                             (      (qb (\(Exists @"x" x) -> p x) .&& qb (\(Exists @"x" x) -> q x))
+                              .<=> -----------------------------------------------------------------
+                                              qb (\(Exists @"x" x) -> p x .&& q x)
+                             )
+                            []
+
+    pure ()
