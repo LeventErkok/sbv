5812cd7ae6780d41e71e185ecd984e528bdf05b4
Levent Erkok <erkokl@gmail.com>
2025-03-10 09:53:58 -0700

    Simplify

--- Documentation/SBV/Examples/KnuckleDragger/StrongInduction.hs
+++ Documentation/SBV/Examples/KnuckleDragger/StrongInduction.hs
@@ -71,19 +71,29 @@ oddSequence1 = runKD $ do
 -- We have:
 --
 -- >>> oddSequence2
--- Inductive lemma (strong): oddSequence
---   Base: oddSequence.Base                Q.E.D.
+-- Lemma: oddSequence_0                    Q.E.D.
+-- Lemma: oddSequence_1                    Q.E.D.
+-- Inductive lemma (strong): oddSequence_sNp2
+--   Base: oddSequence_sNp2.Base           Q.E.D.
 --   Asms: 1                               Q.E.D.
---   Step 1: Case split one way:
---     Case [1 of 1]: n[1]                 Q.E.D.
---     Completeness:                       Q.E.D.
+--   Step: 1                               Q.E.D.
+--   Asms: 2                               Q.E.D.
 --   Step: 2                               Q.E.D.
+--   Asms: 3                               Q.E.D.
 --   Step: 3                               Q.E.D.
 --   Step: 4                               Q.E.D.
 --   Step: 5                               Q.E.D.
 --   Step: 6                               Q.E.D.
 --   Step: 7                               Q.E.D.
---   Step: oddSequence.Step                Q.E.D.
+--   Step: oddSequence_sNp2.Step           Q.E.D.
+-- Lemma: oddSequence
+--   Asms  : 1                             Q.E.D.
+--   Step 1: Case split 3 ways:
+--     Case [1 of 3]: n[1]                 Q.E.D.
+--     Case [2 of 3]: n[2]                 Q.E.D.
+--     Case [3 of 3]: n[3]                 Q.E.D.
+--     Completeness:                       Q.E.D.
+--   Result:                               Q.E.D.
 -- [Proven] oddSequence
 oddSequence2 :: IO Proof
 oddSequence2 = runKD $ do
@@ -92,21 +102,31 @@ oddSequence2 = runKD $ do
                                   $ ite (n .== 1) 3
                                   $ 2 * s (n-1) - s (n-2)
 
-  sInduct "oddSequence"
-          (\(Forall @"n" n) -> n .>= 0 .=> s n .== 2 * n + 1) $
-          \ih n -> [n .>= 0] |- s (n+1)
-                             ?? [cases "n" [n+1 .< 2],  hyp (n .>= 0)]
-                             =: let focus v = ite (n+1 .<= 0) 1 (ite (n + 1 .== 1) 3 v)
-                             in focus (2 * s n - s (n-1))
-                             ?? ih `at` Inst @"n" n
-                             =: focus (2 * (2 * n + 1) - s (n - 1))
-                             ?? ih `at` Inst @"n" (n-1)
-                             =: focus (2 * (2 * n + 1) - (2 * (n - 1) + 1))
-                             =: focus (4 * n + 2 - (2 * n - 1))
-                             =: focus (4 * n + 2 - 2 * n + 1)
-                             =: focus (2 * n + 2 + 1)
-                             =: focus (2 * (n + 1) + 1)
-                             =: qed
+  s0 <- lemma "oddSequence_0" (s 0 .== 1) []
+  s1 <- lemma "oddSequence_1" (s 1 .== 3) []
+
+  sNp2 <- sInduct "oddSequence_sNp2"
+                  (\(Forall @"n" n) -> n .>= 2 .=> s n .== 2 * n + 1) $
+                  \ih n -> [n .>= 2] |- s (n+1)                  ?? n .>= 2
+                                     =: 2 * s n - s (n-1)        ?? [hyp (n .>= 2), hprf (ih `at` Inst @"n" n)    ]
+                                     =: 2*(2*n+1) - s (n-1)      ?? [hyp (n .>= 2), hprf (ih `at` Inst @"n" (n-1))]
+                                     =: 2*(2*n+1)-(2*(n-1) + 1)
+                                     =: 4*n+2-(2*n-1)
+                                     =: 4*n+2-2*n+1
+                                     =: 2*n+2+1
+                                     =: 2*(n+1)+1
+                                     =: qed
+
+  calc "oddSequence" (\(Forall @"n" n) -> n .>= 0 .=> s n .== 2 * n + 1) $
+                     \n -> [n .>= 0] |- s n
+                                     ?? [ cases "n" [n .== 0, n .== 1, n .>= 2]
+                                        , hyp (n .>= 0)
+                                        , hprf s0
+                                        , hprf s1
+                                        , hprf $ sNp2 `at` Inst @"n" n
+                                        ]
+                                     =: 2 * n + 1
+                                     =: qed
 
 -- | For strong induction to work, We have to instantiate the proof at a "smaller" value. This
 -- example demonstrates what happens if we don't. We have:
