a14f520491104d7deae52d22e7a9c040a451fb40
Levent Erkok <erkokl@gmail.com>
2025-05-05 09:14:40 -0700

    KD QC: Restructuring. WIP.

--- Documentation/SBV/Examples/KnuckleDragger/QuickSort.hs
+++ Documentation/SBV/Examples/KnuckleDragger/QuickSort.hs
@@ -18,11 +18,12 @@
 
 module Documentation.SBV.Examples.KnuckleDragger.QuickSort where
 
-import Data.SBV
-import Data.SBV.Tools.KnuckleDragger
+import Prelude hiding (null, (++), head, tail, elem, notElem, fst, length)
 
-import Prelude hiding (null, filter, (++), head, tail, length, all)
-import Data.SBV.List
+import Data.SBV
+import Data.SBV.List hiding (partition)
+import Data.SBV.Tuple
+import Data.SBV.Tools.KnuckleDragger
 
 -- * Quick sort
 
@@ -30,29 +31,21 @@ import Data.SBV.List
 quickSort :: SList Integer -> SList Integer
 quickSort = smtFunction "quickSort" $ \l -> ite (null l)
                                                 nil
-                                                (let (x, xs) = uncons l
-                                                 in    quickSort (filterLT x xs)
-                                                    ++ singleton x
-                                                    ++ quickSort (filterGE x xs))
+                                                (let (x,  xs) = uncons l
+                                                     (lo, hi) = untuple (partition x xs)
+                                                 in  quickSort lo ++ singleton x ++ quickSort hi)
 
--- | Filter all elements that are less than the first argument. Unfortunately we can't just define
--- this as @filter (.< e)@. Why? Because SBV firstifies higher-order calls, and the argument to these
--- functions would become free variables. This is currently not supported, hence the need for explicit definitions.
-filterLT :: SInteger -> SList Integer -> SList Integer
-filterLT = smtFunction "filterLT" $ \e l -> ite (null l)
-                                                nil
-                                                (let (x, xs) = uncons l
-                                                     rest    = filterLT e xs
-                                                 in ite (x .< e) (x .: rest) rest)
-
--- | Filter all elements that are greater than or equal to the first argument. Unfortunately we can't just define
--- this as @filter (.>= e)@ for the same reasons quoted above for 'filterLT.
-filterGE :: SInteger -> SList Integer -> SList Integer
-filterGE = smtFunction "filterMoreEq" $ \e l -> ite (null l)
-                                                    nil
-                                                    (let (x, xs) = uncons l
-                                                         rest    = filterGE e xs
-                                                     in ite (x .>= e) (x .: rest) rest)
+-- | We define @partition@ as an explicit function. Unfortunately, we can't just replace this
+-- with @\pivot xs -> Data.List.SBV.partition (.< pivot) xs@ because that would create a firstified version of partition
+-- with a free-variable captured, which isn't supported due to higher-order limitations in SMTLib.
+partition :: SInteger -> SList Integer -> STuple [Integer] [Integer]
+partition = smtFunction "split" $ \pivot xs -> ite (null xs)
+                                                   (tuple (nil, nil))
+                                                   (let (a,  as) = uncons xs
+                                                        (lo, hi) = untuple (partition pivot as)
+                                                    in ite (a .< pivot)
+                                                           (tuple (a .: lo, hi))
+                                                           (tuple (lo, a .: hi)))
 
 -- * Helper functions
 
@@ -62,14 +55,6 @@ nonDecreasing = smtFunction "nonDecreasing" $ \l ->  null l .|| null (tail l)
                                                  .|| let (x, l') = uncons l
                                                          (y, _)  = uncons l'
                                                      in x .<= y .&& nonDecreasing l'
-
--- | Is the given value less than or equal to all the elements in the list?
-leAll :: SInteger -> SList Integer -> SBool
-leAll = smtFunction "leAll" $ \e l -> null l .|| e .<= head l .&& leAll e (tail l)
-
--- | Is the given value greater than all the elements in the list?
-gtAll :: SInteger -> SList Integer -> SBool
-gtAll = smtFunction "gtAll" $ \e l -> null l .|| e .>  head l .&& gtAll e (tail l)
 
 -- | Count the number of occurrences of an element in a list
 count :: SInteger -> SList Integer -> SInteger
@@ -82,6 +67,14 @@ count = smtFunction "count" $ \e l -> ite (null l)
 -- | Are two lists permutations of each other?
 isPermutation :: SList Integer -> SList Integer -> SBool
 isPermutation xs ys = quantifiedBool (\(Forall @"x" x) -> count x xs .== count x ys)
+
+-- | Is the given value less than or equal to all the elements in the list?
+leAll :: SInteger -> SList Integer -> SBool
+leAll = smtFunction "leAll" $ \e l -> null l .|| e .<= head l .&& leAll e (tail l)
+
+-- | Is the given value greater than all the elements in the list?
+gtAll :: SInteger -> SList Integer -> SBool
+gtAll = smtFunction "gtAll" $ \e l -> null l .|| e .>  head l .&& gtAll e (tail l)
 
 -- * Correctness proof
 
@@ -97,6 +90,7 @@ correctness = runKDWith z3{kdOptions = (kdOptions z3) {ribbonLength = 60}} $ do
     -- Part I. Prove that the output of quick sort is non-decreasing.
     --------------------------------------------------------------------------------------------
 
+{-
     _onDecreasingCons <-
         lemma "nonDecreasingCons"
               (\(Forall @"xs" xs) (Forall @"e" e) ->
@@ -109,49 +103,17 @@ correctness = runKDWith z3{kdOptions = (kdOptions z3) {ribbonLength = 60}} $ do
                   nonDecreasing xs .&& nonDecreasing ys .&& sNot (null ys) .&& gtAll (head ys) xs .=> nonDecreasing (xs ++ ys))
               [sorry]
 
-    filterLTWorks <-
-        lemma "filterLTWorks"
-               (\(Forall @"e" e) (Forall @"l" l) -> gtAll e (quickSort (filterLT e l)))
-               [sorry]
-
-    filterGEWorks <-
-        lemma "filterGEWorks"
-               (\(Forall @"e" e) (Forall @"l" l) -> leAll e (quickSort (filterGE e l)))
-               [sorry]
-
-    filterLTShorter <-
-        lemma "filterLTShorter"
-              (\(Forall @"xs" xs) (Forall @"x" x) -> length (filterLT x xs) .< length (x .: xs))
-              [sorry]
-
-    filterGEShorter <-
-        lemma "filterGEShorter"
-              (\(Forall @"xs" xs) (Forall @"x" x) -> length (filterGE x xs) .< length (x .: xs))
-              [sorry]
-
-    nonDecreasingJoin <-
-        lemma "nonDecreasingJoin"
-              (\(Forall @"xs" xs) (Forall @"e" e) (Forall @"ys" ys) ->
-                 nonDecreasing xs .&& gtAll e xs .&& leAll e ys .&& nonDecreasing ys .=>
-                    nonDecreasing (xs ++ singleton e ++ ys))
-              [sorry]
-
-    quickSortKeepsSize <-
-        lemma "quickSortKeepsSize"
-              (\(Forall @"l" l) -> length (quickSort l) .== length l)
-              [sorry]
-
     sortNonDecreasing <-
         sInductWith cvc5 "sortNonDecreasing"
                 (\(Forall @"l" l) -> nonDecreasing (quickSort l))
                 (length @Integer) $
-                \ih l -> [] |-
+                \_h l -> [] |-
                      split l trivial
                            (\x xs -> nonDecreasing (quickSort (x .: xs))
                                   ?? "unfold"
-                                  =: let left  = quickSort (filterLT x xs)
-                                         right = quickSort (filterGE x xs)
-                                  in nonDecreasing (left ++ singleton x ++ right)
+                                  =: let (lo, hi) = untuple (partition x xs)
+                                  in nonDecreasing (quickSort lo ++ singleton x ++ quickSort hi)
+                                  {-
                                   ?? [ hprf $ ih                 `at`  Inst @"l" (filterLT x xs)
                                      , hprf $ ih                 `at`  Inst @"l" (filterGE x xs)
                                      , hprf $ quickSortKeepsSize `at` (Inst @"l" (filterLT x xs))
@@ -162,7 +124,7 @@ correctness = runKDWith z3{kdOptions = (kdOptions z3) {ribbonLength = 60}} $ do
                                      , hprf $ filterGEShorter    `at` (Inst @"xs" xs, Inst @"x" x)
                                      , hprf $ nonDecreasingJoin  `at` (Inst @"xs" left, Inst @"e" x, Inst @"ys" right)
                                      ]
-                                  =: sTrue
+                                  -}
                                   =: qed)
 
     --------------------------------------------------------------------------------------------
@@ -179,9 +141,66 @@ correctness = runKDWith z3{kdOptions = (kdOptions z3) {ribbonLength = 60}} $ do
                                               =: count e (x .: xs)
                                               =: qed)
 
-    --------------------------------------------------------------------------------------------
-    -- Put the two parts together for the final proof
-    --------------------------------------------------------------------------------------------
-    lemma "quickSortIsCorrect"
-          (\(Forall @"xs" xs) -> let out = quickSort xs in nonDecreasing out .&& isPermutation xs out)
-          [sortNonDecreasing, sortIsPermutation]
+    filterLTShorter <-
+        lemma "filterLTShorter"
+              (\(Forall @"xs" xs) (Forall @"x" x) -> length (filterLT x xs) .< length (x .: xs))
+              [sorry]
+
+    filterGEShorter <-
+        lemma "filterGEShorter"
+              (\(Forall @"xs" xs) (Forall @"x" x) -> length (filterGE x xs) .< length (x .: xs))
+              [sorry]
+-}
+
+    partitionWorks <-
+        lemma "partitionWorks"
+              (\(Forall @"pivot" pivot) (Forall @"xs" xs) ->
+                   let (lo, hi) = untuple (partition pivot xs)
+                   in gtAll pivot lo .&& leAll pivot hi)
+              [sorry]
+
+    {-
+    quickSortKeepsSize <-
+        lemma "quickSortKeepsSize"
+              (\(Forall @"l" l) -> length (quickSort l) .== length l)
+              [sorry]
+              -}
+
+    nonDecreasingJoin <-
+        lemma "nonDecreasingJoin"
+              (\(Forall @"lo" lo) (Forall @"pivot" pivot) (Forall @"hi" hi) ->
+                 nonDecreasing lo .&& gtAll pivot lo .&& leAll pivot hi .&& nonDecreasing hi .=>
+                    nonDecreasing (lo ++ singleton pivot ++ hi))
+              [sorry]
+
+    helper1 <- lemma "helper1" (\(Forall @"pivot" pivot) (Forall @"xs" xs) -> gtAll pivot xs .=> gtAll pivot (quickSort xs)) [sorry]
+    helper2 <- lemma "helper2" (\(Forall @"pivot" pivot) (Forall @"xs" xs) -> leAll pivot xs .=> leAll pivot (quickSort xs)) [sorry]
+
+    sInductWith cvc5 "quickSortIsCorrect"
+       (\(Forall @"xs" xs) -> let out = quickSort xs in nonDecreasing out .&& isPermutation xs out)
+       (length @Integer) $
+       \ih xs -> [] |-
+           split xs
+                 trivial
+                 (\pivot as -> let (lo, hi) = untuple (partition pivot as)
+                                   out      = quickSort lo ++ singleton pivot ++ quickSort hi
+                            in nonDecreasing out .&& isPermutation (pivot .: as) out
+                            ?? [ hprf $ ih                `at` Inst @"xs" lo
+                               , hprf $ ih                `at` Inst @"xs" hi
+                               , hprf $ partitionWorks    `at` (Inst @"pivot" pivot, Inst @"xs" as)
+                               , hprf $ nonDecreasingJoin `at` (Inst @"lo" (quickSort lo), Inst @"pivot" pivot, Inst @"hi" (quickSort hi))
+                               , hprf $ helper1           `at` (Inst @"pivot" pivot, Inst @"xs" lo)
+                               , hprf $ helper2           `at` (Inst @"pivot" pivot, Inst @"xs" hi)
+                               , hcmnt "jump"
+                               ]
+                            =: isPermutation (pivot .: as) out
+                            =: qed)
+
+-- nonDecreasing (quickSort lo)
+-- isPermutation lo (quickSort lo)
+-- nonDecreasing (quickSort hi)
+-- isPermutation hi (quickSort hi)
+-- gtAll pivot (fst (partition pivot as))
+-- leAll pivot (snd (partition pivot as))
+-- gtAll pivot (quickSort (fst (partition pivot as)))
+-- leAll pivot (quickSort (snd (partition pivot as)))
