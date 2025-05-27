26893133c569a914bfcb2712d39fcc21f9ab12ea
Levent Erkok <erkokl@gmail.com>
2025-05-26 17:08:39 -0700

    finish up proof

--- Documentation/SBV/Examples/KnuckleDragger/McCarthy91.hs
+++ Documentation/SBV/Examples/KnuckleDragger/McCarthy91.hs
@@ -38,27 +38,23 @@ spec91 n = ite (n .> 100) (n - 10) 91
 -- and strong induction. We have:
 --
 -- >>> correctness
+-- Lemma: case1                            Q.E.D.
+-- Lemma: case2                            Q.E.D.
+-- Inductive lemma (strong): case3
+--   Step: Measure is non-negative         Q.E.D.
+--   Step: 1 (unfold)                      Q.E.D.
+--   Step: 2                               Q.E.D.
+--   Result:                               Q.E.D.
+-- Lemma: mcCarthy91
+--   Step: 1 (3 way case split)
+--     Step: 1.1                           Q.E.D.
+--     Step: 1.2                           Q.E.D.
+--     Step: 1.3                           Q.E.D.
+--     Step: 1.Completeness                Q.E.D.
+--   Result:                               Q.E.D.
+-- [Proven] mcCarthy91
 correctness :: IO Proof
 correctness = runKD $ do
-
-   -- We start by establishing that @n < mcCarthy91 n + 11@. This will come in handy when we do the induction later.
-   smaller <- sInduct "smaller"
-                      (\(Forall @"n" n) -> n .< mcCarthy91 n + 11)
-                      (\(n :: SInteger) -> ite (n .<= 111) (111 - n) 0) $
-                      \ih n -> [] |- n + 1 .< mcCarthy91 (n + 1) + 11
-                                  =: cases [ n+1 .> 100  ==> n + 1 .< n + 1 - 10 + 11
-                                                          =: n .< n + 1
-                                                          =: sTrue
-                                                          =: qed
-                                           , n+1 .<= 100 ==> n + 1 .< mcCarthy91 (mcCarthy91 (n + 12)) + 11
-                                                          =: n .< mcCarthy91 (mcCarthy91 (n + 12)) + 10
-                                                          ?? ih `at` Inst @"n" (mcCarthy91 (n + 12))
-                                                          ?? "bad"
-                                                          =: mcCarthy91 (n + 12) .< n + 1
-                                                          ?? ih `at` Inst @"n" (n + 12)
-                                                          =: sTrue
-                                                          =: qed
-                                           ]
 
    -- Case 1. When @n > 100@
    case1 <- lemma "case1" (\(Forall @"n" n) -> n .>= 100 .=> mcCarthy91 n .== spec91 n) []
@@ -66,17 +62,15 @@ correctness = runKD $ do
    -- Case 2. When @90 <= n <= 100@
    case2 <- lemma "case2" (\(Forall @"n" n) -> 90 .<= n .&& n .<= 100 .=> mcCarthy91 n .== spec91 n) []
 
-   -- Case 3. When @n < 90@
+   -- Case 3. When @n < 90@. The crucial point here is the measure, which makes sure 101 < 100 < 99 < ...
    case3 <- sInduct "case3"
                     (\(Forall @"n" n) -> n .< 90 .=> mcCarthy91 n .== spec91 n)
-                    (\(n :: SInteger) -> 101 - n) $
+                    (\(n :: SInteger) -> abs (101 - n)) $
                     \ih n -> [n .< 90] |- mcCarthy91 n
                                        ?? "unfold"
                                        =: mcCarthy91 (mcCarthy91 (n + 11))
-                                       ?? [ smaller `at` Inst @"n" (mcCarthy91 (n + 11))
-                                          , ih      `at` Inst @"n" (n + 11)
-                                          ]
-                                       =: spec91 (n + 11)
+                                       ?? ih `at` Inst @"n" (n + 11)
+                                       =: mcCarthy91 91
                                        =: qed
 
    -- Putting it all together
