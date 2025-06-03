5463e1c30481607d6cffeca6a8f1523d7badae9b
Levent Erkok <erkokl@gmail.com>
2025-06-03 14:12:17 -0700

    cvc5 does better here

--- Documentation/SBV/Examples/TP/BinarySearch.hs
+++ Documentation/SBV/Examples/TP/BinarySearch.hs
@@ -108,7 +108,7 @@ inArray arr (low, high) elt = quantifiedBool $ \(Exists @"i" i) -> low .<= i .&&
 --   Result:                                         Q.E.D.
 -- [Proven] bsearchCorrect
 correctness :: IO Proof
-correctness = runTPWith (tpRibbon 50 z3) $ do
+correctness = runTPWith (tpRibbon 50 cvc5) $ do
 
   -- Helper: if a value is not in a range, then it isn't in any subrange of it:
   notInRange <- lemma "notInRange"
