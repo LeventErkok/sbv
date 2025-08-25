d47f7910cd4e01d2da4021b7769dd51636331401
Levent Erkok <erkokl@gmail.com>
2025-08-25 07:23:21 -0700

    simple ADT doc file

---
+++ Documentation/SBV/Examples/ADT/Basic.hs
@@ -0,0 +1,35 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module    : Documentation.SBV.Examples.ADT.Basic
+-- Copyright : (c) Levent Erkok
+-- License   : BSD3
+-- Maintainer: erkokl@gmail.com
+-- Stability : experimental
+--
+-- Basic ADT examples.
+-----------------------------------------------------------------------------
+{-# OPTIONS_GHC -Wall -Werror #-}
+
+{-# LANGUAGE TemplateHaskell #-}
+
+module Documentation.SBV.Examples.ADT.Basic where
+
+import Data.SBV
+
+-- | A basic arithmetic expression type
+data Expr = Var String
+          | Num Integer
+          | Add Expr Expr
+          | Let String Expr Expr
+
+-- | Make it a symbolic value
+mkSymbolicADT ''Expr
+
+-- | Create two different values:
+--
+-- >>> test
+-- Satisfiable. Model:
+--   s0 = Add (Num 0) (Num 0) :: Expr
+--   s1 =               Num 0 :: Expr
+test :: IO SatResult
+test = sat $ \x y -> x ./= (y :: SExpr)
