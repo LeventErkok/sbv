43a403e186b8778e1f223e29025aa544f5f05348
Levent Erkok <erkokl@gmail.com>
2025-08-28 14:03:19 -0700

    Simplify Expr type and update test in Basic.hs
    Removed unused constructors from the Expr data type and updated the test function to use proveWith instead of satWith. Also removed the DataKinds language extension as it is no longer needed.

--- Documentation/SBV/Examples/ADT/Basic.hs
+++ Documentation/SBV/Examples/ADT/Basic.hs
@@ -10,7 +10,6 @@
 -----------------------------------------------------------------------------
 {-# OPTIONS_GHC -Wall -Werror #-}
 
-{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TemplateHaskell #-}
 
 module Documentation.SBV.Examples.ADT.Basic where
@@ -18,28 +17,15 @@ module Documentation.SBV.Examples.ADT.Basic where
 import Data.SBV
 
 -- | A basic arithmetic expression type
-data Expr = Var String
-          | Num Integer
+data Expr = Num Integer
+          | Var String
           | Add Expr Expr
           | Let String Expr Expr
-          | Interval (Expr, Expr)
-          | CL [Expr]
-          | CM (Maybe (Expr, Integer))
-          | CE (Either Integer Expr)
-          | W8   Word8
-          | I65  [WordN 5]
-          | FP16 [FloatingPoint 5 10]
-          | KReal     AlgReal
-          | KRational Rational
 
--- | Make it a symbolic value
 mkSymbolic ''Expr
 
 -- | Create two different values:
 --
 -- >>> test
--- Satisfiable. Model:
---   s0 = Add (Num 0) (Num 0) :: Expr
---   s1 =               Num 0 :: Expr
-test :: IO SatResult
-test = satWith z3{verbose=True} $ \x y -> x ./== (y :: SExpr) .&& y ./= literal (W8 0)
+test :: IO ThmResult
+test = proveWith z3{verbose=True} $ \x y -> x ./== (y :: SExpr) -- .&& y ./= literal (Num 3)
