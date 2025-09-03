6833048dfa31feec1b83c9985f906a979b15c659
Levent Erkok <erkokl@gmail.com>
2025-09-03 09:10:42 -0700

    Refactor ADT example and add variable validation
    Removed unused language extensions and code, and added the isValid function to ensure variables are scoped and valid in symbolic expressions. The test now constrains expressions to be valid, improving correctness and clarity.

--- Documentation/SBV/Examples/ADT/Basic.hs
+++ Documentation/SBV/Examples/ADT/Basic.hs
@@ -8,25 +8,18 @@
 --
 -- Basic ADT examples.
 -----------------------------------------------------------------------------
-{-# OPTIONS_GHC -Wall -Werror -Wno-partial-type-signatures #-}
+{-# OPTIONS_GHC -Wall -Werror #-}
 
-{-# LANGUAGE DeriveAnyClass      #-}
-{-# LANGUAGE DeriveDataTypeable  #-}
-{-# LANGUAGE FlexibleInstances   #-}
-{-# LANGUAGE StandaloneDeriving  #-}
 {-# LANGUAGE QuasiQuotes         #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TemplateHaskell     #-}
-{-# LANGUAGE PartialTypeSignatures#-}
-{-# LANGUAGE FlexibleContexts     #-}
 
 module Documentation.SBV.Examples.ADT.Basic where
 
 import Data.SBV
+import Data.SBV.RegExp
 import Data.SBV.Tuple
 import qualified Data.SBV.List as SL
-
-import Data.SBV.Control
 
 -- | A basic arithmetic expression type
 data Expr = Num Integer
@@ -34,18 +27,7 @@ data Expr = Num Integer
           | Add Expr Expr
           | Let String Expr Expr
           deriving Show
-
-reconstruct :: SymVal Expr => String -> [_] -> Expr
-reconstruct "Num" [i]    = Num $ fromCV i
-reconstruct "Var" [s]    = Var $ fromCV s
-reconstruct "Add" [a, b] = Add (fromCV a) (fromCV b)
-reconstruct "Let" [s, a, b] = Let (fromCV s) (fromCV a) (fromCV b)
-reconstruct _ _ = error "reconstruct"
-
 mkSymbolic ''Expr
-
-data I = I1 | I2 deriving (Enum, Bounded)
-mkSymbolic ''I
 
 eval :: SExpr -> SInteger
 eval = go SL.nil
@@ -62,6 +44,19 @@ eval = go SL.nil
                                          $ let (k, v) = untuple (SL.head env)
                                            in ite (s .== k) v (get (SL.tail env) s)
 
+-- Make sure variables are scoped correctly and they are valid
+isValid :: SExpr -> SBool
+isValid = go SL.nil
+  where isId s = s `match` (asciiLower * KStar (asciiLetter + digit))
+        go :: SList String -> SExpr -> SBool
+        go = smtFunction "valid" $ \env expr -> [sCase|Expr expr of
+                                                   Var s     -> isId s .&& s `SL.elem` env
+                                                   Num _     -> sTrue
+                                                   Add l r   -> go env l .&& go env r
+                                                   Let s a b -> isId s .&& go env a .&& go (s SL..: env) b
+                                                |]
+
+
 -- | Create two different values:
 --
 -- >>> test
@@ -72,17 +67,11 @@ test = satWith z3{verbose=True} $ do
 
           q :: SInteger <- free "q"
 
+          constrain $ isValid x
+          constrain $ isValid y
+
           constrain $ x ./== y
           constrain $ y .=== sLet (literal "x") (sNum q) (sAdd (sVar (literal "x")) (sNum 3))
           constrain $ isLet x
           constrain $ eval x .== 3
           constrain $ eval x .== eval y + 5
-
-test2 :: IO ()
-test2 = runSMT $ do a :: SExpr <- free "a"
-                    constrain $ isAdd a
-                    query $ do cs <- checkSat
-                               case cs of
-                                 Sat{} -> do v <- getValue a
-                                             io $ putStrLn $ "Got: " ++ show v
-                                 _     -> error $ "Unexpected: " ++ show cs
