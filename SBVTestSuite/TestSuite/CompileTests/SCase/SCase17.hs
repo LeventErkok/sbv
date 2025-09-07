4be2d12d650285b287fd6d7572cc42a6cbabf705
Levent Erkok <erkokl@gmail.com>
2025-09-07 11:45:42 -0700

    fix test

--- SBVTestSuite/TestSuite/CompileTests/SCase/SCase17.hs
+++ SBVTestSuite/TestSuite/CompileTests/SCase/SCase17.hs
@@ -12,7 +12,7 @@ t e = [sCase|Expr e of
                Zero           -> 0
                Num i | i .< 3 -> i
                Var s          -> ite (s .== literal "a") 1 2
-               Add a b        -> t e + t b
+               Add a b        -> t a + t b
                Num i          -> i+1
                Let _   _a  b  -> t b
       |]
