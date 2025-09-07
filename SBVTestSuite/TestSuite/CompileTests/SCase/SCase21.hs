53cd2a87c414d67231de2dddbc5865cfc3d50bc1
Levent Erkok <erkokl@gmail.com>
2025-09-07 11:29:48 -0700

    more tests

---
+++ SBVTestSuite/TestSuite/CompileTests/SCase/SCase21.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE QuasiQuotes #-}
+
+module T where
+
+import Expr
+import Data.SBV
+
+t :: SExpr -> SInteger
+t e = [sCase|Expr e of
+               Zero           -> 0
+               Num i | i .> 4 -> 4
+               Var s          -> ite (s .== literal "a") 1 2
+               Add a b        -> t e + t b
+               Let _   _a  b  -> t b
+      |]
