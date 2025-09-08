bb182ee298d9410962bdd6020a0f7297bb395944
Levent Erkok <erkokl@gmail.com>
2025-09-07 19:02:51 -0700

    more tests

--- SBVTestSuite/TestSuite/ADT/Expr.hs
+++ SBVTestSuite/TestSuite/ADT/Expr.hs
@@ -25,8 +25,7 @@ import Documentation.SBV.Examples.ADT.Basic
 tests :: TestTree
 tests =
   testGroup "ADT" [
-      goldenCapturedIO "adt_gen00"  t00
-    , goldenCapturedIO "adt_expr00" $ evalCheck (eval e00,  3)
+      goldenCapturedIO "adt_expr00" $ evalCheck (eval e00,  3)
     , goldenCapturedIO "adt_expr01" $ evalCheck (eval e01,  7)
     , goldenCapturedIO "adt_expr02" $ evalCheck (eval e02, 21)
     , goldenCapturedIO "adt_expr03" $ evalCheck (eval e03, 28)
@@ -45,6 +44,19 @@ tests =
     , goldenCapturedIO "adt_expr16" $ evalCheck (f e03, 6)
     , goldenCapturedIO "adt_expr17" $ evalCheck (f e04, 6)
     , goldenCapturedIO "adt_expr18" $ evalCheck (f e05, 6)
+    , goldenCapturedIO "adt_gen00"  t00
+    , goldenCapturedIO "adt_gen01"  $ tSat (-1)
+    , goldenCapturedIO "adt_gen02"  $ tSat 0
+    , goldenCapturedIO "adt_gen03"  $ tSat 1
+    , goldenCapturedIO "adt_gen04"  $ tSat 2
+    , goldenCapturedIO "adt_gen05"  $ tSat 3
+    , goldenCapturedIO "adt_gen06"  $ tSat 4
+    , goldenCapturedIO "adt_gen07"  $ tSat 5
+    , goldenCapturedIO "adt_gen08"  $ tSat 6
+    , goldenCapturedIO "adt_gen09"  $ tSat 7
+    , goldenCapturedIO "adt_gen10"  $ tSat 8
+    , goldenCapturedIO "adt_gen11"  $ tSat 9
+    , goldenCapturedIO "adt_gen12"  $ tSat 100
     ]
     where a = literal "a"
           b = literal "a"
@@ -55,28 +67,6 @@ tests =
           e03 = sLet a e02 (sVar a + e01)        -- 28
           e04 = e03 + sLet a e03 (sVar a + e01)  -- 28 + 28 + 7 = 63
           e05 = sLet b e04 (sVar b * sVar b)     -- 63 * 63 = 3969
-
--- Create something like:
---       let a = _
---    in let b = _
---    in _ + _
--- such that it evaluates to 12
-t00 :: FilePath -> IO ()
-t00 rf = runSMTWith z3{verbose=True, redirectVerbose = Just rf} $ do
-            a :: SExpr <- free "a"
-            constrain $ isValid a
-            constrain $ eval a .== 12
-
-            constrain $ isLet a
-            constrain $ isLet (getLet_3 a)
-            constrain $ isAdd (getLet_3 (getLet_3 a))
-
-            query $ do cs <- checkSat
-                       case cs of
-                         Sat{} -> do v <- getValue a
-                                     io $ do appendFile rf $ "\nGot: " ++ show v
-                                             appendFile rf   "\nDONE\n"
-                         _     -> error $ "Unexpected: " ++ show cs
 
 evalCheck :: SymVal a => (SBV a, a) -> FilePath -> IO ()
 evalCheck (sv, v) rf = runSMTWith z3{verbose=True, redirectVerbose = Just rf} $ do
@@ -108,3 +98,56 @@ f e = [sCase|Expr e of
 
          _                                                   -> 6
       |]
+
+-- Create something like:
+--       let a = _
+--    in let b = _
+--    in _ + _
+-- such that it evaluates to 12
+t00 :: FilePath -> IO ()
+t00 rf = runSMTWith z3{verbose=True, redirectVerbose = Just rf} $ do
+            a :: SExpr <- free "a"
+            constrain $ isValid a
+            constrain $ eval a .== 12
+
+            constrain $ isLet a
+            constrain $ isLet (getLet_3 a)
+            constrain $ isAdd (getLet_3 (getLet_3 a))
+
+            query $ do cs <- checkSat
+                       case cs of
+                         Sat{} -> do v <- getValue a
+                                     io $ do appendFile rf $ "\nGot: " ++ show v
+                                             appendFile rf   "\nDONE\n"
+                         _     -> error $ "Unexpected: " ++ show cs
+
+g :: SExpr -> SInteger
+g e = [sCase|Expr e of
+         Var s     | s .== literal "a"                       -> 0
+                   | s .== literal "b" .|| s .== literal "c" -> 1
+                   | sTrue                                   -> 2
+
+         Num i     | i .<  10                                -> 3
+                   | i .== 10                                -> 4
+                   | i .>  10                                -> 5
+
+         Add _ _ -> 6
+         Mul _ _ -> 7
+         Let{}   -> 8
+
+         _ -> 100
+      |]
+
+-- Show that g can never produce anything but 0..8
+tSat :: Integer -> FilePath -> IO ()
+tSat i rf = runSMTWith z3{verbose=True, redirectVerbose = Just rf} $ do
+              a :: SExpr <- free "a"
+              constrain $ g a .== literal i
+
+              query $ do cs <- checkSat
+                         case cs of
+                           Sat{} -> do v <- getValue a
+                                       io $ do appendFile rf $ "\nGot: " ++ show v
+                                               appendFile rf   "\nDONE\n"
+                           Unsat -> io $ do appendFile rf $ "\nUNSAT\n"
+                           _     -> error $ "Unexpected: " ++ show cs
