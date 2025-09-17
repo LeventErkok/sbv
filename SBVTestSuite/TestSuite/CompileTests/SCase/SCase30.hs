f16120f98166fd89e6be43079e6a4069df91db90
Levent Erkok <erkokl@gmail.com>
2025-09-17 08:15:56 -0700

    Another test

---
+++ SBVTestSuite/TestSuite/CompileTests/SCase/SCase30.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE QuasiQuotes       #-}
+{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE TypeApplications  #-}
+
+{-# OPTIONS_GHC -Wall -Werror #-}
+
+module T where
+
+import Data.SBV
+
+-- Testing constructor/type name conflct
+data A = A Integer
+       | B Float
+       | C A A
+
+mkSymbolic ''A
+
+t :: SA -> SA
+t a = [sCase|A a of
+         A u     -> sA (u+1)
+         B f     -> sB (f+2)
+         C a1 a2 -> sC (t a1) (t a2)
+      |]
