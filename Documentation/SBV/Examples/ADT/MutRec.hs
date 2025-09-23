0169794697ad6215f3a5d5fa474f29b945e83bb9
Levent Erkok <erkokl@gmail.com>
2025-09-23 14:39:44 -0700

    Slightly more interesting example. WIP.

--- Documentation/SBV/Examples/ADT/MutRec.hs
+++ Documentation/SBV/Examples/ADT/MutRec.hs
@@ -22,15 +22,31 @@
 module Documentation.SBV.Examples.ADT.MutRec where
 
 import Data.SBV
+import Data.SBV.Control
 
+-- | Expression layer
 data Expr var val = Con val
                   | Var var
                   | Add (Expr var val) (Expr var val)
                   | Mul (Expr var val) (Expr var val)
-                  | Let (Decl var val) (Expr var val)
+                  | Let (Stmt var val) (Expr var val)
 
-data Decl var val = Assign var (Expr var val)
-                  | Seq        (Decl var val) (Decl var val)
+-- | Statement layer
+data Stmt var val = Assign var (Expr var val)
+                  | Seq        (Stmt var val) (Stmt var val)
 
-mkSymbolic ''Decl
 mkSymbolic ''Expr
+mkSymbolic ''Stmt
+
+exPgm :: IO (Stmt String Integer)
+exPgm = runSMT $ do p :: SStmt String Integer <- free "p"
+
+                    -- Make sure there are at least three statements
+                    constrain $ isSeq p
+                    constrain $ isSeq (getSeq_2 p)
+                    constrain $ isSeq (getSeq_2 (getSeq_2 p))
+
+                    query $ do cs <- checkSat
+                               case cs of
+                                 Sat -> getValue p
+                                 _   -> error $ "Unexpected result: " ++ show cs
