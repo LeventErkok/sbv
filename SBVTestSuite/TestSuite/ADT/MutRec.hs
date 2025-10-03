53db8a1174bd68b2cb869e9595f73b1087c2a869
Levent Erkok <erkokl@gmail.com>
2025-10-03 14:26:07 -0700

    more tests

--- SBVTestSuite/TestSuite/ADT/MutRec.hs
+++ SBVTestSuite/TestSuite/ADT/MutRec.hs
@@ -28,18 +28,19 @@ import Data.Proxy
 
 import Data.SBV.RegExp
 import Data.SBV.Tuple
+import Data.SBV.Maybe
 import qualified Data.SBV.List  as SL
 import qualified Data.SBV.Tuple as ST
 
 -- | Expression layer
-data Expr var val = Con val
-                  | Var var
-                  | Add (Expr var val) (Expr var val)
-                  | Mul (Expr var val) (Expr var val)
+data Expr var val = Con { con :: val }
+                  | Var { var :: var }
+                  | Add { add1 :: Expr var val, add2 :: Expr var val }
+                  | Mul { mul1 :: Expr var val, mul2 :: Expr var val }
 
 -- | Statement layer
-data Stmt var val = Assign var (Expr var val)
-                  | Seq        (Stmt var val) (Stmt var val)
+data Stmt var val = Assign {lhs  :: var,          rhs  :: Expr var val }
+                  | Seq    {seqH :: Stmt var val, seqT :: Stmt var val }
 
 mkSymbolic [''Expr, ''Stmt]
 
@@ -90,33 +91,54 @@ isValid = ST.fst . goS SL.nil
                                                                    in tuple (lv .&& rv, env'')
                                                   |]
 
-exPgm :: FilePath -> Symbolic ()
-exPgm rf = do p :: SStmt String Integer <- free "p"
-
-              registerType (Proxy @(Expr Integer Integer))
-
-              -- Make sure there's some structure to the program:
-              constrain $ isSeq    p
-              constrain $ isSeq    (getSeq_2 p)
-              constrain $ isSeq    (getSeq_2 (getSeq_2 p))
-              constrain $ isAssign (getSeq_2 (getSeq_2 (getSeq_2 p)))
-              constrain $ isAdd    (getAssign_2 (getSeq_2 (getSeq_2 (getSeq_2 p))))
-              constrain $ isVar    (getAdd_1    (getAssign_2 (getSeq_2 (getSeq_2 (getSeq_2 p)))))
-
-              -- Would love to have the following. But it creates too big of a problem.
-              -- constrain $ isValid p
-              constrain $ isValid (sAssign (literal "a") (sCon (literal 1)) :: SStmt String Float)
-
-              query $ do cs <- checkSat
-                         case cs of
-                           Sat -> do r <- getValue p
-                                     io $ do appendFile rf $ "\nGot:\n" ++ show r
-                                             appendFile rf   "\nDONE\n"
-                           _   -> error $ "Unexpected result: " ++ show cs
-
 tests :: TestTree
 tests =
   testGroup "ADT_MR" [
-      goldenCapturedIO "adt_mr00" $ r exPgm
+      goldenCapturedIO "adt_mr00" $ r t00
+    , goldenCapturedIO "adt_mr01" $ r t01
     ]
   where r p rf = runSMTWith z3{verbose=True, redirectVerbose = Just rf} (p rf)
+
+t00 :: FilePath -> Symbolic ()
+t00 rf = do p :: SStmt String Integer <- free "p"
+
+            registerType (Proxy @(Expr Integer Integer))
+
+            -- Make sure there's some structure to the program:
+            constrain $ isSeq    p
+            constrain $ isSeq    (getSeq_2 p)
+            constrain $ isSeq    (getSeq_2 (getSeq_2 p))
+            constrain $ isAssign (getSeq_2 (getSeq_2 (getSeq_2 p)))
+            constrain $ isAdd    (getAssign_2 (getSeq_2 (getSeq_2 (getSeq_2 p))))
+            constrain $ isVar    (getAdd_1    (getAssign_2 (getSeq_2 (getSeq_2 (getSeq_2 p)))))
+
+            -- Would love to have the following. But it creates too big of a problem.
+            -- constrain $ isValid p
+            constrain $ isValid (sAssign (literal "a") (sCon (literal 1)) :: SStmt String Float)
+
+            query $ do cs <- checkSat
+                       case cs of
+                         Sat -> do r <- getValue p
+                                   io $ do appendFile rf $ "\nGot:\n" ++ show r
+                                           appendFile rf   "\nDONE\n"
+                         _   -> error $ "Unexpected result: " ++ show cs
+
+t01 :: FilePath -> Symbolic ()
+t01 rf = do p :: SStmt String (Maybe (Either Integer Bool)) <- free "p"
+
+            constrain $ isAssign  p
+            constrain $ isAdd     (srhs p)
+            constrain $ isCon     (sadd1 (srhs p))
+            constrain $ isCon     (sadd2 (srhs p))
+            constrain $ isNothing (scon (sadd1 (srhs p)))
+            constrain $ isJust    (scon (sadd2 (srhs p)))
+
+            query $ do cs <- checkSat
+                       case cs of
+                         Sat -> do r <- getValue p
+                                   io $ do appendFile rf $ "\nGot:\n" ++ show r
+                                           appendFile rf   "\nDONE\n"
+                         _   -> error $ "Unexpected result: " ++ show cs
+
+_unused :: a
+_unused = undefined con var add1 add2 mul1 mul2 lhs rhs seqH seqT
