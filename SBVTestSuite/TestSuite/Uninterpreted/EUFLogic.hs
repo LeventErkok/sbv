487e484a1644a732596da3f282fe6c23f75004da
Levent Erkok <erkokl@gmail.com>
2025-11-22 10:37:07 -0800

    Move test here

--- SBVTestSuite/TestSuite/Uninterpreted/EUFLogic.hs
+++ SBVTestSuite/TestSuite/Uninterpreted/EUFLogic.hs
@@ -4,6 +4,7 @@
 -- License   : BSD3
 -- Stability : experimental
 --
+-- Test suite for the EUFLogic example
 -----------------------------------------------------------------------------
 
 {-# OPTIONS_GHC -Wall -Werror #-}
@@ -18,6 +19,27 @@ import Utils.SBVTestFramework
 tests :: TestTree
 tests =
   testGroup "Uninterpreted.EUFLogic"
-    [ testCase         "euflogic-1"       $ assertIsThm (interpEUF example1)
-    , testCase         "euflogic-2"       $ assertIsThm (interpEUF example2)
+    [ testCase  "euflogic-1" $ assertIsThm (interpEUF example)
+    , testCase  "euflogic-2" $ assertIsThm (interpEUF fourteen)
     ]
+
+fourteen :: EUFExpr Tp_Bool
+fourteen =
+  applyOp Op_And (applyOp (Op_BVEq knownBVWidth)
+                          (applyOp f14 0 0 0 0 0 0 0 0 0 0 0 0 0
+                          (applyOp f14 0 0 0 0 0 0 0 0 0 0 0 0 0 a))
+                          (applyOp f14 0 0 0 0 0 0 0 0 0 0 0 0 0 a))
+                 (applyOp Op_And (applyOp (Op_BVEq knownBVWidth)
+                                          a
+                                          (applyOp f14 0 0 0 0 0 0 0 0 0 0 0 0 0 b))
+                                          (applyOp Op_Not
+    (applyOp (Op_BVEq knownBVWidth) a b)))
+  where
+    f14 :: Op '[Tp_BV 8, Tp_BV 8, Tp_BV 8, Tp_BV 8, Tp_BV 8, Tp_BV 8, Tp_BV 8,
+                Tp_BV 8, Tp_BV 8, Tp_BV 8, Tp_BV 8, Tp_BV 8, Tp_BV 8, Tp_BV 8]
+           (Tp_BV 8)
+    f14 = mkUnintOp "f"
+    a :: EUFExpr (Tp_BV 8)
+    a = mkUnintExpr "a"
+    b :: EUFExpr (Tp_BV 8)
+    b = mkUnintExpr "b"
