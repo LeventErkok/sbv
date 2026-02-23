59763d05efd631651f65c40bfdd1041b752421b4
Levent Erkok <erkokl@gmail.com>
2026-02-23 12:13:18 -0800

    update gold

--- SBVTestSuite/TestSuite/CompileTests/PCase/PCase08.hs
+++ SBVTestSuite/TestSuite/CompileTests/PCase/PCase08.hs
@@ -1,4 +1,8 @@
-{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE QuasiQuotes       #-}
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeAbstractions  #-}
+{-# LANGUAGE TypeApplications  #-}
 
 {-# OPTIONS_GHC -Wall -Werror #-}
 
@@ -8,10 +12,11 @@ import Expr
 import Data.SBV
 import Data.SBV.TP
 
--- Wildcard not allowed in pCase
-t :: SExpr -> Proof SBool
-t e = [pCase|Expr e of
-        Zero  -> undefined
-        Num _ -> undefined
-        _     -> undefined
-      |]
+-- Positive: wildcard at the end catches remaining constructors
+t :: TP (Proof (Forall "e" Expr -> SBool))
+t = calc "t" (\(Forall @"e" (e :: SExpr)) -> e .== e) $ \e -> []
+    |- [pCase|Expr e of
+         Zero    -> e .== e =: qed
+         Num _   -> e .== e =: qed
+         _       -> e .== e =: qed
+       |]
