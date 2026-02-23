ca9d16b64a4340b7d217f1e1adb98eb80cb20199
Levent Erkok <erkokl@gmail.com>
2026-02-23 14:03:18 -0800

    new test

---
+++ SBVTestSuite/TestSuite/CompileTests/PCase/PCase55.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE QuasiQuotes       #-}
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeAbstractions  #-}
+{-# LANGUAGE TypeApplications  #-}
+
+{-# OPTIONS_GHC -Wall -Werror -ddump-splices #-}
+
+module T where
+
+import Expr
+import Data.SBV
+import Data.SBV.TP
+
+-- Dump test: wildcard after some constructors (De Morgan negation)
+t :: TP (Proof (Forall "e" Expr -> SBool))
+t = calc "t" (\(Forall @"e" (e :: SExpr)) -> e .== e) $ \e -> []
+    |- [pCase|Expr e of
+         Zero      -> e .== e =: qed
+         Num _     -> e .== e =: qed
+         _         -> e .== e =: qed
+       |]
