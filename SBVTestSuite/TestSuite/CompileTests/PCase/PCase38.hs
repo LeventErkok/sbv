ab749f4a8114859fb0446a619bd21770aa2fdba9
Levent Erkok <erkokl@gmail.com>
2026-02-23 12:41:07 -0800

    update gold

--- SBVTestSuite/TestSuite/CompileTests/PCase/PCase38.hs
+++ SBVTestSuite/TestSuite/CompileTests/PCase/PCase38.hs
@@ -8,9 +8,6 @@ import Expr
 import Data.SBV
 import Data.SBV.TP
 
--- Negative in pCase: all constructors covered, then redundant wildcard at end
--- (sCase says "wildcard match is redundant";
---  pCase says wildcards are not allowed at all)
 t :: SExpr -> Proof SBool
 t e = [pCase|Expr e of
         Zero      -> undefined
@@ -18,5 +15,4 @@ t e = [pCase|Expr e of
         Var _     -> undefined
         Add _ _   -> undefined
         Let _ _ _ -> undefined
-        _         -> undefined
       |]
