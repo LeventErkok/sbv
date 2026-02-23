cb58fc667492223c9904022894cb6e2b8666f4d8
Levent Erkok <erkokl@gmail.com>
2026-02-23 00:26:28 -0800

    Add PCase37 compile test (guarded wildcard)
    Add a new compile-time test PCase37 to SBVTestSuite that exercises pCase with a guarded wildcard following explicit constructor matches. The Haskell test (PCase37.hs) defines a simple proof over Expr using pCase with a final guarded wildcard branch; the accompanying .stderr records the expected compilation output (no failures). This ensures the compiler accepts a guarded wildcard when all constructors are otherwise covered.

---
+++ SBVTestSuite/TestSuite/CompileTests/PCase/PCase37.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE QuasiQuotes       #-}
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeAbstractions  #-}
+{-# LANGUAGE TypeApplications  #-}
+
+{-# OPTIONS_GHC -Wall -Werror #-}
+
+module T where
+
+import Expr
+import Data.SBV
+import Data.SBV.TP
+
+-- Negative: all constructors covered + guarded wildcard at end
+-- (sCase rejects: guarded wildcard might fail; pCase rejects: wildcards not allowed)
+t :: TP (Proof (Forall "e" Expr -> SBool))
+t = calc "t" (\(Forall @"e" (e :: SExpr)) -> e .== e) $ \e -> []
+    |- [pCase|Expr e of
+       Zero           -> e .== e =: qed
+       Num i          -> sNum i .== sNum i =: e .== e =: qed
+       Var _          -> e .== e =: qed
+       Add _ _        -> e .== e =: qed
+       Let _ _ _      -> e .== e =: qed
+       _ | 2 .>= 3   -> e .== e =: qed
+     |]
